45ad59107f0e074c71ff870f7bebff11b0df6499
Keith Mahoney <kmahone@microsoft.com>
2018-12-07 17:22:33 -0800

    run all interaction tests

--- build/Helix/runtests.cmd
+++ build/Helix/runtests.cmd
@@ -4,4 +4,4 @@ set
 robocopy %HELIX_CORRELATION_PAYLOAD% . /s
 dir /b /s
 te MUXControls.Test.dll /list /unicodeOutput:false
-te MUXControls.Test.dll /unicodeOutput:false /name:Windows.UI.Xaml.Tests.MUXControls.InteractionTests.ColorPickerTests.* /testtimeout:0:01
+te MUXControls.Test.dll /unicodeOutput:false /testtimeout:0:01 %*
