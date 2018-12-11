62ce395c53653a5de0ba5d455798b11d0857b51a
Keith Mahoney <kmahone@microsoft.com>
2018-12-11 15:45:26 -0800

    add script to convert from taef to xunit log

--- build/Helix/runtests.cmd
+++ build/Helix/runtests.cmd
@@ -4,3 +4,4 @@ set
 robocopy %HELIX_CORRELATION_PAYLOAD% . /s
 dir /b /s
 te MUXControls.Test.dll MUXControlsTestApp.appx /enablewttlogging /unicodeOutput:false /testtimeout:0:05 %*
+powershell Te.wtl testResults.xml
