a3d0f314c5bac6e8fd48ff69ab1514d7da142797
Keith Mahoney <kmahone@microsoft.com>
2018-12-11 16:16:07 -0800

    fix path to ps1

--- build/Helix/runtests.cmd
+++ build/Helix/runtests.cmd
@@ -4,4 +4,4 @@ set
 robocopy %HELIX_CORRELATION_PAYLOAD% . /s
 dir /b /s
 te MUXControls.Test.dll MUXControlsTestApp.appx /enablewttlogging /unicodeOutput:false /testtimeout:0:05 %*
-powershell ConvertWttLogToXUnit.ps1 Te.wtl testResults.xml
+powershell ./ConvertWttLogToXUnit.ps1 Te.wtl testResults.xml
