1af73f2c054dda39a87067b466c245934709d0fe
Keith Mahoney <kmahone@microsoft.com>
2018-12-13 12:43:01 -0800

    fix nuget path 2

--- build/Helix/PrepareHelixPayloadDir.ps1
+++ build/Helix/PrepareHelixPayloadDir.ps1
@@ -25,7 +25,7 @@ copy "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtim
 copy "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\native\*" "$payloadDir\.NETCoreApp2.1\"
 
 # Copy files from the repo-level nuget packages dir
-copy "$nugetPackagesDir\packages\MUXCustomBuildTasks.1.0.38\tools\$platform\WttLog.dll" $payloadDir
+copy "$nugetPackagesDir\MUXCustomBuildTasks.1.0.38\tools\$platform\WttLog.dll" $payloadDir
 
 # Copy files from the 'drop' artifact dir
 copy "$repoDirectory\Artifacts\drop\$flavor\$platform\Test\MUXControls.Test.dll" $payloadDir
