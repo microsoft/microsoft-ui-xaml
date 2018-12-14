66170ae0256f43d97f40ee9378ea29b484fdaa01
Keith Mahoney <kmahone@microsoft.com>
2018-12-14 11:52:23 -0800

    CR Feedback

--- build/Helix/PrepareHelixPayload.ps1
+++ build/Helix/PrepareHelixPayload.ps1
@@ -13,23 +13,23 @@ If(test-path $payloadDir)
 New-Item -ItemType Directory -Force -Path $payloadDir
 
 # Copy files from nuget packages
-copy "$nugetPackagesDir\microsoft.windows.apps.test.1.0.181203002\lib\netcoreapp2.1\*.dll" $payloadDir
-copy "$nugetPackagesDir\taef.redist.wlk.10.31.180822002\build\Binaries\$platform\*" $payloadDir
-copy "$nugetPackagesDir\taef.redist.wlk.10.31.180822002\build\Binaries\$platform\CoreClr\*" $payloadDir
-copy "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\lib\netcoreapp2.1\*" $payloadDir
-copy "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\native\*" $payloadDir
-mkdir "$payloadDir\.NETCoreApp2.1\"
-copy "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\lib\netcoreapp2.1\*" "$payloadDir\.NETCoreApp2.1\"
-copy "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\native\*" "$payloadDir\.NETCoreApp2.1\"
-copy "$nugetPackagesDir\MUXCustomBuildTasks.1.0.38\tools\$platform\WttLog.dll" $payloadDir
+Copy-Item "$nugetPackagesDir\microsoft.windows.apps.test.1.0.181203002\lib\netcoreapp2.1\*.dll" $payloadDir
+Copy-Item "$nugetPackagesDir\taef.redist.wlk.10.31.180822002\build\Binaries\$platform\*" $payloadDir
+Copy-Item "$nugetPackagesDir\taef.redist.wlk.10.31.180822002\build\Binaries\$platform\CoreClr\*" $payloadDir
+Copy-Item "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\lib\netcoreapp2.1\*" $payloadDir
+Copy-Item "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\native\*" $payloadDir
+New-Item -ItemType Directory -Force -Path "$payloadDir\.NETCoreApp2.1\"
+Copy-Item "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\lib\netcoreapp2.1\*" "$payloadDir\.NETCoreApp2.1\"
+Copy-Item "$nugetPackagesDir\runtime.win-$platform.microsoft.netcore.app.2.1.0\runtimes\win-$platform\native\*" "$payloadDir\.NETCoreApp2.1\"
+Copy-Item "$nugetPackagesDir\MUXCustomBuildTasks.1.0.38\tools\$platform\WttLog.dll" $payloadDir
 
 # Copy files from the 'drop' artifact dir
-copy "$repoDirectory\Artifacts\drop\$flavor\$platform\Test\MUXControls.Test.dll" $payloadDir
-copy "$repoDirectory\Artifacts\drop\$flavor\$platform\AppxPackages\MUXControlsTestApp_Test\*" $payloadDir
-copy "$repoDirectory\Artifacts\drop\$flavor\$platform\AppxPackages\MUXControlsTestApp_Test\Dependencies\$platform\*" $payloadDir
+Copy-Item "$repoDirectory\Artifacts\drop\$flavor\$platform\Test\MUXControls.Test.dll" $payloadDir
+Copy-Item "$repoDirectory\Artifacts\drop\$flavor\$platform\AppxPackages\MUXControlsTestApp_Test\*" $payloadDir
+Copy-Item "$repoDirectory\Artifacts\drop\$flavor\$platform\AppxPackages\MUXControlsTestApp_Test\Dependencies\$platform\*" $payloadDir
 
 # Copy files from the repo
-copy "build\helix\runtests.cmd" $payloadDir
-mkdir "$payloadDir\scripts"
-copy "build\helix\ConvertWttLogToXUnit.ps1" "$payloadDir\scripts"
-copy "build\helix\ConvertWttLogToXUnit.cs" "$payloadDir\scripts"
+Copy-Item "build\helix\runtests.cmd" $payloadDir
+New-Item -ItemType Directory -Force -Path "$payloadDir\scripts"
+Copy-Item "build\helix\ConvertWttLogToXUnit.ps1" "$payloadDir\scripts"
+Copy-Item "build\helix\ConvertWttLogToXUnit.cs" "$payloadDir\scripts"
