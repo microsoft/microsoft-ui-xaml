b2c336835a10618bd09e525bf5bf1a9a7abc0279
Jevan Saks <jevansa@microsoft.com>
2018-12-20 14:59:01 -0800

    Change to using FindPackages to be able to search by package family name and find apps that were installed in other architectures

--- tools/InstallTestApp.Desktop.cmd
+++ tools/InstallTestApp.Desktop.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-if exist "%SystemDrive%\Program Files\WindowsApps\407b1cc5-f51e-4bfa-b5d2-04afa83fe380_1.0.0.0_neutral__8wekyb3d8bbwe\MUXControlsTestApp.exe" (
+if exist "%SystemDrive%\Program Files\WindowsApps\MUXControlsTestApp_1.0.0.0_neutral__8wekyb3d8bbwe\MUXControlsTestApp.exe" (
     echo MUXControlsTestApp.appx already installed. Uninstalling...
-    powershell Remove-AppxPackage 407b1cc5-f51e-4bfa-b5d2-04afa83fe380_1.0.0.0_neutral__8wekyb3d8bbwe
+    powershell Remove-AppxPackage MUXControlsTestApp_1.0.0.0_neutral__8wekyb3d8bbwe
 )
 
 echo Installing MUXControlsTestApp.appx...
--- tools/InstallTestApp.Phone.cmd
+++ tools/InstallTestApp.Phone.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-if exist "%SystemDrive%\Data\PROGRAMS\WINDOWSAPPS\407b1cc5-f51e-4bfa-b5d2-04afa83fe380_1.0.0.0_neutral__8wekyb3d8bbwe\MUXControlsTestApp.exe" (
+if exist "%SystemDrive%\Data\PROGRAMS\WINDOWSAPPS\MUXControlsTestApp_1.0.0.0_neutral__8wekyb3d8bbwe\MUXControlsTestApp.exe" (
     echo MUXControlsTestApp.appx already installed. Uninstalling...
-    th {407B1CC5-F51E-4BFA-B5D2-04AFA83FE380} -u
+    th {MUXControlsTestApp} -u
     echo.
 )
 
