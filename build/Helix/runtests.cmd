cc37960c02156f8004aa3e88b1396b35446668df
Keith Mahoney <kmahone@microsoft.com>
2018-12-23 15:45:15 -0800

    upload te.wtl

--- build/Helix/runtests.cmd
+++ build/Helix/runtests.cmd
@@ -2,6 +2,8 @@ robocopy %HELIX_CORRELATION_PAYLOAD% . /s
 
 te MUXControls.Test.dll MUXControlsTestApp.appx IXMPTestApp.appx /enablewttlogging /unicodeOutput:false /sessionTimeout:0:15 /testtimeout:0:10 %*
 
+%HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result te.wtl -result_name kmahone-te.wtl
+
 cd scripts
 powershell -ExecutionPolicy Bypass .\ConvertWttLogToXUnit.ps1 ..\te.wtl ..\testResults.xml %testnameprefix%
 cd ..
