5a275d5cc69196372969c36a1c1771cec83eebf8
Luke Longley <llongley@microsoft.com>
2019-01-15 18:15:20 -0800

    CR feedback.

--- tools/UpdateBuildMachineGitInstalls.ps1
+++ tools/UpdateBuildMachineGitInstalls.ps1
@@ -9,9 +9,9 @@ $adalPath = Get-AdalPath
 Write-Verbose "ADALPath: $adalPath"
 Add-Type -Path "$adalPath\lib\net45\Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
 
-$result = Queue-BuildOnMachine -MachineName "PKGESWINUI24" -ClientAlias "winui2" -BuildId "17093" # Install GVFS
+$result = Queue-BuildOnMachine -MachineName "PKGESWINUI24" -BuildId "17093" # Install GVFS
 Write-Host "PKGESWINUI24: $($result._links.web.href)"
-$result = Queue-BuildOnMachine -MachineName "PKGESWINUI25" -ClientAlias "winui2" -BuildId "17093" # Install GVFS
+$result = Queue-BuildOnMachine -MachineName "PKGESWINUI25" -BuildId "17093" # Install GVFS
 Write-Host "PKGESWINUI25: $($result._links.web.href)"
-$result = Queue-BuildOnMachine -MachineName "PKGESWINUI44" -ClientAlias "winui" -BuildId "17093" # Install GVFS
+$result = Queue-BuildOnMachine -MachineName "PKGESWINUI44" -BuildId "17093" # Install GVFS
 Write-Host "PKGESWINUI44: $($result._links.web.href)"
