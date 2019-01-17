68b6c2dc0ce8d7f521600643eff7074fe1c3e2b5
Luke Longley <llongley@microsoft.com>
2019-01-17 13:50:03 -0800

    Adding FlowsTo and FlowsFrom to ensure that the primary commands are connected to the secondary commands.

--- dev/CommandBarFlyout/CommandBarFlyoutCommandBar.h
+++ dev/CommandBarFlyout/CommandBarFlyoutCommandBar.h
@@ -28,16 +28,25 @@ private:
     void AttachEventHandlers();
     void DetachEventHandlers(bool useSafeGet = false);
 
+    void UpdateFlowsFromAndFlowsTo();
     void UpdateUI(bool useTransitions = true);
     void UpdateVisualState(bool useTransitions);
     void UpdateTemplateSettings();
 
     tracker_ref<winrt::FrameworkElement> m_primaryItemsRoot{ this };
     tracker_ref<winrt::FrameworkElement> m_secondaryItemsRoot{ this };
+    tracker_ref<winrt::ButtonBase> m_moreButton{ this };
     weak_ref<winrt::CommandBarFlyout> m_owningFlyout{ nullptr };
     winrt::FrameworkElement::SizeChanged_revoker m_secondaryItemsRootSizeChangedRevoker{};
     winrt::IInspectable m_keyDownHandler{ nullptr };
     winrt::FrameworkElement::Loaded_revoker m_firstSecondaryItemLoadedRevoker{};
+
+    // We need to manually connect the end element of the primary items to the start element of the secondary items
+    // for the purposes of UIA items navigation. To ensure that we only have the current start and end elements registered
+    // (e.g., if the app adds a new start element to the secondary commands, we want to unregister the previous start element),
+    // we'll save references to those elements.
+    tracker_ref<winrt::FrameworkElement> m_currentPrimaryItemsEndElement{ this };
+    tracker_ref<winrt::FrameworkElement> m_currentSecondaryItemsStartElement{ this };
 
     tracker_ref<winrt::Storyboard> m_openingStoryboard{ this };
     tracker_ref<winrt::Storyboard> m_closingStoryboard{ this };
