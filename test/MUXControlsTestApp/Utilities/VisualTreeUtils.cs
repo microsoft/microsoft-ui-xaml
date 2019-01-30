2aed2ce40095c65ee7094cfad624d72a756796cd
Luke Longley <llongley@microsoft.com>
2019-01-30 12:37:47 -0800

    Removed an unused using.

--- test/MUXControlsTestApp/Utilities/VisualTreeUtils.cs
+++ test/MUXControlsTestApp/Utilities/VisualTreeUtils.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 
