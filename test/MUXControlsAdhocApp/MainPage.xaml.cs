09b860d1430018636d6f2965b7bde0801e2bb99b
Keith Mahoney <kmahone@microsoft.com>
2019-02-02 11:19:10 -0800

    revert adhoc app changes

--- test/MUXControlsAdhocApp/MainPage.xaml.cs
+++ test/MUXControlsAdhocApp/MainPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 namespace MUXControlsAdhocApp
@@ -27,23 +26,6 @@ namespace MUXControlsAdhocApp
             
             MainStackPanel.Children.Add(commandBarFlyoutButton);
 #endif
-        }
-
-        private void RichEditBox_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-            var selection = richEditBox.Document.Selection;
-            var bold = selection.CharacterFormat.Bold;
-            var italic = selection.CharacterFormat.Italic;
-            var underline = selection.CharacterFormat.Underline;
-            var length = selection.Length;
-            Log("bold={0}, italic={1}, underline={2}, length={3}", bold, italic, underline, length);
-        }
-
-        private void Log(string msg, params object[] args)
-        {
-            var msg2 = string.Format(msg, args);
-            Debug.WriteLine(msg2);
-            tb1.Text = msg2;
         }
     }
 }
