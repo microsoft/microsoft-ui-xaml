1079c19e4e61a76cecbb93bfc428f777bc58d65e
Stephen Peters <stpete@microsoft.com>
2019-02-08 12:54:58 -0800

    Make a generic for the element retreival.

--- dev/TeachingTip/InteractionTests/TeachingTipTestPageElements.cs
+++ dev/TeachingTip/InteractionTests/TeachingTipTestPageElements.cs
@@ -31,363 +31,194 @@ namespace Windows.UI.Xaml.Tests.MUXControls.InteractionTests
     {
         public ListBox GetLstTeachingTipEvents()
         {
-            if (lstTeachingTipEvents == null)
-            {
-                Log.Comment("Find the lstTeachingTipEvents");
-                lstTeachingTipEvents = new ListBox(FindElement.ByName("lstTeachingTipEvents"));
-                Verify.IsNotNull(lstTeachingTipEvents);
-            }
-            return lstTeachingTipEvents;
+            return GetElement(ref lstTeachingTipEvents, "lstTeachingTipEvents");
         }
         private ListBox lstTeachingTipEvents;
 
         public TextBlock GetEffectivePlacementTextBlock()
         {
-            if (effectivePlacementTextBlock == null)
-            {
-                Log.Comment("Find the EffectivePlacementTextBlock");
-                effectivePlacementTextBlock = new TextBlock(FindElement.ByName("EffectivePlacementTextBlock"));
-                Verify.IsNotNull(effectivePlacementTextBlock);
-            }
-            return effectivePlacementTextBlock;
+            return GetElement(ref effectivePlacementTextBlock, "EffectivePlacementTextBlock");
         }
         private TextBlock effectivePlacementTextBlock;
 
         public Button GetTargetBoundsButton()
         {
-            if (targetBoundsButton == null)
-            {
-                Log.Comment("Find the GetTargetBoundsButton");
-                targetBoundsButton = new Button(FindElement.ByName("GetTargetBoundsButton"));
-                Verify.IsNotNull(targetBoundsButton);
-            }
-            return targetBoundsButton;
+            return GetElement(ref targetBoundsButton, "GetTargetBoundsButton");
         }
         private Button targetBoundsButton;
 
         public TextBlock GetTargetXOffsetTextBlock()
         {
-            if (targetXOffsetTextBlock == null)
-            {
-                Log.Comment("Find the TargetXOffsetTextBlock");
-                targetXOffsetTextBlock = new TextBlock(FindElement.ByName("TargetXOffsetTextBlock"));
-                Verify.IsNotNull(targetXOffsetTextBlock);
-            }
-            return targetXOffsetTextBlock;
+            return GetElement(ref targetXOffsetTextBlock, "TargetXOffsetTextBlock");
         }
         private TextBlock targetXOffsetTextBlock;
 
         public TextBlock GetTargetYOffsetTextBlock()
         {
-            if (targetYOffsetTextBlock == null)
-            {
-                Log.Comment("Find the TargetYOffsetTextBlock");
-                targetYOffsetTextBlock = new TextBlock(FindElement.ByName("TargetYOffsetTextBlock"));
-                Verify.IsNotNull(targetYOffsetTextBlock);
-            }
-            return targetYOffsetTextBlock;
+            return GetElement(ref targetYOffsetTextBlock, "TargetYOffsetTextBlock");
         }
         private TextBlock targetYOffsetTextBlock;
 
         public TextBlock GetTargetWidthTextBlock()
         {
-            if (targetWidthTextBlock == null)
-            {
-                Log.Comment("Find the TargetWidthTextBlock");
-                targetWidthTextBlock = new TextBlock(FindElement.ByName("TargetWidthTextBlock"));
-                Verify.IsNotNull(targetWidthTextBlock);
-            }
-            return targetWidthTextBlock;
+            return GetElement(ref targetWidthTextBlock, "TargetWidthTextBlock");
         }
         private TextBlock targetWidthTextBlock;
 
         public TextBlock GetTargetHeightTextBlock()
         {
-            if (targetHeightTextBlock == null)
-            {
-                Log.Comment("Find the TargetHeightTextBlock");
-                targetHeightTextBlock = new TextBlock(FindElement.ByName("TargetHeightTextBlock"));
-                Verify.IsNotNull(targetHeightTextBlock);
-            }
-            return targetHeightTextBlock;
+            return GetElement(ref targetHeightTextBlock, "TargetHeightTextBlock");
         }
         private TextBlock targetHeightTextBlock;
 
         public CheckBox GetUseTestWindowBoundsCheckbox()
         {
-            if (useTestWindowBoundsCheckbox == null)
-            {
-                Log.Comment("Find the UseTestWindowBoundsCheckbox");
-                useTestWindowBoundsCheckbox = new CheckBox(FindElement.ByName("UseTestWindowBoundsCheckbox"));
-                Verify.IsNotNull(useTestWindowBoundsCheckbox);
-            }
-            return useTestWindowBoundsCheckbox;
+            return GetElement(ref useTestWindowBoundsCheckbox, "UseTestWindowBoundsCheckbox");
         }
         private CheckBox useTestWindowBoundsCheckbox;
 
         public Edit GetTestWindowBoundsXTextBox()
         {
-            if (testWindowBoundsXTextBox == null)
-            {
-                Log.Comment("Find the TestWindowBoundsXTextBox");
-                testWindowBoundsXTextBox = new Edit(FindElement.ByName("TestWindowBoundsXTextBox"));
-                Verify.IsNotNull(testWindowBoundsXTextBox);
-            }
-            return testWindowBoundsXTextBox;
+            return GetElement(ref testWindowBoundsXTextBox, "TestWindowBoundsXTextBox");
         }
         private Edit testWindowBoundsXTextBox;
 
         public Edit GetTestWindowBoundsYTextBox()
         {
-            if (testWindowBoundsYTextBox == null)
-            {
-                Log.Comment("Find the TestWindowBoundsYTextBox");
-                testWindowBoundsYTextBox = new Edit(FindElement.ByName("TestWindowBoundsYTextBox"));
-                Verify.IsNotNull(testWindowBoundsYTextBox);
-            }
-            return testWindowBoundsYTextBox;
+            return GetElement(ref testWindowBoundsYTextBox, "TestWindowBoundsYTextBox");
         }
         private Edit testWindowBoundsYTextBox;
 
         public Edit GetTestWindowBoundsWidthTextBox()
         {
-            if (testWindowBoundsWidthTextBox == null)
-            {
-                Log.Comment("Find the TestWindowBoundsWidthTextBox");
-                testWindowBoundsWidthTextBox = new Edit(FindElement.ByName("TestWindowBoundsWidthTextBox"));
-                Verify.IsNotNull(testWindowBoundsWidthTextBox);
-            }
-            return testWindowBoundsWidthTextBox;
+            return GetElement(ref testWindowBoundsWidthTextBox, "TestWindowBoundsWidthTextBox");
         }
         private Edit testWindowBoundsWidthTextBox;
 
         public Edit GetTestWindowBoundsHeightTextBox()
         {
-            if (testWindowBoundsHeightTextBox == null)
-            {
-                Log.Comment("Find the TestWindowBoundsHeightTextBox");
-                testWindowBoundsHeightTextBox = new Edit(FindElement.ByName("TestWindowBoundsHeightTextBox"));
-                Verify.IsNotNull(testWindowBoundsHeightTextBox);
-            }
-            return testWindowBoundsHeightTextBox;
+            return GetElement(ref testWindowBoundsHeightTextBox, "TestWindowBoundsHeightTextBox");
         }
         private Edit testWindowBoundsHeightTextBox;
 
         public TextBlock GetTipWidthTextBlock()
         {
-            if (tipWidthTextBlock == null)
-            {
-                Log.Comment("Find the TipWidthTextBlock");
-                tipWidthTextBlock = new TextBlock(FindElement.ByName("TipWidthTextBlock"));
-                Verify.IsNotNull(tipWidthTextBlock);
-            }
-            return tipWidthTextBlock;
+            return GetElement(ref tipWidthTextBlock, "TipWidthTextBlock");
         }
         private TextBlock tipWidthTextBlock;
 
         public TextBlock GetScrollViewerStateTextBox()
         {
-            if (scrollViewerStateTextBox == null)
-            {
-                Log.Comment("Find the ScrollViewerStateTextBox");
-                scrollViewerStateTextBox = new TextBlock(FindElement.ByName("ScrollViewerStateTextBox"));
-                Verify.IsNotNull(scrollViewerStateTextBox);
-            }
-            return scrollViewerStateTextBox;
+            return GetElement(ref scrollViewerStateTextBox, "ScrollViewerStateTextBox");
         }
         private TextBlock scrollViewerStateTextBox;
 
         public Edit GetScrollViewerOffsetTextBox()
         {
-            if (scrollViewerOffsetTextBox == null)
-            {
-                Log.Comment("Find the ScrollViewerOffsetTextBox");
-                scrollViewerOffsetTextBox = new Edit(FindElement.ByName("ScrollViewerOffsetTextBox"));
-                Verify.IsNotNull(scrollViewerOffsetTextBox);
-            }
-            return scrollViewerOffsetTextBox;
+            return GetElement(ref scrollViewerOffsetTextBox, "ScrollViewerOffsetTextBox");
         }
         private Edit scrollViewerOffsetTextBox;
 
         public Button GetScrollViewerOffsetButton()
         {
-            if (scrollViewerOffsetButton == null)
-            {
-                Log.Comment("Find the ScrollViewerOffsetButton");
-                scrollViewerOffsetButton = new Button(FindElement.ByName("ScrollViewerOffsetButton"));
-                Verify.IsNotNull(scrollViewerOffsetButton);
-            }
-            return scrollViewerOffsetButton;
+            return GetElement(ref scrollViewerOffsetButton, "ScrollViewerOffsetButton");
         }
         private Button scrollViewerOffsetButton;
 
         public TextBlock GetPopupVerticalOffsetTextBlock()
         {
-            if (popupVerticalOffsetTextBlock == null)
-            {
-                Log.Comment("Find the PopupVerticalOffsetTextBlock");
-                popupVerticalOffsetTextBlock = new TextBlock(FindElement.ByName("PopupVerticalOffsetTextBlock"));
-                Verify.IsNotNull(popupVerticalOffsetTextBlock);
-            }
-            return popupVerticalOffsetTextBlock;
+            return GetElement(ref popupVerticalOffsetTextBlock, "PopupVerticalOffsetTextBlock");
         }
         private TextBlock popupVerticalOffsetTextBlock;
 
         public ComboBox GetBleedingContentComboBox()
         {
-            if (bleedingContentComboBox == null)
-            {
-                Log.Comment("Find the BleedingContentComboBox");
-                bleedingContentComboBox = new ComboBox(FindElement.ByName("BleedingContentComboBox"));
-                Verify.IsNotNull(bleedingContentComboBox);
-            }
-            return bleedingContentComboBox;
+            return GetElement(ref bleedingContentComboBox, "BleedingContentComboBox");
         }
         private ComboBox bleedingContentComboBox;
 
         public Button GetSetBleedingContentButton()
         {
-            if (setBleedingContentButton == null)
-            {
-                Log.Comment("Find the SetBleedingContentButton");
-                setBleedingContentButton = new Button(FindElement.ByName("SetBleedingContentButton"));
-                Verify.IsNotNull(setBleedingContentButton);
-            }
-            return setBleedingContentButton;
+            return GetElement(ref setBleedingContentButton, "SetBleedingContentButton");
         }
         private Button setBleedingContentButton;
 
         public ComboBox GetPlacementComboBox()
         {
-            if (placementComboBox == null)
-            {
-                Log.Comment("Find the PlacementComboBox");
-                placementComboBox = new ComboBox(FindElement.ByName("PlacementComboBox"));
-                Verify.IsNotNull(placementComboBox);
-            }
-            return placementComboBox;
+            return GetElement(ref placementComboBox, "PlacementComboBox");
         }
         private ComboBox placementComboBox;
 
         public Button GetSetPlacementButton()
         {
-            if (setPlacementButton == null)
-            {
-                Log.Comment("Find the SetPlacementButton");
-                setPlacementButton = new Button(FindElement.ByName("SetPlacementButton"));
-                Verify.IsNotNull(setPlacementButton);
-            }
-            return setPlacementButton;
+            return GetElement(ref setPlacementButton, "SetPlacementButton");
         }
         private Button setPlacementButton;
 
         public ComboBox GetIsLightDismissEnabledComboBox()
         {
-            if (isLightDismissEnabledComboBox == null)
-            {
-                Log.Comment("Find the IsLightDismissEnabledComboBox");
-                isLightDismissEnabledComboBox = new ComboBox(FindElement.ByName("IsLightDismissEnabledComboBox"));
-                Verify.IsNotNull(isLightDismissEnabledComboBox);
-            }
-            return isLightDismissEnabledComboBox;
+            return GetElement(ref isLightDismissEnabledComboBox, "IsLightDismissEnabledComboBox");
         }
         private ComboBox isLightDismissEnabledComboBox;
 
         public Button GetIsLightDismissEnabledButton()
         {
-            if (isLightDismissEnabledButton == null)
-            {
-                Log.Comment("Find the IsLightDismissEnabledButton");
-                isLightDismissEnabledButton = new Button(FindElement.ByName("IsLightDismissEnabledButton"));
-                Verify.IsNotNull(isLightDismissEnabledButton);
-            }
-            return isLightDismissEnabledButton;
+            return GetElement(ref isLightDismissEnabledButton, "IsLightDismissEnabledButton");
         }
         private Button isLightDismissEnabledButton;
 
         public Button GetShowButton()
         {
-            if (showButton == null)
-            {
-                Log.Comment("Find the ShowButton");
-                showButton = new Button(FindElement.ByName("ShowButton"));
-                Verify.IsNotNull(showButton);
-            }
-            return showButton;
+            return GetElement(ref showButton, "ShowButton");
         }
         private Button showButton;
 
         public Button GetCloseButton()
         {
-            if (closeButton == null)
-            {
-                Log.Comment("Find the CloseButton");
-                closeButton = new Button(FindElement.ByName("CloseButton"));
-                Verify.IsNotNull(closeButton);
-            }
-            return closeButton;
+            return GetElement(ref closeButton, "CloseButton");
         }
         private Button closeButton;
 
         public CheckBox GetIsOpenCheckBox()
         {
-            if (isOpenCheckBox == null)
-            {
-                Log.Comment("Find the IsOpenCheckBox");
-                isOpenCheckBox = new CheckBox(FindElement.ByName("IsOpenCheckBox"));
-                Verify.IsNotNull(isOpenCheckBox);
-            }
-            return isOpenCheckBox;
+            return GetElement(ref isOpenCheckBox, "IsOpenCheckBox");
         }
         private CheckBox isOpenCheckBox;
 
         public CheckBox GetIsIdleCheckBox()
         {
-            if (isIdleCheckBox == null)
-            {
-                Log.Comment("Find the IsIdleCheckBox");
-                isIdleCheckBox = new CheckBox(FindElement.ByName("IsIdleCheckBox"));
-                Verify.IsNotNull(isIdleCheckBox);
-            }
-            return isIdleCheckBox;
+            return GetElement(ref isIdleCheckBox, "IsIdleCheckBox");
         }
         private CheckBox isIdleCheckBox;
 
         public Button GetBringTargetIntoViewButton()
         {
-            if (bringTargetIntoViewButton == null)
-            {
-                Log.Comment("Find the BringTargetIntoViewButton");
-                bringTargetIntoViewButton = new Button(FindElement.ByName("BringTargetIntoViewButton"));
-                Verify.IsNotNull(bringTargetIntoViewButton);
-            }
-            return bringTargetIntoViewButton;
+            return GetElement(ref bringTargetIntoViewButton, "BringTargetIntoViewButton");
         }
         private Button bringTargetIntoViewButton;
 
         public CheckBox GetTipFollowsTargetCheckBox()
         {
-            if (tipFollowsTargetCheckBox == null)
-            {
-                Log.Comment("Find the TipFollowsTargetCheckBox");
-                tipFollowsTargetCheckBox = new CheckBox(FindElement.ByName("TipFollowsTargetCheckBox"));
-                Verify.IsNotNull(tipFollowsTargetCheckBox);
-            }
-            return tipFollowsTargetCheckBox;
+            return GetElement(ref tipFollowsTargetCheckBox, "TipFollowsTargetCheckBox");
         }
         private CheckBox tipFollowsTargetCheckBox;
 
         public UIObject GetTeachingTip()
         {
-            if (teachingTip == null)
-            {
-                Log.Comment("Find the TeachingTip");
-                teachingTip = FindElement.ByName("TeachingTip");
-                Verify.IsNotNull(teachingTip);
-            }
-            return teachingTip;
+            return GetElement(ref teachingTip, "TeachingTip");
         }
         private UIObject teachingTip;
+
+        private T GetElement<T>(ref T element, string elementName) where T : UIObject
+        {
+            if (element == null)
+            {
+                Log.Comment("Find the " + elementName);
+                element = FindElement.ByName<T>(elementName);
+                Verify.IsNotNull(element);
+            }
+            return element;
+        }
 
         public enum PlacementOptions
         {
