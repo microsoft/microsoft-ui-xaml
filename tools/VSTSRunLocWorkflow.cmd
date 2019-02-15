c7c15956b190d52a10d3ea9e4947cf69b6953b23
Jevan Saks <jevan.saks@gmail.com>
2019-02-15 13:40:10 -0800

    Add loc handoff pipeline

--- tools/VSTSRunLocWorkflow.cmd
+++ tools/VSTSRunLocWorkflow.cmd
@@ -1,11 +1,11 @@
 setlocal
 
-@if not defined XES_NUGETPACKVERSIONNOBETA (
-	echo ERROR: Expecting XES_NUGETPACKVERSIONNOBETA to be set
+@if not defined BUILDDATE (
+	echo ERROR: Expecting BUILDDATE to be set
 	exit /B 1
 )
 
-call %~dp0\..\build\localization\RunLocWorkflow.cmd Daily-%XES_NUGETPACKVERSIONNOBETA%
+call %~dp0\..\build\localization\RunLocWorkflow.cmd Daily-%BUILDDATE%.%BUILDREVISION%
 
 REM Undo any changes that were made since we are just doing handoff and the process will automatically hand back.
 pushd %~dp0\..
