48f9c35b62ac987576760d8cbf3a27960c81e7b7
Jevan Saks <jevan.saks@gmail.com>
2019-02-15 14:06:15 -0800

    CR Feedback

--- tools/VSTSRunLocWorkflow.cmd
+++ tools/VSTSRunLocWorkflow.cmd
@@ -5,6 +5,11 @@ setlocal
 	exit /B 1
 )
 
+@if not defined BUILDREVISION (
+	echo ERROR: Expecting BUILDREVISION to be set
+	exit /B 1
+)
+
 call %~dp0\..\build\localization\RunLocWorkflow.cmd Daily-%BUILDDATE%.%BUILDREVISION%
 
 REM Undo any changes that were made since we are just doing handoff and the process will automatically hand back.
