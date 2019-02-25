e275fa24a7aa27d694593adf80933a4c74cf329e
Jevan Saks <jevansa@microsoft.com>
2019-02-25 13:03:18 -0800

    Stash changes

---
+++ dev/PersonPicture/PersonPictureTemplateSettings.h
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "PersonPictureTemplateSettings.g.h"
+#include "PersonPictureTemplateSettings.properties.h"
+
+class PersonPictureTemplateSettingsTemplateSettings :
+    public winrt::implementation::PersonPictureTemplateSettingsTemplateSettingsT<PersonPictureTemplateSettingsTemplateSettings>,
+    public PersonPictureTemplateSettingsTemplateSettingsProperties
+{
+};
