41706f2e59faf4a01af4949ae16421f00172a71b
Kai Guo <guokai.ok@gmail.com>
2019-03-01 21:35:08 -0800

    Fix NavigationView.IsPaneVisible (#359)
    * Fix EnsurePaneCanBeHidden
    
    * New fix
    
    * Add test
    
    * Add comments
    
    * Update test
    
    * Disable tests

--- dev/NavigationView/NavigationView.cpp
+++ dev/NavigationView/NavigationView.cpp
@@ -583,7 +583,7 @@ void NavigationView::UpdateAdaptiveLayout(double width, bool forceSetDisplayMode
 
     SetDisplayMode(displayMode, forceSetDisplayMode);
 
-    if (displayMode == winrt::NavigationViewDisplayMode::Expanded)
+    if (displayMode == winrt::NavigationViewDisplayMode::Expanded && IsPaneVisible())
     {
         if (!m_wasForceClosed)
         {
@@ -2640,6 +2640,18 @@ void NavigationView::OnPropertyChanged(const winrt::DependencyPropertyChangedEve
     {
         UpdatePaneVisibility();
         UpdateVisualStateForDisplayModeGroup(DisplayMode());
+
+        // When NavView is in expaneded mode with fixed window size, setting IsPaneVisible to false doesn't closes the pane
+        // We manually close/open it for this case
+        if (!IsPaneVisible() && IsPaneOpen())
+        {
+            ClosePane();
+        }
+
+        if (IsPaneVisible() && DisplayMode() == winrt::NavigationViewDisplayMode::Expanded && !IsPaneOpen())
+        {
+            OpenPane();
+        }
     }
     else if (property == s_OverflowLabelModeProperty)
     {
