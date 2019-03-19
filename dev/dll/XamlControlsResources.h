a4866ab2a8842d6a6f6ea9cb4d4d5da56277b95d
CanHua Li <licanhua@live.com>
2019-03-19 12:25:21 -0700

    preview

---
+++ dev/dll/XamlControlsResources.h
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "XamlControlsResources.g.h"
+#include "XamlControlsResources.properties.h"
+
+class XamlControlsResources :
+    public ReferenceTracker<XamlControlsResources, winrt::implementation::XamlControlsResourcesT, winrt::composable>,
+    public XamlControlsResourcesProperties
+{
+public:
+    XamlControlsResources();
+
+    void OnPropertyChanged(const winrt::DependencyPropertyChangedEventArgs&  args);
+
+    static void EnsureRevealLights(winrt::UIElement const& element);
+private:
+    void UpdateSource();
+};
