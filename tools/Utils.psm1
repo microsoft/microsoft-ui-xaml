4d06639181c3de3f6ebf5b2bbadebac2a1d99fe1
Ranjesh <28935693+ranjeshj@users.noreply.github.com>
2019-03-19 21:43:00 -0700

    couple of updates to scope down the number of types in the manifest

--- tools/Utils.psm1
+++ tools/Utils.psm1
@@ -82,9 +82,48 @@ namespace WinmdHelper
 
             return loadFrom.GetExportedTypes().ToList();
         }
+
+        public static List<Type> GetActivatableTypesFromFile(string assemblyPath, string referenceWinmds)
+        {
+            var exportedTypes = FromFile(assemblyPath, referenceWinmds);
+            List<Type> activatableTypes = new List<Type>();
+            foreach(var type in exportedTypes)
+            {
+                if(!type.IsEnum && 
+                   !type.IsValueType &&
+                   !type.IsAbstract)
+                   {
+                     activatableTypes.Add(type);
+                   }
+            }
+
+            return activatableTypes;
+            
+        }
     }
 }
 "@
+
+function Get-ActivatableTypes
+{
+    param(
+        [Parameter(Mandatory=$true, HelpMessage="Paths to the WinMD files whose types need to be added to the manifest.")]
+        [string]$WinmdPaths,
+
+        [Parameter(Mandatory=$true, HelpMessage="Semicolon-delimited list of reference WinMD files.")]
+        [string]$ReferenceWinmds
+    )
+
+    $winmdPathList = $WinmdPaths.Split(";", [System.StringSplitOptions]::RemoveEmptyEntries)
+    [System.Collections.Generic.List[System.Type]]$typeList = @()
+
+    foreach ($winmdPath in $winmdPathList)
+    {
+        $typeList.AddRange([WinmdHelper.LoadWinmdTypes]::GetActivatableTypesFromFile($winmdPath, $ReferenceWinmds))
+    }
+
+    $typeList
+}
 
 function Get-WinmdTypes
 {
