f894d886db8f23e69739c7ed2c2800b879717cec
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-03-26 16:35:05 -0700

    Stub out CSS Grid implementation

---
+++ test/MUXControlsAdhocApp/GridPages/GridPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MUXControlsAdhocApp.GridPages
+{
+    public sealed partial class GridPage : Page
+    {
+        public GridPage()
+        {
+            this.InitializeComponent();
+
+            _contentFrame.Navigate(typeof(GridPlaygroundPage));
+        }
+
+        private void NavigationView_BackRequested(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs args)
+        {
+            Frame.GoBack();
+        }
+
+        private void NavigationView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+            string tag = args.InvokedItemContainer.Tag.ToString();
+            switch (tag)
+            {
+                case "Home":
+                    _contentFrame.Navigate(typeof(GridPlaygroundPage));
+                    break;
+            }
+        }
+    }
+}
---
+++ test/MUXControlsAdhocApp/GridPages/GridPlaygroundPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace MUXControlsAdhocApp.GridPages
+{
+    public sealed partial class GridPlaygroundPage : Page
+    {
+        public GridPlaygroundPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InvalidateButton_Click(object sender, RoutedEventArgs e)
+        {
+            _grid.InvalidateMeasure();
+        }
+    }
+}
