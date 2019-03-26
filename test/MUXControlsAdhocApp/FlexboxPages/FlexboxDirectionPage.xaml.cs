8299cc0da1aee85f44059cb83d8cb4d64234ae2b
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-03-26 12:50:03 -0700

    Break flexbox tests into separate pages

---
+++ test/MUXControlsAdhocApp/FlexboxPages/FlexboxDirectionPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MUXControlsAdhocApp.FlexboxPages
+{
+    public sealed partial class FlexboxDirectionPage : Page
+    {
+        public FlexboxDirectionPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
