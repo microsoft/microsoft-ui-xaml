aa4d697401b9081d927abdec733745cf7b3b2d19
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-03-28 10:22:28 -0700

    * Add handling of "auto" tracks, which require some speculative measuring * Add some trace support for ease of debugging

--- test/MUXControlsAdhocApp/GridPages/Grid.cs
+++ test/MUXControlsAdhocApp/GridPages/Grid.cs
@@ -4,6 +4,9 @@ using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
+using Debug = System.Diagnostics.Debug;
+using ConditionalAttribute = System.Diagnostics.ConditionalAttribute;
+
 namespace MUXControlsAdhocApp.GridPages
 {
     public class GridTrackInfo
@@ -149,50 +152,155 @@ namespace MUXControlsAdhocApp.GridPages
             }
         }
 
-        protected struct MeasureInfo
+        // Calculated info on one of the grid tracks
+        protected class MeasureInfo
         {
             public double Size;
+
+            public double Start;
         }
         private Dictionary<GridTrackInfo, MeasureInfo> _columns = new Dictionary<GridTrackInfo, MeasureInfo>();
         private Dictionary<GridTrackInfo, MeasureInfo> _rows = new Dictionary<GridTrackInfo, MeasureInfo>();
 
-        private static void PopulateFromTemplate(List<GridTrackInfo> template, Dictionary<GridTrackInfo, MeasureInfo> calculated, double available)
+        // Tracks all the intermediate calculations of one direction (row or column) of the grid
+        protected struct MeasureBlah
         {
-            double totalFixed = 0.0;
-            double totalFraction = 0.0;
+            public MeasureBlah(List<GridTrackInfo> template, Dictionary<GridTrackInfo, MeasureInfo> calculated)
+            {
+                Template = template;
+                Calculated = calculated;
+                Available = 0.0;
+                TotalFixed = 0.0;
+                TotalFraction = 0.0;
+                Remaining = 0.0;
+            }
+
+            public List<GridTrackInfo> Template { get; private set; }
+            public Dictionary<GridTrackInfo, MeasureInfo> Calculated { get; private set; }
+
+            public double Available;
+            public double TotalFixed;
+            public double TotalFraction;
+            public double Remaining;
+        }
+
+        private static void ProcessFixedSizes(List<GridTrackInfo> template, Dictionary<GridTrackInfo, MeasureInfo> calculated, double available, out MeasureBlah measure)
+        {
+            measure = new MeasureBlah(template, calculated) { Available = available };
+
             for (int i = 0; i < template.Count; i++)
             {
                 double fixedSize = template[i].Length;
+
+                // Percentage is effectively a fixed size in that it needs to be applied before any
+                // of the more relative sizes (fraction, auto, etc.)
                 if (fixedSize == 0.0)
                 {
                     fixedSize = (template[i].Percentage * available);
                 }
-                totalFixed += fixedSize;
-                totalFraction += template[i].Fraction;
+
+                measure.TotalFixed += fixedSize;
+
+                // Accumulate the fractional sizes now so we know how many pieces of pie to dish out
+                measure.TotalFraction += template[i].Fraction;
 
                 calculated[template[i]] = new MeasureInfo { Size = fixedSize };
             }
 
-            // First subtract the fixed size
-            double remaining = available - totalFixed;
+            measure.Remaining = measure.Available - measure.TotalFixed;
+        }
+
+        private void ProcessAutoSizes(ref MeasureBlah measureHorizontal, ref MeasureBlah measureVertical)
+        {
+            foreach (UIElement child in Children)
+            {
+                // Read preferences off the child
+                GridLocation colStart = GetColumnStart(child);
+                GridLocation rowStart = GetRowStart(child);
+                GridLocation colEnd = GetColumnEnd(child);
+                GridLocation rowEnd = GetRowEnd(child);
+
+                // Map those to our grid lines
+                GridTrackInfo colStartTrack = GetTrack(measureHorizontal.Template, colStart);
+                GridTrackInfo rowStartTrack = GetTrack(measureVertical.Template, rowStart);
+                GridTrackInfo colEndTrack = GetTrack(measureHorizontal.Template, colStart);
+                GridTrackInfo rowEndTrack = GetTrack(measureVertical.Template, rowStart);
+
+                // If none of the grid tracks are Auto then we can skip this item
+                if (!colStartTrack.Auto && !rowStartTrack.Auto)
+                {
+                    continue;
+                }
+
+                // Measure this child to see how much of the available space they would take.
+                // Note that even if there are multiple Autos we don't attempt to preemptively split 
+                // the space between them. They are all given a crack at being greedy.
+                Size measureSize = new Size(measureHorizontal.Remaining, measureVertical.Remaining);
+                child.Measure(measureSize);
+
+                // Update that row/column with the dimensions
+                Action<GridTrackInfo, MeasureBlah, double> updateAutoBasedOnMeasured = (GridTrackInfo track, MeasureBlah measure, double childDesired) =>
+                {
+                    if (track.Auto)
+                    {
+                        MeasureInfo info = measure.Calculated[track];
+                        if (childDesired > info.Size)
+                        {
+                            info.Size = childDesired;
+                        }
+                    }
+                };
+                updateAutoBasedOnMeasured(colStartTrack, measureHorizontal, child.DesiredSize.Width);
+                updateAutoBasedOnMeasured(rowStartTrack, measureVertical, child.DesiredSize.Height);
+            }
+
+            // TODO: Do we do anything with Remaining? Or is that left to be handled in Arrange?
+        }
+
+        private static void ProcessFractionalSizes(ref MeasureBlah measure)
+        {
+            if (measure.TotalFraction <= 0.0)
+            {
+                return;
+            }
 
             // What is the size of each fraction?
-            double fractionSlice = 0.0;
-            if (totalFraction > 0.0)
+            double fractionSlice = measure.Remaining / measure.TotalFraction;
+
+            for (int i = 0; i < measure.Template.Count; i++)
             {
-                fractionSlice = remaining / totalFraction;
+                GridTrackInfo track = measure.Template[i];
+                if (track.Fraction == 0.0)
+                {
+                    continue;
+                }
+
+                // We only apply the fraction if the item didn't also have a fixed size
+                MeasureInfo info = measure.Calculated[track];
+                if (info.Size != 0.0)
+                {
+                    continue;
+                }
+
+                double myFractionSlice = track.Fraction * fractionSlice;
+                info.Size = myFractionSlice;
             }
 
-            // Assign any fraction elements
-            for (int i = 0; i < template.Count; i++)
+            // Fractions consume all that's left
+            measure.Remaining = 0.0;
+        }
+
+
+        private double ProcessOffsets(ref MeasureBlah measure)
+        {
+            double offset = 0.0;
+            foreach (var entry in measure.Calculated)
             {
-                if (calculated[template[i]].Size == 0.0)
-                {
-                    double fraction = template[i].Fraction;
-                    double myFractionSlice = fraction * fractionSlice;
-                    calculated[template[i]] = new MeasureInfo { Size = myFractionSlice };
-                }
+                entry.Value.Start = offset;
+                offset += entry.Value.Size;
             }
+
+            return offset;
         }
 
         private GridTrackInfo GetTrack(List<GridTrackInfo> list, GridLocation location)
@@ -217,13 +325,71 @@ namespace MUXControlsAdhocApp.GridPages
             return calculated[info];
         }
 
+        [Conditional("TRACE")]
+        private void DumpTemplates()
+        {
+            Action<List<GridTrackInfo>> dumpTemplate = (List<GridTrackInfo> template) =>
+            {
+                Debug.Indent();
+                for (int i = 0; i < template.Count; i++)
+                {
+                    GridTrackInfo track = template[i];
+                    Debug.WriteLine($"{i} {{LineName={track.LineName}, Length={track.Length}, Percentage={track.Percentage}, Fraction={track.Fraction}, Auto={track.Auto}}}");
+                }
+                Debug.Unindent();
+            };
+
+            Debug.WriteLine("TemplateColumns");
+            dumpTemplate(_templateColumns);
+
+            Debug.WriteLine("TemplateRows");
+            dumpTemplate(_templateRows);
+        }
+
+        [Conditional("TRACE")]
+        private void DumpMeasureInfo(ref MeasureBlah measure, string info)
+        {
+            Debug.WriteLine(info);
+
+            Debug.Indent();
+            foreach (var entry in measure.Calculated)
+            {
+                int trackIndex = measure.Template.IndexOf(entry.Key);
+                Debug.WriteLine($"{trackIndex} {{Size={entry.Value.Size}, Start={entry.Value.Start}}}");
+                
+            }
+            Debug.Unindent();
+        }
+
+        [Conditional("TRACE")]
+        private void DumpMeasureInfo(ref MeasureBlah horizontalMeasure, ref MeasureBlah verticalMeasure, string info)
+        {
+            DumpMeasureInfo(ref horizontalMeasure, "Horizontal " + info);
+            DumpMeasureInfo(ref verticalMeasure, "Vertical " + info);
+        }
+
         protected override Size MeasureOverride(Size availableSize)
         {
+            DumpTemplates();
+
             _columns.Clear();
             _rows.Clear();
 
-            PopulateFromTemplate(_templateColumns, _columns, availableSize.Width);
-            PopulateFromTemplate(_templateRows, _rows, availableSize.Height);
+            // First process any fixed sizes
+            MeasureBlah horizontalMeasure;
+            MeasureBlah verticalMeasure;
+            ProcessFixedSizes(_templateColumns, _columns, availableSize.Width, out horizontalMeasure);
+            ProcessFixedSizes(_templateRows, _rows, availableSize.Height, out verticalMeasure);
+            DumpMeasureInfo(ref horizontalMeasure, ref verticalMeasure, "fixed sizes");
+
+            // Next we need to know how large the auto sizes are
+            ProcessAutoSizes(ref horizontalMeasure, ref verticalMeasure);
+            DumpMeasureInfo(ref horizontalMeasure, ref verticalMeasure, "auto sizes");
+
+            // Then we can figure out how large the fractional sizes should be
+            ProcessFractionalSizes(ref horizontalMeasure);
+            ProcessFractionalSizes(ref verticalMeasure);
+            DumpMeasureInfo(ref horizontalMeasure, ref verticalMeasure, "fractional sizes");
 
             Size usedSize = new Size(0, 0);
 
@@ -247,21 +413,51 @@ namespace MUXControlsAdhocApp.GridPages
 
                 // Give each child the maximum available space
                 child.Measure(measureSize);
-                Size childDesiredSize = child.DesiredSize;
-
-                usedSize.Width = Math.Max(usedSize.Width, childDesiredSize.Width);
-                usedSize.Height = Math.Max(usedSize.Height, childDesiredSize.Height);
             }
 
-            return usedSize;
+            // Now that the sizes are known we can calculate the offsets for the grid tracks
+            double width = ProcessOffsets(ref horizontalMeasure);
+            double height = ProcessOffsets(ref verticalMeasure);
+            DumpMeasureInfo(ref horizontalMeasure, ref verticalMeasure, "offsets");
+
+            // If there's no entry for columns/rows use the minimal size, otherwise use the whole space.
+            if (_templateColumns.Count > 0)
+            {
+                width = availableSize.Width;
+            }
+            if (_templateRows.Count > 0)
+            {
+                height = availableSize.Height;
+            }
+
+            return new Size(width, height);
         }
 
         protected override Size ArrangeOverride(Size finalSize)
         {
             foreach (UIElement child in Children)
             {
-                Size childDesiredSize = child.DesiredSize;
-                child.Arrange(new Rect(new Point(0, 0), childDesiredSize));
+                GridLocation colStart = GetColumnStart(child);
+                GridLocation rowStart = GetRowStart(child);
+                GridLocation colEnd = GetColumnEnd(child);
+                GridLocation rowEnd = GetRowEnd(child);
+
+                GridTrackInfo colStartTrack = GetTrack(_templateColumns, colStart);
+                GridTrackInfo rowStartTrack = GetTrack(_templateRows, rowStart);
+                GridTrackInfo colEndTrack = GetTrack(_templateColumns, colStart);
+                GridTrackInfo rowEndTrack = GetTrack(_templateRows, rowStart);
+
+                MeasureInfo colMeasure = GetMeasureInfo(colStartTrack, _columns);
+                MeasureInfo rowMeasure = GetMeasureInfo(rowStartTrack, _rows);
+
+                double left = colMeasure.Start;
+                double top = rowMeasure.Start;
+
+                double width = colMeasure.Size;
+                double height = rowMeasure.Size;
+
+                Size arrangeSize = new Size(width, height);
+                child.Arrange(new Rect(new Point(left, top), arrangeSize));
             }
             return finalSize;
         }
