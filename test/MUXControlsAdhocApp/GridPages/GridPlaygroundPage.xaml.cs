12f451c4a23ec138846555a2384185c634cd5b54
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-04-01 15:12:17 -0700

    Hook up controls to items column/row start/end

--- test/MUXControlsAdhocApp/GridPages/GridPlaygroundPage.xaml.cs
+++ test/MUXControlsAdhocApp/GridPages/GridPlaygroundPage.xaml.cs
@@ -15,6 +15,16 @@ namespace MUXControlsAdhocApp.GridPages
             _columnEndIndex.ValueChanged += (object sender, RangeBaseValueChangedEventArgs e) => { IndexValueChanged(sender, GridLocationType.ColumnEnd); };
             _rowStartIndex.ValueChanged += (object sender, RangeBaseValueChangedEventArgs e) => { IndexValueChanged(sender, GridLocationType.RowStart); };
             _rowEndIndex.ValueChanged += (object sender, RangeBaseValueChangedEventArgs e) => { IndexValueChanged(sender, GridLocationType.RowEnd); };
+
+            _columnStartLineName.TextChanged += (object sender, TextChangedEventArgs e) => { LineNameChanged(sender, GridLocationType.ColumnStart); };
+            _columnEndLineName.TextChanged += (object sender, TextChangedEventArgs e) => { LineNameChanged(sender, GridLocationType.ColumnEnd); };
+            _rowStartLineName.TextChanged += (object sender, TextChangedEventArgs e) => { LineNameChanged(sender, GridLocationType.RowStart); };
+            _rowEndLineName.TextChanged += (object sender, TextChangedEventArgs e) => { LineNameChanged(sender, GridLocationType.RowEnd); };
+
+            _columnStartSpan.ValueChanged += (object sender, RangeBaseValueChangedEventArgs e) => { SpanValueChanged(sender, GridLocationType.ColumnStart); };
+            _columnEndSpan.ValueChanged += (object sender, RangeBaseValueChangedEventArgs e) => { SpanValueChanged(sender, GridLocationType.ColumnEnd); };
+            _rowStartSpan.ValueChanged += (object sender, RangeBaseValueChangedEventArgs e) => { SpanValueChanged(sender, GridLocationType.RowStart); };
+            _rowEndSpan.ValueChanged += (object sender, RangeBaseValueChangedEventArgs e) => { SpanValueChanged(sender, GridLocationType.RowEnd); };
         }
 
         private enum GridLocationType
@@ -40,9 +50,25 @@ namespace MUXControlsAdhocApp.GridPages
 
         private void IndexValueChanged(object sender, GridLocationType type)
         {
-            int index = (int)((Slider)sender).Value;
+            int value = (int)((Slider)sender).Value;
             GridLocation location = GetGridLocation(type);
-            location.Index = index;
+            location.Index = value;
+            _grid.InvalidateMeasure();
+        }
+
+        private void LineNameChanged(object sender, GridLocationType type)
+        {
+            string value = ((TextBox)sender).Text;
+            GridLocation location = GetGridLocation(type);
+            location.LineName = value;
+            _grid.InvalidateMeasure();
+        }
+
+        private void SpanValueChanged(object sender, GridLocationType type)
+        {
+            int value = (int)((Slider)sender).Value;
+            GridLocation location = GetGridLocation(type);
+            location.Span = value;
             _grid.InvalidateMeasure();
         }
 
