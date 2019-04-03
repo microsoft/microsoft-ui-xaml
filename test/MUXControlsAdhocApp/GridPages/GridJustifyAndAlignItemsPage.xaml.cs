226bdb8e20a0d769fecae4f8fd011e32225ee130
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-04-03 13:42:56 -0700

    Add stub pages to demonstrate various grid properties

---
+++ test/MUXControlsAdhocApp/GridPages/GridJustifyAndAlignItemsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MUXControlsAdhocApp.GridPages
+{
+    public sealed partial class GridJustifyAndAlignItemsPage : Page
+    {
+        public GridJustifyAndAlignItemsPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
