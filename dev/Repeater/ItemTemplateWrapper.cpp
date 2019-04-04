d5abae654e054a90cba03b1a77e90d141baf3b91
Ranjesh <28935693+ranjeshj@users.noreply.github.com>
2019-04-04 16:38:19 -0700

    dont try to activate recycle pool which is not public. (#526)
    * dont try to activate recycle pool which is not public.
    
    * empty commit to kick the pipeline
    
    * Ensure properties before they are used.

--- dev/Repeater/ItemTemplateWrapper.cpp
+++ dev/Repeater/ItemTemplateWrapper.cpp
@@ -41,7 +41,7 @@ void ItemTemplateWrapper::TemplateSelector(winrt::DataTemplateSelector const& va
 winrt::UIElement ItemTemplateWrapper::GetElement(winrt::ElementFactoryGetArgs const& args)
 {
     auto selectedTemplate = m_dataTemplate ? m_dataTemplate : m_dataTemplateSelector.SelectTemplate(args.Data());
-    auto recyclePool = winrt::RecyclePool::GetPoolInstance(selectedTemplate);
+    auto recyclePool = RecyclePool::GetPoolInstance(selectedTemplate);
     winrt::UIElement element = nullptr;
 
     if (recyclePool)
@@ -68,12 +68,12 @@ void ItemTemplateWrapper::RecycleElement(winrt::ElementFactoryRecycleArgs const&
     winrt::DataTemplate selectedTemplate = m_dataTemplate? 
         m_dataTemplate:
         element.GetValue(RecyclePool::GetOriginTemplateProperty()).as<winrt::DataTemplate>();
-    auto recyclePool = CachedVisualTreeHelpers::GetPoolInstance(selectedTemplate);
+    auto recyclePool = RecyclePool::GetPoolInstance(selectedTemplate);
     if (!recyclePool)
     {
         // No Recycle pool in the template, create one.
         recyclePool = winrt::make<RecyclePool>();
-        CachedVisualTreeHelpers::SetPoolInstance(selectedTemplate, recyclePool);
+        RecyclePool::SetPoolInstance(selectedTemplate, recyclePool);
     }
 
     recyclePool.PutElement(args.Element(), L"" /* key */, args.Parent());
