4ede725adbe83bb1017ea9d6b8bc170f0aa86061
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-04-12 14:42:15 -0700

    Add JustifySelf/AlignSelf

--- test/MUXControlsAdhocApp/GridPages/GridPage.xaml.cs
+++ test/MUXControlsAdhocApp/GridPages/GridPage.xaml.cs
@@ -53,6 +53,7 @@ namespace MUXControlsAdhocApp.GridPages
                     break;
 
                 case "ColumnStartAndRowStart":
+                    _contentFrame.Navigate(typeof(GridColumnAndRowStartPage));
                     break;
 
                 case "ColumnEndAndRowEnd":
