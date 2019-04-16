819927761cc63e13166bcf0a3d2d979f6231dc8a
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-04-16 14:34:24 -0700

    Stub for FlexboxLayout type

---
+++ dev/Repeater/FlexboxLayout.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include <pch.h>
+#include <common.h>
+//#include <DoubleUtil.h>
+#include "FlexboxLayout.h"
+#include "RuntimeProfiler.h"
+
+FlexboxLayout::FlexboxLayout()
+{
+    //__RP_Marker_ClassById(RuntimeProfiler::ProfId_FlexboxLayout);
+}
