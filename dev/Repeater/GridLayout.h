061336ff2b298c25751f0a049f629f3094a32278
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-04-16 14:49:35 -0700

    Add stub for GridLayout

---
+++ dev/Repeater/GridLayout.h
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "VirtualizingLayout.h"
+#include "GridLayout.g.h"
+
+class GridLayout :
+    public ReferenceTracker<GridLayout, winrt::implementation::GridLayoutT, VirtualizingLayout>
+{
+public:
+    GridLayout();
+};
