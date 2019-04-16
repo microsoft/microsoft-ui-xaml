f6990bee8ef60406b89862e18f07e0a0c2e4b021
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-04-16 14:34:24 -0700

    Stub for FlexboxLayout type

---
+++ dev/Repeater/FlexboxLayout.h
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "VirtualizingLayout.h"
+#include "FlexboxLayout.g.h"
+
+class FlexboxLayout :
+    public ReferenceTracker<FlexboxLayout, winrt::implementation::FlexboxLayoutT, VirtualizingLayout>
+{
+public:
+    FlexboxLayout();
+};
