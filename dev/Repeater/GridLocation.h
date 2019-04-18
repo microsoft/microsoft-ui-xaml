bb2be5dc0068d7e513fbc5ea244f51a19c1f6111
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-04-18 13:19:49 -0700

    Add Grid support types

---
+++ dev/Repeater/GridLocation.h
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "GridLocation.g.h"
+
+class GridLocation :
+    public ReferenceTracker<GridLocation, winrt::implementation::GridLocationT, winrt::composable, winrt::composing>
+{
+public:
+    GridLocation();
+
+    int Index();
+    void Index(int const& value);
+
+    winrt::hstring LineName();
+    void LineName(winrt::hstring const& value);
+
+    int Span();
+    void Span(int const& value);
+
+private:
+    int m_index;
+    winrt::hstring m_lineName;
+    int m_span;
+};
---
+++ dev/Repeater/GridTrackInfo.h
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "GridTrackInfo.g.h"
+
+class GridTrackInfo :
+    public ReferenceTracker<GridTrackInfo, winrt::implementation::GridTrackInfoT, winrt::composable, winrt::composing>
+{
+public:
+    GridTrackInfo();
+
+    winrt::hstring LineName();
+    void LineName(winrt::hstring const& value);
+
+    double Length();
+    void Length(double const& value);
+
+    bool Auto();
+    void Auto(bool const& value);
+
+    double Fraction();
+    void Fraction(double const& value);
+
+    double Percentage();
+    void Percentage(double const& value);
+
+private:
+    winrt::hstring m_lineName;
+    double m_length;
+    bool m_auto;
+    double m_fraction;
+    double m_percentage;
+};
