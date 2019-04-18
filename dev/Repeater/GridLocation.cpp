8f364c72489c8d6f992a3fac09b9e58073eec386
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-04-18 13:19:49 -0700

    Add Grid support types

---
+++ dev/Repeater/GridLocation.cpp
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
+#include "common.h"
+#include "GridLocation.h"
+
+GridLocation::GridLocation()
+{
+}
+
+int GridLocation::Index()
+{
+    return m_index;
+}
+
+void GridLocation::Index(int const& value)
+{
+    m_index = value;
+}
+
+winrt::hstring GridLocation::LineName()
+{
+    return m_lineName;
+}
+
+void GridLocation::LineName(winrt::hstring const& value)
+{
+    m_lineName = value;
+}
+
+int GridLocation::Span()
+{
+    return m_span;
+}
+
+void GridLocation::Span(int const& value)
+{
+    m_span = value;
+}
---
+++ dev/Repeater/GridTrackInfo.cpp
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
+#include "common.h"
+#include "GridTrackInfo.h"
+
+GridTrackInfo::GridTrackInfo()
+{
+}
+
+winrt::hstring GridTrackInfo::LineName()
+{
+    return m_lineName;
+}
+
+void GridTrackInfo::LineName(winrt::hstring const& value)
+{
+    m_lineName = value;
+}
+
+double GridTrackInfo::Length()
+{
+    return m_length;
+}
+
+void GridTrackInfo::Length(double const& value)
+{
+    m_length = value;
+}
+
+bool GridTrackInfo::Auto()
+{
+    return m_auto;
+}
+
+void GridTrackInfo::Auto(bool const& value)
+{
+    m_auto = value;
+}
+
+double GridTrackInfo::Fraction()
+{
+    return m_fraction;
+}
+
+void GridTrackInfo::Fraction(double const& value)
+{
+    m_fraction = value;
+}
+
+double GridTrackInfo::Percentage()
+{
+    return m_percentage;
+}
+
+void GridTrackInfo::Percentage(double const& value)
+{
+    m_percentage = value;
+}
