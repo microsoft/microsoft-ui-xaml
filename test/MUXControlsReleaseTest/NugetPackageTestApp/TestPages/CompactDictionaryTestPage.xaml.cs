fcf0672ddbb8aa7c3a16ee9435d54cc8248ba06b
Canhua Li <licanhua@live.com>
2019-04-24 13:02:30 -0700

    Exclude compact.xbf from package if it belongs to other project (#610)
    * Exclude compact.xbf from package if it belongs to other project

---
+++ test/MUXControlsReleaseTest/NugetPackageTestApp/TestPages/CompactDictionaryTestPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using MUXControlsTestApp;
+
+namespace NugetPackageTestApp
+{
+    public sealed partial class CompactDictionaryTestPage : TestPage
+    {
+        public CompactDictionaryTestPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
