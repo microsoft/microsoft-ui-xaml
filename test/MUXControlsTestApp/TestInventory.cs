6561ebdcfed69ae96086c82c20484cb649605c8d
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-04-26 17:16:07 -0700

    Move ms-appx:///Assets/ prefix into the TestDeclaration class.

--- test/MUXControlsTestApp/TestInventory.cs
+++ test/MUXControlsTestApp/TestInventory.cs
@@ -13,24 +13,24 @@ namespace MUXControlsTestApp
             Tests = new List<TestDeclaration>();
 
 #if !BUILD_LEAN_MUX_FOR_THE_STORE_APP
-            Tests.Add(new TestDeclaration("ColorPicker", typeof(ColorPickerPage), "ms-appx:///Assets/ColorPicker.png"));
+            Tests.Add(new TestDeclaration("ColorPicker", typeof(ColorPickerPage), "ColorPicker.png"));
             Tests.Add(new TestDeclaration("Leak", typeof(LeakTestPage)));
-            Tests.Add(new TestDeclaration("PersonPicture", typeof(PersonPicturePage), "ms-appx:///Assets/PersonPicture.png"));
-            Tests.Add(new TestDeclaration("RatingControl", typeof(RatingControlPage), "ms-appx:///Assets/RatingControl.png"));
-            Tests.Add(new TestDeclaration("SwipeControl", typeof(SwipePage), "ms-appx:///Assets/Swipe.png"));
-            Tests.Add(new TestDeclaration("TreeView", typeof(TreeViewPage), "ms-appx:///Assets/TreeView.png"));
-            Tests.Add(new TestDeclaration("TeachingTip", typeof(TeachingTipPage), "ms-appx:///Assets/TeachingTip.png"));
+            Tests.Add(new TestDeclaration("PersonPicture", typeof(PersonPicturePage), "PersonPicture.png"));
+            Tests.Add(new TestDeclaration("RatingControl", typeof(RatingControlPage), "RatingControl.png"));
+            Tests.Add(new TestDeclaration("SwipeControl", typeof(SwipePage), "Swipe.png"));
+            Tests.Add(new TestDeclaration("TreeView", typeof(TreeViewPage), "TreeView.png"));
+            Tests.Add(new TestDeclaration("TeachingTip", typeof(TeachingTipPage), "TeachingTip.png"));
             Tests.Add(new TestDeclaration("TwoPaneView", typeof(TwoPaneViewPage)));
-            Tests.Add(new TestDeclaration("PTR", typeof(PTRPage), "ms-appx:///Assets/PullToRefresh.png"));
-            Tests.Add(new TestDeclaration("MenuBar", typeof(MenuBarPage), "ms-appx:///Assets/MenuBar.png"));
-            Tests.Add(new TestDeclaration("MenuFlyout", typeof(MenuFlyoutPage), "ms-appx:///Assets/MenuFlyout.png"));
-            Tests.Add(new TestDeclaration("ScrollViewerAdapter", typeof(ScrollViewerAdapterPage), "ms-appx:///Assets/ScrollViewer.png"));
-            Tests.Add(new TestDeclaration("SplitButton", typeof(SplitButtonPage), "ms-appx:///Assets/SplitButton.png"));
-            Tests.Add(new TestDeclaration("DropDownButton", typeof(DropDownButtonPage), "ms-appx:///Assets/DropdownButton.png"));
-            Tests.Add(new TestDeclaration("CommandBarFlyout", typeof(CommandBarFlyoutMainPage), "ms-appx:///Assets/CommandBarFlyout.png"));
+            Tests.Add(new TestDeclaration("PTR", typeof(PTRPage), "PullToRefresh.png"));
+            Tests.Add(new TestDeclaration("MenuBar", typeof(MenuBarPage), "MenuBar.png"));
+            Tests.Add(new TestDeclaration("MenuFlyout", typeof(MenuFlyoutPage), "MenuFlyout.png"));
+            Tests.Add(new TestDeclaration("ScrollViewerAdapter", typeof(ScrollViewerAdapterPage), "ScrollViewer.png"));
+            Tests.Add(new TestDeclaration("SplitButton", typeof(SplitButtonPage), "SplitButton.png"));
+            Tests.Add(new TestDeclaration("DropDownButton", typeof(DropDownButtonPage), "DropdownButton.png"));
+            Tests.Add(new TestDeclaration("CommandBarFlyout", typeof(CommandBarFlyoutMainPage), "CommandBarFlyout.png"));
             Tests.Add(new TestDeclaration("CommonStyles", typeof(CommonStylesPage)));
-            Tests.Add(new TestDeclaration("Compact", typeof(CompactPage), "ms-appx:///Assets/CompactSizing.png"));
-            Tests.Add(new TestDeclaration("RadioButtons", typeof(RadioButtonsPage), "ms-appx:///Assets/RadioButton.png"));
+            Tests.Add(new TestDeclaration("Compact", typeof(CompactPage), "CompactSizing.png"));
+            Tests.Add(new TestDeclaration("RadioButtons", typeof(RadioButtonsPage), "RadioButton.png"));
             Tests.Add(new TestDeclaration("RadioMenuFlyoutItem", typeof(RadioMenuFlyoutItemPage)));
 #endif
 
@@ -38,21 +38,21 @@ namespace MUXControlsTestApp
             // We can't compile them without attaching the same feature annotation, and MIDL doesn't let us attach feature attributes
             // to non-public types.  So for now we'll just exclude these from the OS repo.
 #if (!BUILD_WINDOWS && !BUILD_LEAN_MUX_FOR_THE_STORE_APP)
-            Tests.Add(new TestDeclaration("ScrollViewer", typeof(ScrollViewerPage), "ms-appx:///Assets/ScrollViewer.png"));
+            Tests.Add(new TestDeclaration("ScrollViewer", typeof(ScrollViewerPage), "ScrollViewer.png"));
 #if (USE_INTERNAL_SDK)
-            Tests.Add(new TestDeclaration("ButtonInteraction", typeof(ButtonInteractionPage), "ms-appx:///Assets/Button.png"));
-            Tests.Add(new TestDeclaration("SliderInteraction", typeof(SliderInteractionPage), "ms-appx:///Assets/Slider.png"));
+            Tests.Add(new TestDeclaration("ButtonInteraction", typeof(ButtonInteractionPage), "Button.png"));
+            Tests.Add(new TestDeclaration("SliderInteraction", typeof(SliderInteractionPage), "Slider.png"));
 #endif
 #if (USE_INSIDER_SDK)
-            Tests.Add(new TestDeclaration("AnimatedVisualPlayer", typeof(AnimatedVisualPlayerPage), "ms-appx:///Assets/Animations.png"));
+            Tests.Add(new TestDeclaration("AnimatedVisualPlayer", typeof(AnimatedVisualPlayerPage), "Animations.png"));
 #endif
 #endif
-            Tests.Add(new TestDeclaration("NavigationView", typeof(NavigationViewCaseBundle), "ms-appx:///Assets/NavigationView.png"));
-            Tests.Add(new TestDeclaration("ParallaxView", typeof(ParallaxViewPage), "ms-appx:///Assets/ParallaxView.png"));
-            Tests.Add(new TestDeclaration("Acrylic", typeof(AcrylicPage), "ms-appx:///Assets/AcrylicBrush.png"));
-            Tests.Add(new TestDeclaration("Reveal", typeof(RevealPage), "ms-appx:///Assets/Reveal.png"));
-            Tests.Add(new TestDeclaration("ItemsRepeater", typeof(RepeaterTestUIPage), "ms-appx:///Assets/ListView.png"));
-            Tests.Add(new TestDeclaration("Scroller", typeof(ScrollerPage), "ms-appx:///Assets/ScrollViewer.png"));
+            Tests.Add(new TestDeclaration("NavigationView", typeof(NavigationViewCaseBundle), "NavigationView.png"));
+            Tests.Add(new TestDeclaration("ParallaxView", typeof(ParallaxViewPage), "ParallaxView.png"));
+            Tests.Add(new TestDeclaration("Acrylic", typeof(AcrylicPage), "AcrylicBrush.png"));
+            Tests.Add(new TestDeclaration("Reveal", typeof(RevealPage), "Reveal.png"));
+            Tests.Add(new TestDeclaration("ItemsRepeater", typeof(RepeaterTestUIPage), "ListView.png"));
+            Tests.Add(new TestDeclaration("Scroller", typeof(ScrollerPage), "ScrollViewer.png"));
 
             Tests.Sort((a, b) =>
             {
