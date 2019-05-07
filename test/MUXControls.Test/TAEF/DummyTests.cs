f1f9a38f618d1cc7984d053cb597fe7123bdc61c
Luke Longley <llongley@microsoft.com>
2019-05-07 12:29:48 -0700

    Adding dummy tests - one that always passes, one that always fails, and one that sometimes fails - to help test the re-running of tests.

---
+++ test/MUXControls.Test/TAEF/DummyTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+
+#if USING_TAEF
+using WEX.TestExecution;
+using WEX.TestExecution.Markup;
+using WEX.Logging.Interop;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+#endif
+
+namespace Windows.UI.Xaml.Tests.MUXControls.InteractionTests
+{
+    [TestClass]
+    public class DummyTests
+    {
+        [TestMethod]
+        public void PassingTest()
+        {
+            Verify.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void FailingTest()
+        {
+            Verify.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void UnreliableTest()
+        {
+            Verify.IsTrue(new Random(DateTime.Now.Millisecond).Next() % 2 == 0);
+        }
+    }
+}
