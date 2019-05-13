8d9ec40692a57266e73a1ca664ae02301b8a93e6
Luke Longley <llongley@microsoft.com>
2019-05-13 11:51:09 -0700

    Fixing things so we properly retrieve failing tests.

--- build/Helix/runtests.cmd
+++ build/Helix/runtests.cmd
@@ -4,9 +4,7 @@ robocopy %HELIX_CORRELATION_PAYLOAD% . /s /NP
 
 reg add HKLM\Software\Policies\Microsoft\Windows\Appx /v AllowAllTrustedApps /t REG_DWORD /d 1 /f
 
-cd scripts
-powershell -ExecutionPolicy Bypass .\InstallTestAppDependencies.ps1
-cd ..
+powershell -ExecutionPolicy Bypass scripts\InstallTestAppDependencies.ps1
 
 set testBinaryCandidates=MUXControls.Test.dll MUXControlsTestApp.appx IXMPTestApp.appx MUXControls.ReleaseTest.dll NugetPackageTestApp.appx NugetPackageTestAppCX.appx
 set testBinaries=
@@ -18,14 +16,6 @@ for %%B in (%testBinaryCandidates%) do (
 
 te %testBinaries% /enablewttlogging /unicodeOutput:false /sessionTimeout:0:15 /testtimeout:0:10 /screenCaptureOnError %*
 
-cd scripts
-for /F "tokens=* usebackq" %%A IN (`powershell -ExecutionPolicy Bypass .\OutputFailedTests.ps1 .\te.wtl`) DO (
-  set FailingTestQuery=%%A
-)
-cd ..
-
-te %testBinaries% /enablewttlogging /unicodeOutput:false /sessionTimeout:0:15 /testtimeout:0:10 /screenCaptureOnError /testmode:Loop /LoopTest:10 /select:"%FailingTestQuery%"
-
 %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result te.wtl -result_name te.wtl
 
 FOR %%I in (WexLogFileOutput\*.jpg) DO (
@@ -35,11 +25,31 @@ FOR %%I in (WexLogFileOutput\*.jpg) DO (
 
 move te.wtl te_old.wtl
 
+for /F "tokens=* usebackq" %%A IN (`powershell -ExecutionPolicy Bypass scripts\OutputFailedTests.ps1 .\te.wtl`) DO (
+  set FailingTestQuery=%%A
+)
+
+te %testBinaries% /enablewttlogging /unicodeOutput:false /sessionTimeout:0:15 /testtimeout:0:10 /screenCaptureOnError /testmode:Loop /LoopTest:10 /select:"%FailingTestQuery%"
+
+%HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result te.wtl -result_name te_rerun.wtl
+
+FOR %%I in (WexLogFileOutput\*.jpg) DO (
+    echo Uploading %%I to "%HELIX_RESULTS_CONTAINER_URI%/%%I%HELIX_RESULTS_CONTAINER_RSAS%"
+    %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result %%I -result_name %%~nI%%~xI_rerun
+)
+
+echo.
+echo te_old.wtl:
+echo.
+
 type te_old.wtl
+
+echo.
+echo te.wtl:
+echo.
+
 type te.wtl
 
-cd scripts
-powershell -ExecutionPolicy Bypass .\ConvertWttLogToXUnit.ps1 ..\te.wtl ..\testResults.xml %testnameprefix%
-cd ..
+powershell -ExecutionPolicy Bypass scripts\ConvertWttLogToXUnit.ps1 .\te.wtl .\testResults.xml %testnameprefix%
 
 type testResults.xml
