543b19995f66790ea14db063980907f61194b40a
Luke Longley <llongley@microsoft.com>
2019-05-17 15:32:55 -0700

    Revert "Also copying other stuff we need."
    This reverts commit e3063e2ee85fc6f0ffe6dbe9b6f6fc54e083caf8.

--- build/CopyFilesToStagingDir.ps1
+++ build/CopyFilesToStagingDir.ps1
@@ -37,7 +37,7 @@ PublishFile -IfExists $FullBuildOutput\Microsoft.UI.Xaml\Generic.xaml $FullPubli
 PublishFile -IfExists $FullBuildOutput\Microsoft.UI.Xaml.Design\Microsoft.UI.Xaml.Design.dll $FullPublishDir\Microsoft.UI.Xaml.Design\
 PublishFile -IfExists $BuildOutputDir\$Configuration\AnyCPU\MUXControls.Test.TAEF\MUXControls.Test.dll $FullPublishDir\Test\
 PublishFile -IfExists $BuildOutputDir\$Configuration\AnyCPU\MUXControls.ReleaseTest.TAEF\MUXControls.ReleaseTest.dll $FullPublishDir\Test\
-PublishFile -IfExists $BuildOutputDir\$Configuration\AnyCPU\DummyUnitTestProject\*.* $FullPublishDir\Test\
+PublishFile -IfExists $BuildOutputDir\$Configuration\AnyCPU\DummyUnitTestProject\DummyUnitTestProject.dll $FullPublishDir\Test\
 PublishFile -IfExists $FullBuildOutput\FrameworkPackage\*.* $FullPublishDir\FrameworkPackage
 
 # Publish pdbs:
