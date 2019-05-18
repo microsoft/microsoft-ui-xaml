1990eaa8a3533ea38cd2e0b37f93fc457b38f7e5
Luke Longley <llongley@microsoft.com>
2019-05-17 21:38:05 -0700

    Adding back the three dummy tests to ensure we have an unreliable test in the mix.

---
+++ test/MUXControls.Test/TAEF/DummyTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if USING_TAEF
+using WEX.TestExecution;
+using WEX.TestExecution.Markup;
+using WEX.Logging.Interop;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+#endif
+
+namespace MUXControls.Test
+{
+    [TestClass]
+    public class DummyTests
+    {
+        [TestMethod]
+        public void PassingTest()
+        {
+            Verify.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void FailingTest()
+        {
+            Verify.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void UnreliableTest()
+        {
+            Verify.IsTrue(new Random().Next() % 3 == 0);
+        }
+    }
+}
