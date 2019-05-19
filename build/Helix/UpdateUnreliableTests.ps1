a364738327eb885aaec26680b699d4c85cef5d23
Luke Longley <llongley@microsoft.com>
2019-05-19 03:46:45 -0700

    Cleaned up UpdateUnreliableTests.ps1 in order to have it not do so much debug spew now that it's working properly.

--- build/Helix/UpdateUnreliableTests.ps1
+++ build/Helix/UpdateUnreliableTests.ps1
@@ -4,48 +4,48 @@ $azureDevOpsRestApiHeaders = @{
     "Accept"="application/json"
     "Authorization"="Basic $([System.Convert]::ToBase64String([System.Text.ASCIIEncoding]::ASCII.GetBytes(":$($env:SYSTEM_ACCESSTOKEN)")))"
 }
-      
-Write-Host "Invoke-RestMethod -Uri `"https://dev.azure.com/ms/microsoft-ui-xaml/_apis/test/runs?buildUri=$($env:BUILD_BUILDURI)`" -Method Get -Headers $azureDevOpsRestApiHeaders"
-Invoke-RestMethod -Uri "https://dev.azure.com/ms/microsoft-ui-xaml/_apis/test/runs?buildUri=$($env:BUILD_BUILDURI)" -Method Get -Headers $azureDevOpsRestApiHeaders
+
+# To account for unreliable tests, we'll iterate through all of the tests associated with this build, check to see any tests that were unreliable
+# (denoted by being marked as "skipped"), and if so, we'll instead mark those tests with a warning and enumerate all of the attempted runs
+# with their pass/fail states as well as any relevant error messages for failed attempts.
 $testRuns = Invoke-RestMethod -Uri "https://dev.azure.com/ms/microsoft-ui-xaml/_apis/test/runs?buildUri=$($env:BUILD_BUILDURI)" -Method Get -Headers $azureDevOpsRestApiHeaders
       
 foreach ($testRun in $testRuns.value)
 {
     $testRunResultsUri = "$($testRun.url)/results?api-version=5.0"
         
-    Write-Host "Marking test as in progress, so we can change its values if need be."
-    Write-Host "Invoke-RestMethod -Uri `"$($testRun.url)?api-version=5.0`" -Method Patch -Body (ConvertTo-Json @{ `"state`" = `"InProgress`" }) -Headers $azureDevOpsRestApiHeaders -ContentType `"application/json`""
+    Write-Host "Marking test run `"$($testRun.name)`" as in progress so we can change its results to account for unreliable tests."
     Invoke-RestMethod -Uri "$($testRun.url)?api-version=5.0" -Method Patch -Body (ConvertTo-Json @{ "state" = "InProgress" }) -Headers $azureDevOpsRestApiHeaders -ContentType "application/json"
-        
-    Write-Host "Invoke-RestMethod -Uri `"$($testRun.url)/results?api-version=5.0`" -Method Get -Headers $azureDevOpsRestApiHeaders"
-    Invoke-RestMethod -Uri $testRunResultsUri -Method Get -Headers $azureDevOpsRestApiHeaders
+
+    Write-Host "Retrieving test results..."
     $testResults = Invoke-RestMethod -Uri "$($testRun.url)/results?api-version=5.0" -Method Get -Headers $azureDevOpsRestApiHeaders
         
     foreach ($testResult in $testResults.value)
     {
-        Write-Host "Test $($testResult.testCaseTitle) : $($testResult.outcome)"
-          
         if ($testResult.outcome -eq "NotExecuted")
         {
-            Write-Host "(Actually passed on re-run. Updating.)"
+            Write-Host "  Test $($testResult.testCaseTitle) was detected as unreliable. Updating..."
             
             $rerunResults = ConvertFrom-Json $testResult.errorMessage
             [System.Collections.Generic.List[System.Collections.Hashtable]]$rerunDataList = @()
-            $attemptCount = 0
+            $attemptCount = 1
             $totalDuration = 0
             
             foreach ($rerun in $rerunResults.results)
             {
                 $rerunData = @{
-                    "displayName" = "Attempt #$attemptCount - $($testResult.testCaseTitle)";
+                    "displayName" = "Attempt #$($attemptCount) - $($testResult.testCaseTitle)";
                     "durationInMs" = $rerun.duration;
                     "outcome" = $rerun.outcome;
                 }
                 
-                if ($attemptCount -gt 0)
+                if ($attemptCount -gt 1)
                 {
-                    $rerunData["sequenceId"] = $attemptCount
+                    # -1 to make this zero-indexed.
+                    $rerunData["sequenceId"] = $attemptCount - 1
                 }
+
+                Write-Host "    Attempt #$($attemptCount): $($rerun.outcome)"
                 
                 if ($rerun.outcome -ne "Passed")
                 {
@@ -71,21 +71,15 @@ $($rerunResults.errors[$rerun.errorIndex - 1])
                     $rerunData["errorMessage"] = $fullErrorMessage
                 }
                 
-                $attemptCount++
                 $totalDuration += $rerun.duration
-                
                 $rerunDataList.Add($rerunData)
             }
             
-            $updateBody = ConvertTo-Json @(@{ "id" = $testResult.id; "outcome" = "Warning"; durationInMs = $totalDuration; "subResults" = $rerunDataList; "resultGroupType" = "rerun" }) -Depth 5
-            Write-Host "Invoke-RestMethod -Uri $testRunResultsUri -Method Patch -Headers $azureDevOpsRestApiHeaders -Body $updateBody -ContentType `"application/json`""
-            Write-Host "`$updateBody:"
-            $updateBody
+            $updateBody = ConvertTo-Json @(@{ "id" = $testResult.id; "outcome" = "Warning"; "durationInMs" = $totalDuration; "subResults" = $rerunDataList; "resultGroupType" = "rerun" }) -Depth 5
             Invoke-RestMethod -Uri $testRunResultsUri -Method Patch -Headers $azureDevOpsRestApiHeaders -Body $updateBody -ContentType "application/json"
         }
     }
         
-    Write-Host "Marking test as completed again."
-    Write-Host "Invoke-RestMethod -Uri `"$($testRun.url)?api-version=5.0`" -Method Patch -Body (ConvertTo-Json @{ `"state`" = `"Completed`" }) -Headers $azureDevOpsRestApiHeaders -ContentType `"application/json`""
+    Write-Host "Finished updates. Re-marking test run `"$($testRun.name)`" as completed."
     Invoke-RestMethod -Uri "$($testRun.url)?api-version=5.0" -Method Patch -Body (ConvertTo-Json @{ "state" = "Completed" }) -Headers $azureDevOpsRestApiHeaders -ContentType "application/json"
 }
