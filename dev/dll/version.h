b963abad944e2b497eff925c387977a022e0d2df
Keith Mahoney <kmahone@microsoft.com>
2019-05-22 14:08:18 -0700

    Enable file version in build

---
+++ dev/dll/version.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
+
+
+#define VER_FILE_VERSION            MUX_VERSION_MAJOR, MUX_VERSION_MINOR, MUX_VERSION_BUILD, MUX_VERSION_REVISION
+#define VER_FILE_VERSION_STR        STRINGIZE(MUX_VERSION_MAJOR)        \
+                                    "." STRINGIZE(MUX_VERSION_MINOR)    \
+                                    "." STRINGIZE(MUX_VERSION_BUILD)    \
+                                    "." STRINGIZE(MUX_VERSION_REVISION) \
+
+#define VER_PRODUCT_VERSION         VER_FILE_VERSION
+#define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
+
