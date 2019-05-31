a118f3f4ac297667a2fe22d9dbdaeac1d9f74200
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2019-05-31 11:41:52 -0700

    Bring CalendarDatePicker, CheckBox, and ToolTip templates into WinUI (these need to be changed in order to support CornerRadius correctly) Also take some existing templates that were under CommonStyles and break into separate projects so there's a natural place to put test pages: * AutoSuggestBox * DatePicker * Slider * TimePicker Added test page entry points for all those controls.

---
+++ dev/CalendarDatePicker/TestUI/CalendarDatePickerPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace MUXControlsTestApp
+{
+    public sealed partial class CalendarDatePickerPage : TestPage
+    {
+        public CalendarDatePickerPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
