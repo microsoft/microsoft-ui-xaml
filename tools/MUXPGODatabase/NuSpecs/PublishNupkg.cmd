8d7671bd34161b722553f94f0d89bc8d25f5e787
Ranjesh Jaganathan <28935693+ranjeshj@users.noreply.github.com>
2019-07-03 13:20:22 -0700

    if nuget restored, optimize the build

--- tools/MUXPGODatabase/NuSpecs/PublishNupkg.cmd
+++ tools/MUXPGODatabase/NuSpecs/PublishNupkg.cmd
@@ -4,8 +4,9 @@ set ERRORLEVEL=
 
 for %%A IN (%~dp0MUXPGODatabase.??.??.*.nupkg) do (
     echo Candidate nuget package: %%A
-    echo %~dp0..\..\NugetWrapper.cmd push %%A -Source https://microsoft.pkgs.visualstudio.com/DefaultCollection/_packaging/DEPControls/nuget/v3/index.json -apikey VSTS
-    %~dp0..\..\NugetWrapper.cmd push %%A -Source https://microsoft.pkgs.visualstudio.com/DefaultCollection/_packaging/DEPControls/nuget/v3/index.json -apikey VSTS
+    echo %~dp0..\..\NugetWrapper.cmd push %%A -Source https://pkgs.dev.azure.com/ms/_packaging/MUX-CI/nuget/v3/index.json -apikey VSTS
+rem you need to get an authorized version of nuget.exe from https://dev.azure.com/ms/microsoft-ui-xaml/_packaging?_a=package&feed=MUX-CI&package=Microsoft.UI.Xaml&protocolType=NuGet&version=2.2.190702001-prerelease
+    "C:\Users\ranjeshj\Desktop\CredentialProviderBundle\NuGet.exe" push %%A -Source https://pkgs.dev.azure.com/ms/_packaging/MUX-CI/nuget/v3/index.json -apikey VSTS
 )
 
 :END
