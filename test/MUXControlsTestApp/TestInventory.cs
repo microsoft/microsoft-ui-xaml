0d625fbc19fc93a2cd4a621e7e67a26be3c5e51d
Ranjesh Jaganathan <28935693+ranjeshj@users.noreply.github.com>
2019-07-10 16:28:33 -0700

    some more updates

--- test/MUXControlsTestApp/TestInventory.cs
+++ test/MUXControlsTestApp/TestInventory.cs
@@ -34,63 +34,165 @@ namespace MUXControlsTestApp
         {
             Tests = new List<TestDeclaration>()
             {
-#if !BUILD_LEAN_MUX_FOR_THE_STORE_APP
+// #if FEATURE_SCROLLER_ENABLED && FEATURE_COLOR_PICKER_ENABLED && FEATURE_NAVIGATION_VIEW_ENABLED && FEATURE_RATING_CONTROL_ENABLED
+//              {"Leak", typeof(LeakTestPage)},
+//#endif
+
+#if FEATURE_COLOR_PICKER_ENABLED
                 {"ColorPicker", typeof(ColorPickerPage), "ColorPicker.png"},
-                {"Leak", typeof(LeakTestPage)},
+#endif
+
+#if FEATURE_PERSON_PICTURE_ENABLED
                 {"PersonPicture", typeof(PersonPicturePage), "PersonPicture.png"},
+#endif
+
+#if FEATURE_RATING_CONTROL_ENABLED
                 {"RatingControl", typeof(RatingControlPage), "RatingControl.png"},
+#endif
+
+#if FEATURE_SWIPE_CONTROL_ENABLED
                 {"SwipeControl", typeof(SwipePage), "Swipe.png"},
+#endif
+
+#if FEATURE_TREEVIEW_ENABLED
                 {"TreeView", typeof(TreeViewPage), "TreeView.png"},
+#endif
+
+#if FEATURE_TEACHING_TIP_ENABLED
                 {"TeachingTip", typeof(TeachingTipPage), "TeachingTip.png"},
+#endif
+
+#if FEATURE_TWO_PANE_VIEW_ENABLED
                 {"TwoPaneView", typeof(TwoPaneViewPage)},
+#endif
+
+#if FEATURE_PULL_TO_REFRESH_ENABLED
                 {"PTR", typeof(PTRPage), "PullToRefresh.png"},
+#endif
+
+#if FEATURE_MENUBAR_ENABLED
                 {"MenuBar", typeof(MenuBarPage), "MenuBar.png"},
+#endif
+
+#if FEATURE_MENU_FLYOUT_ENABLED
                 {"MenuFlyout", typeof(MenuFlyoutPage), "MenuFlyout.png"},
+#endif
+
+#if FEATURE_SCROLLVIEWER_ENABLED
                 {"ScrollViewerAdapter", typeof(ScrollViewerAdapterPage), "ScrollViewer.png"},
+#endif
+
+#if FEATURE_SPLIT_BUTTON_ENABLED
                 {"SplitButton", typeof(SplitButtonPage), "SplitButton.png"},
+#endif
+
+#if FEATURE_DROPDOWN_BUTTON_ENABLED
                 {"DropDownButton", typeof(DropDownButtonPage), "DropdownButton.png"},
+#endif
+
+#if FEATURE_COMMANDBAR_FLYOUT_ENABLED
                 {"CommandBarFlyout", typeof(CommandBarFlyoutMainPage), "CommandBarFlyout.png"},
+#endif
+
+#if FEATURE_COMMON_STYLES_ENABLED
                 {"CommonStyles", typeof(CommonStylesPage)},
                 {"Compact", typeof(CompactPage), "CompactSizing.png"},
+                {"CornerRadius", typeof(CornerRadiusPage)},
+#endif
+
+#if FEATURE_RADIOBUTTONS_ENABLED
                 {"RadioButtons", typeof(RadioButtonsPage), "RadioButton.png"},
+#endif
+
+#if FEATURE_RADIO_MENU_FLYOUT_ITEM_ENABLED
                 {"RadioMenuFlyoutItem", typeof(RadioMenuFlyoutItemPage)},
+#endif
+
+#if FEATURE_TABVIEW_ENABLED
                 {"TabView", typeof(TabViewPage)},
+#endif
+
+#if FEATURE_FLIPVIEW_ENABLED
                 {"FlipView", typeof(FlipViewPage), "FlipView.png"},
+#endif
+
+#if FEATURE_COMBOBOX_ENABLED
                 {"ComboBox", typeof(ComboBoxPage), "ComboBox.png"},
+#endif
+
+#if FEATURE_PIVOT_ENABLED
                 {"Pivot", typeof(PivotPage), "Pivot.png"},
+#endif
+
+#if FEATURE_SCROLL_BAR_ENABLED
                 {"ScrollBar", typeof(ScrollBarPage)},
 #endif
+
 
                 // These two depend on the type InteractionBase, which is behind the Velocity feature Feature_Xaml2018 in the OS repo.
                 // We can't compile them without attaching the same feature annotation, and MIDL doesn't let us attach feature attributes
                 // to non-public types.  So for now we'll just exclude these from the OS repo.
-#if (!BUILD_WINDOWS && !BUILD_LEAN_MUX_FOR_THE_STORE_APP)
+#if FEATURE_SCROLLVIEWER_ENABLED
                 {"ScrollViewer", typeof(ScrollViewerPage), "ScrollViewer.png"},
-#if (USE_INTERNAL_SDK)
+#endif
+
+#if FEATURE_INTERACTIONS_ENABLED
                 {"ButtonInteraction", typeof(ButtonInteractionPage), "Button.png"},
                 {"SliderInteraction", typeof(SliderInteractionPage), "Slider.png"},
 #endif
 
-#if (ANIMATED_VISUAL_PLAYER_INCLUDED)
+#if FEATURE_ANIMATED_VISUAL_PLAYER_ENABLED
                 {"AnimatedVisualPlayer", typeof(AnimatedVisualPlayerPage), "Animations.png"},
 #endif
-#endif
+
+#if FEATURE_NAVIGATION_VIEW_ENABLED
                 {"NavigationView", typeof(NavigationViewCaseBundle), "NavigationView.png"},
+#endif
+
+#if FEATURE_PARALLAX_VIEW_ENABLED
                 {"ParallaxView", typeof(ParallaxViewPage), "ParallaxView.png"},
+#endif
+
+#if FEATURE_MATERIALS_ENABLED
                 {"Acrylic", typeof(AcrylicPage), "AcrylicBrush.png"},
                 {"Reveal", typeof(RevealPage), "Reveal.png"},
-#if (REPEATER_INCLUDED)
+#endif
+
+#if FEATURE_REPEATER_ENABLED
                 {"ItemsRepeater", typeof(RepeaterTestUIPage), "ListView.png"},
 #endif
+
+#if FEATURE_SCROLLER_ENABLED
                 {"Scroller", typeof(ScrollerPage), "ScrollViewer.png"},
-                {"CornerRadius", typeof(CornerRadiusPage)},
+#endif
+
+#if FEATURE_AUTOSUGGESTBOX_ENABLED
                 {"AutoSuggestBox", typeof(AutoSuggestBoxPage), "AutoSuggestBox.png"},
+#endif
+
+#if FEATURE_CHECKBOX_ENABLED
                 {"CheckBox", typeof(CheckBoxPage), "CheckBox.png"},
+#endif
+
+#if FEATURE_CALENDAR_DATE_PICKER_ENABLED
                 {"CalendarDatePicker", typeof(CalendarDatePickerPage), "CalendarDatePicker.png"},
+#endif
+
+#if FEATURE_DATE_PICKER_ENABLED
                 {"DatePicker", typeof(DatePickerPage), "DatePicker.png"},
+#endif
+
+#if FEATURE_SLIDER_ENABLED
                 {"Slider", typeof(SliderPage), "Slider.png"},
+#endif
+
+#if FEATURE_TIME_PICKER_ENABLED
                 {"TimePicker", typeof(TimePickerPage), "TimePicker.png"},
+#endif
+
+#if FEATURE_TOOL_TIP_ENABLED
                 {"ToolTip", typeof(ToolTipPage), "ToolTip.png"},
+#endif
             };
 
             Tests.Sort((a, b) =>
