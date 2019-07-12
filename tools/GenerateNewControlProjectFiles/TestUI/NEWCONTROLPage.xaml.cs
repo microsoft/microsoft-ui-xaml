87c64ad75dd0ff654495269d2f92ad91d35a5c75
Ranjesh Jaganathan <28935693+ranjeshj@users.noreply.github.com>
2019-07-12 13:01:51 -0700

    update the GenerateNewControlProject scripts

--- tools/GenerateNewControlProjectFiles/TestUI/NEWCONTROLPage.xaml.cs
+++ tools/GenerateNewControlProjectFiles/TestUI/NEWCONTROLPage.xaml.cs
@@ -16,6 +16,7 @@ using NEWCONTROL = Microsoft.UI.Xaml.Controls.NEWCONTROL;
 
 namespace MUXControlsTestApp
 {
+    [AddToTestInventory(Name = "NEWCONTROL")]
     public sealed partial class NEWCONTROLPage : TestPage
     {
         public NEWCONTROLPage()
