fa3cd51f5b3030e8abb47ddb54b2833d55efd9e4
Adam Hammond <adam.gregory.hammond@gmail.com>
2019-07-16 12:13:20 -0700

    Added UserSwitcherViewModel definition and implementation

---
+++ FlickCpp/UserSwitcherViewModel.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include <UserSwitcherViewModel.h>
+
+using namespace FlickCpp;
+using namespace Platform;
+using namespace Platform::Collections;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Interop;
+
+UserSwitcherViewModel::UserSwitcherViewModel()
+{
+}
+
+UserSwitcherViewModel::~UserSwitcherViewModel()
+{
+}
+
+IVector<String^>^ UserSwitcherViewModel::Users::get()
+{
+    if (m_users == nullptr)
+    {
+        m_users = ref new Vector<String^>();
+        m_users->Append("Item 0");
+        /*m_users->Append("Item 1");
+        m_users->Append("Item 2");
+        m_users->Append("Item 3");
+        m_users->Append("Item 4");
+        m_users->Append("Item 5");*/
+    }
+
+    return m_users;
+}
+
+void UserSwitcherViewModel::Users::set(IVector<String^>^ value)
+{
+    m_users = value;
+    PropertyChanged(this, ref new PropertyChangedEventArgs(L"Data"));
+}
+
+String^ UserSwitcherViewModel::SelectedUser::get()
+{
+    return m_selectedUser;
+}
+
+void UserSwitcherViewModel::SelectedUser::set(String^ value)
+{
+    m_selectedUser = value;
+    PropertyChanged(this, ref new PropertyChangedEventArgs(L"SelectedUser"));
+}
---
+++ FlickCpp/UserSwitcherViewModel.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+namespace FlickCpp
+{
+    [Windows::UI::Xaml::Data::Bindable]
+    [Windows::Foundation::Metadata::WebHostHidden]
+    public ref class UserSwitcherViewModel sealed
+        : public Windows::UI::Xaml::Data::INotifyPropertyChanged
+    {
+    public:
+        UserSwitcherViewModel();
+        virtual ~UserSwitcherViewModel();
+
+        // INotifyPropertyChanged
+        virtual event Windows::UI::Xaml::Data::PropertyChangedEventHandler^ PropertyChanged;
+
+        property Windows::Foundation::Collections::IVector<Platform::String^>^ Users
+        {
+            Windows::Foundation::Collections::IVector<Platform::String^>^ get();
+            void set(Windows::Foundation::Collections::IVector<Platform::String^>^ value);
+        }
+
+        property Platform::String^ SelectedUser
+        {
+            Platform::String^ get();
+            void set(Platform::String^ value);
+        }
+
+    private:
+        Windows::Foundation::Collections::IVector<Platform::String^>^ m_users;
+        Platform::String^ m_selectedUser;
+    };
+}
