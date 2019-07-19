ef3307d5ed61f0cc062297a57a57f8eed3c02fde
Régis Brid <36547063+RBrid@users.noreply.github.com>
2019-07-19 15:23:30 -0700

    Scroller: Adding support for ContentOrientation.Both (#1072)
    * Adding blank ScrollViewer test page for convenience of temporary scenario testing.
    
    * ContentOrientation.Both - Part 1.
    
    * ContentOrientation.Both - Part 2.
    Fixing Scroller's missing call to SetupTransformExpressionAnimations when Content.RenderSize changed.
    
    * Fixing typos.
    
    * Fixed test fragility.

--- dev/ScrollViewer/TestUI/ScrollViewerPage.xaml.cs
+++ dev/ScrollViewer/TestUI/ScrollViewerPage.xaml.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Common;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Common;
 
 #if USING_TAEF
 using WEX.TestExecution;
@@ -14,10 +14,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
 #endif
 
-#if !BUILD_WINDOWS
 using MUXControlsTestHooks = Microsoft.UI.Private.Controls.MUXControlsTestHooks;
 using ScrollerTestHooks = Microsoft.UI.Private.Controls.ScrollerTestHooks;
-#endif
 
 namespace MUXControlsTestApp
 {
@@ -37,6 +35,7 @@ namespace MUXControlsTestApp
             navigateToScrollControllers.Click += delegate { Frame.NavigateWithoutAnimation(typeof(ScrollViewerWithScrollControllersPage), 0); };
             navigateToRTL.Click += delegate { Frame.NavigateWithoutAnimation(typeof(ScrollViewerWithRTLFlowDirectionPage), 0); };
             navigateToKeyboardAndGamepadNavigation.Click += delegate { Frame.NavigateWithoutAnimation(typeof(ScrollViewerKeyboardAndGamepadNavigationPage), 0); };
+            navigateToBlank.Click += delegate { Frame.NavigateWithoutAnimation(typeof(ScrollViewerBlankPage), 0); };
 
             chkIsInteractionTrackerPointerWheelRedirectionEnabled.IsChecked = ScrollerTestHooks.IsInteractionTrackerPointerWheelRedirectionEnabled;
         }
