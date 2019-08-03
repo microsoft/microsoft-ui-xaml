a79ccd82b5bb902b76fc561cf280e29d00e8fbf1
Kevin Taha <t-ketaha@microsoft.com>
2019-08-02 17:11:41 -0700

    Addressed various feedback issues, added StepFreq to test page

--- dev/NumberBox/TestUI/NumberBoxPage.xaml.cs
+++ dev/NumberBox/TestUI/NumberBoxPage.xaml.cs
@@ -18,7 +18,6 @@ namespace MUXControlsTestApp
     [TopLevelTestPage(Name = "NumberBox")]
     public sealed partial class NumberBoxPage : TestPage
     {
-        NumberBox box;
         public NumberBoxPage()
         {
             this.InitializeComponent();
@@ -26,65 +25,42 @@ namespace MUXControlsTestApp
 
         private void Hyperscroll_Checked(object sender, RoutedEventArgs e)
         {
-
-            box = FindVisualChildByName(this, "numBox") as NumberBox;
             CheckBox check = (CheckBox) sender;
             if ( (bool) check.IsChecked)
             {
-                box.HyperScrollEnabled = true;
+                numBox.HyperScrollEnabled = true;
             }
             else
             {
-                box.HyperScrollEnabled = false;
+                numBox.HyperScrollEnabled = false;
             }
         }
 
         private void SpinMode_Changed(object sender, RoutedEventArgs e)
         {
-            box = FindVisualChildByName(this, "numBox") as NumberBox;
             ComboBox spinmode = (ComboBox)sender;
-            if (box == null)
+            if (numBox == null)
             {
                 return;
             }
             if ((string) spinmode.SelectedValue == "Inline")
             {
-                box.SpinButtonPlacementMode = NumberBoxSpinButtonPlacementMode.Inline;
+                numBox.SpinButtonPlacementMode = NumberBoxSpinButtonPlacementMode.Inline;
             }
             else if ((string) spinmode.SelectedValue == "Hidden")
             {
-                box.SpinButtonPlacementMode = NumberBoxSpinButtonPlacementMode.Hidden;
+                numBox.SpinButtonPlacementMode = NumberBoxSpinButtonPlacementMode.Hidden;
             }
         }
 
         private void Bounds_Changed(object sender, RoutedEventArgs e)
         {
-            box = FindVisualChildByName(this, "numBox") as NumberBox;
-            ComboBox boundmode = (ComboBox)sender;
-            if (box == null)
+            if (numBox == null)
             {
                 return;
             }
-            if ((string)boundmode.SelectedValue == "None")
-            {
-                box.MinMaxMode = NumberBoxMinMaxMode.None;
-            }
-            else if ((string)boundmode.SelectedValue == "MinEnabled")
-            {
-                box.MinMaxMode = NumberBoxMinMaxMode.MinEnabled;
-            }
-            else if ((string)boundmode.SelectedValue == "MaxEnabled")
-            {
-                box.MinMaxMode = NumberBoxMinMaxMode.MaxEnabled;
-            }
-            else if ((string)boundmode.SelectedValue == "MinAndMaxEnabled")
-            {
-                box.MinMaxMode = NumberBoxMinMaxMode.MinAndMaxEnabled;
-            }
-            else if ((string)boundmode.SelectedValue == "WrapEnabled")
-            {
-                box.MinMaxMode = NumberBoxMinMaxMode.WrapEnabled;
-            }
+            ComboBox boundmode = (ComboBox)sender;
+            numBox.MinMaxMode = (NumberBoxMinMaxMode) Enum.Parse(typeof(NumberBoxMinMaxMode), boundmode.SelectedValue as string);
         }
     }
 }
