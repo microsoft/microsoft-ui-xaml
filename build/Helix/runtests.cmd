7781a09ebb1b46b74caed415d3c15b99a10fe008
Keith Mahoney <kmahone@microsoft.com>
2019-09-03 11:29:48 -0700

    collect crash dumps for muxcontrolstestapp.exe

--- build/Helix/runtests.cmd
+++ build/Helix/runtests.cmd
@@ -3,6 +3,8 @@ setlocal ENABLEDELAYEDEXPANSION
 robocopy %HELIX_CORRELATION_PAYLOAD% . /s /NP > NUL
 
 reg add HKLM\Software\Policies\Microsoft\Windows\Appx /v AllowAllTrustedApps /t REG_DWORD /d 1 /f
+reg add HKLM\Software\Microsoft\Windows\Windows Error Reporting\LocalDumps\MUXControlsTestApp.exe /v DumpFolder /t REG_EXPAND_SZ /d c:\cores /f
+reg add HKLM\Software\Microsoft\Windows\Windows Error Reporting\LocalDumps\MUXControlsTestApp.exe /v DumpType /t REG_DWORD /d 2 /f
 
 :: kill dhandler, which is a tool designed to handle unexpected windows appearing. But since our tests are 
 :: expected to show UI we don't want it running.
@@ -22,17 +24,10 @@ for %%B in (%testBinaryCandidates%) do (
 te %testBinaries% /enablewttlogging /unicodeOutput:false /sessionTimeout:0:15 /testtimeout:0:10 /screenCaptureOnError %*
 
 move te.wtl te_original.wtl
-%HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result te_original.wtl -result_name te_original.wtl
 
-FOR %%I in (WexLogFileOutput\*.jpg) DO (
-    echo Uploading %%I to "%HELIX_RESULTS_CONTAINER_URI%/%%I%HELIX_RESULTS_CONTAINER_RSAS%"
-    %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result %%I -result_name %%~nI%%~xI 
-)
-
-FOR %%I in (*.pgc) DO (
-    echo Uploading %%I to "%HELIX_RESULTS_CONTAINER_URI%/%%I%HELIX_RESULTS_CONTAINER_RSAS%"
-    %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result %%I -result_name %%~nI%%~xI 
-)
+copy te_original.wtl %HELIX_WORKITEM_UPLOAD_ROOT%
+copy WexLogFileOutput\*.jpg %HELIX_WORKITEM_UPLOAD_ROOT%
+copy *.pgc %HELIX_WORKITEM_UPLOAD_ROOT%
 
 set FailedTestQuery=
 for /F "tokens=* usebackq" %%I IN (`powershell -ExecutionPolicy Bypass .\OutputFailedTestQuery.ps1 te_original.wtl`) DO (
@@ -46,12 +41,9 @@ if "%FailedTestQuery%" == "" goto :SkipReruns
 te %testBinaries% /enablewttlogging /unicodeOutput:false /sessionTimeout:0:15 /testtimeout:0:10 /screenCaptureOnError /select:"%FailedTestQuery%"
 
 move te.wtl te_rerun.wtl
-%HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result te_rerun.wtl -result_name te_rerun.wtl
 
-FOR %%I in (WexLogFileOutput\*.jpg) DO (
-    echo Uploading %%I to "%HELIX_RESULTS_CONTAINER_URI%/%%~nI_rerun%%~xI%HELIX_RESULTS_CONTAINER_RSAS%"
-    %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result %%I -result_name %%~nI_rerun%%~xI
-)
+copy te_rerun.wtl %HELIX_WORKITEM_UPLOAD_ROOT%
+copy WexLogFileOutput\*.jpg %HELIX_WORKITEM_UPLOAD_ROOT%
 
 rem If there are still failing tests remaining, we'll run them eight more times, so they'll have been run a total of ten times.
 rem If any tests fail all ten times, we can be pretty confident that these are actual test failures rather than unreliable tests.
@@ -67,21 +59,15 @@ if "%FailedTestQuery%" == "" goto :SkipReruns
 te %testBinaries% /enablewttlogging /unicodeOutput:false /sessionTimeout:0:15 /testtimeout:0:10 /screenCaptureOnError /testmode:Loop /LoopTest:8 /select:"%FailedTestQuery%"
 
 move te.wtl te_rerun_multiple.wtl
-%HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result te_rerun_multiple.wtl -result_name te_rerun_multiple.wtl
 
-FOR %%I in (WexLogFileOutput\*.jpg) DO (
-    echo Uploading %%I to "%HELIX_RESULTS_CONTAINER_URI%/%%~nI_rerun_multiple%%~xI%HELIX_RESULTS_CONTAINER_RSAS%"
-    %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result %%I -result_name %%~nI_rerun_multiple%%~xI
-)
+copy te_rerun_multiple.wtl %HELIX_WORKITEM_UPLOAD_ROOT%
+copy WexLogFileOutput\*.jpg %HELIX_WORKITEM_UPLOAD_ROOT%
 
 :SkipReruns
 
 powershell -ExecutionPolicy Bypass .\OutputSubResultsJsonFiles.ps1 te_original.wtl te_rerun.wtl te_rerun_multiple.wtl %testnameprefix%
 powershell -ExecutionPolicy Bypass .\ConvertWttLogToXUnit.ps1 te_original.wtl te_rerun.wtl te_rerun_multiple.wtl testResults.xml %testnameprefix%
 
-FOR %%I in (*_subresults.json) DO (
-    echo Uploading %%I to "%HELIX_RESULTS_CONTAINER_URI%/%%I%HELIX_RESULTS_CONTAINER_RSAS%"
-    %HELIX_PYTHONPATH% %HELIX_SCRIPT_ROOT%\upload_result.py -result %%I -result_name %%I
-)
+copy *_subresults.json %HELIX_WORKITEM_UPLOAD_ROOT%
 
 type testResults.xml
