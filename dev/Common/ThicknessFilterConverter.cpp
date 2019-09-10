b10a695b0c9bc0e06c0a32ef2434f19c4ab5182a
Stephen Peters <stpete@microsoft.com>
2019-09-10 11:02:27 -0700

    Fix teaching tips adorners to be compatible with rounded corners.

---
+++ dev/Common/ThicknessFilterConverter.cpp
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include <pch.h>
+#include <common.h>
+#include "ThicknessFilterConverter.h"
+
+winrt::Thickness ThicknessFilterConverter::Convert(winrt::CornerRadius const& radius, winrt::ThicknessFilterConverterKind const& filterKind)
+{
+    auto result = winrt::Thickness{};
+
+    switch (filterKind)
+    {
+    case winrt::ThicknessFilterConverterKind::Top:
+        result.Left = radius.TopLeft;
+        result.Right = radius.TopRight;
+        result.Top = 0;
+        result.Bottom = 0;
+        break;
+    case winrt::ThicknessFilterConverterKind::Bottom:
+        result.Left = radius.BottomLeft;
+        result.Right = radius.BottomRight;
+        result.Top = 0;
+        result.Bottom = 0;
+        break;
+    case winrt::ThicknessFilterConverterKind::Left:
+        result.Left = 0;
+        result.Right = 0;
+        result.Top = radius.TopLeft;
+        result.Bottom = radius.BottomLeft;
+        break;
+    case winrt::ThicknessFilterConverterKind::Right:
+        result.Left = 0;
+        result.Right = 0;
+        result.Top = radius.TopRight;
+        result.Bottom = radius.BottomRight;
+        break;
+    }
+
+    return result;
+}
+
+winrt::IInspectable ThicknessFilterConverter::Convert(
+    winrt::IInspectable const& value,
+    winrt::TypeName const& targetType,
+    winrt::IInspectable const& parameter,
+    winrt::hstring const& language)
+{
+    auto radius = unbox_value<winrt::CornerRadius>(value);
+
+    return box_value(Convert(radius, Filter()));
+}
+
+winrt::IInspectable ThicknessFilterConverter::ConvertBack(
+    winrt::IInspectable const& value,
+    winrt::TypeName const& targetType,
+    winrt::IInspectable const& parameter,
+    winrt::hstring const& language)
+{
+    winrt::throw_hresult(E_NOTIMPL);
+}
---
+++ dev/Common/ThicknessFilterConverter.h
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+#include "ThicknessFilterConverter.g.h"
+#include "ThicknessFilterConverter.properties.h"
+
+class ThicknessFilterConverter :
+    public winrt::implementation::ThicknessFilterConverterT<ThicknessFilterConverter>,
+    public ThicknessFilterConverterProperties
+{
+public:
+    winrt::Thickness Convert(
+        winrt::CornerRadius const& radius,
+        winrt::ThicknessFilterConverterKind const& filterKind);
+
+    winrt::IInspectable Convert(
+        winrt::IInspectable const& value,
+        winrt::TypeName const& targetType,
+        winrt::IInspectable const& parameter,
+        winrt::hstring const& language);
+
+    winrt::IInspectable ConvertBack(
+        winrt::IInspectable const& value,
+        winrt::TypeName const& targetType,
+        winrt::IInspectable const& parameter,
+        winrt::hstring const& language);
+};
