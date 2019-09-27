00af6de216b44ae198c708ce1a331fbdf5f9a23d
Marcel Wagner <marcel.alex.wagner@outlook.com>
2019-09-28 01:57:42 +0200

    Add documentation for ExtendedObservableCollectopn

--- test/MUXControlsTestApp/Utilities/ExtendedObservableCollection.cs
+++ test/MUXControlsTestApp/Utilities/ExtendedObservableCollection.cs
@@ -7,8 +7,19 @@ using System.Collections.Specialized;
 
 namespace MUXControlsTestApp.Utilities
 {
+    /// <summary>
+    /// This class aims to add functionality that is present in other collections that can be used as ItemSource for controls,
+    /// e.g. ReplaceAll of the winrt::IVector.
+    /// See https://github.com/microsoft/microsoft-ui-xaml/issues/1379 for additional context as to why this is needed for easy testing.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     class ExtendedObservableCollection<T> : ObservableCollection<T>
     {
+        /// <summary>
+        /// Removes all items of this collection and replaces them with the collection specified.
+        /// Triggers the "Reset" action.
+        /// </summary>
+        /// <param name="items">Collection containing the items that will be replacing the previous content</param>
         public void ReplaceAll(ICollection<T> items)
         {
             this.Items.Clear();
