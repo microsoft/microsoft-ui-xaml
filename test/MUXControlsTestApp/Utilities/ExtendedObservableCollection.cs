62f9c949f02a3b3b0011ffcba3ac368d2a40245e
Marcel Wagner <marcel.alex.wagner@outlook.com>
2019-09-28 01:30:20 +0200

    Add missing copyright header

--- test/MUXControlsTestApp/Utilities/ExtendedObservableCollection.cs
+++ test/MUXControlsTestApp/Utilities/ExtendedObservableCollection.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.Text;
 
 namespace MUXControlsTestApp.Utilities
 {
--- test/TestAppCX/TreeViewTestPage.xaml.cpp
+++ test/TestAppCX/TreeViewTestPage.xaml.cpp
@@ -1,4 +1,7 @@
-﻿#include "pch.h"
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
 #include "TreeViewTestPage.xaml.h"
 
 using namespace TestAppCX;
