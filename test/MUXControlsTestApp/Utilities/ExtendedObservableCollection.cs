e0cc03aa79b8cc6ffbf31e395dfaaa07ceff2963
Marcel Wagner <marcel.alex.wagner@outlook.com>
2019-09-28 01:24:54 +0200

    Add test for bug #1379

---
+++ test/MUXControlsTestApp/Utilities/ExtendedObservableCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace MUXControlsTestApp.Utilities
+{
+    class ExtendedObservableCollection<T> : ObservableCollection<T>
+    {
+        public void ReplaceAll(ICollection<T> items)
+        {
+            this.Items.Clear();
+            foreach(T item in items)
+            {
+                this.Items.Add(item);
+            }
+            this.OnCollectionChanged(
+                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset)
+            );
+        }
+
+    }
+}
