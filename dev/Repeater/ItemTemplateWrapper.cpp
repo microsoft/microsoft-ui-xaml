7d05d07cc5f7bd04c023bedad2caaeee09a09869
Marcel Wagner <marcel.alex.wagner@outlook.com>
2019-09-30 22:40:47 +0200

    Refactor rectangle creation

--- dev/Repeater/ItemTemplateWrapper.cpp
+++ dev/Repeater/ItemTemplateWrapper.cpp
@@ -57,9 +57,10 @@ winrt::UIElement ItemTemplateWrapper::GetElement(winrt::ElementFactoryGetArgs co
 
         // Template returned null, so insert empty element to render nothing
         if (!element) {
-            element = winrt::Rectangle();
-            element.as<winrt::Rectangle>().Width(0);
-            element.as<winrt::Rectangle>().Height(0);
+            auto rectangle = winrt::Rectangle();
+            rectangle.Width(0);
+            rectangle.Height(0);
+            element = rectangle;
         }
 
         // Associate template with element
