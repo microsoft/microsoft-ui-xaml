41b010d918cebdbdf4f461d1f6d593ed094940b6
Luke Longley <18177025+llongley@users.noreply.github.com>
2019-09-30 16:52:58 -0700

    CR feedback.

--- test/MUXControls.Test/MSTest/BuildAppX.cmd
+++ test/MUXControls.Test/MSTest/BuildAppX.cmd
@@ -1,12 +1,9 @@
 @echo off
+setlocal
 
-if "%DevEnvDir%" == "" (
-    if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise" (
-        call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
-    ) else (
-        call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
-    )
-)
+"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -Latest -requires Microsoft.Component.MSBuild -property InstallationPath > %TEMP%\vsinstalldir.txt
+set /p _VsInstallDir=<%TEMP%\vsinstalldir.txt
+call "%_VsInstallDir%\Common7\Tools\VsDevCmd.bat"
 
 set Command=MSBuild.exe %1 /p:platform="%2" /p:configuration="%3" /p:VisualStudioVersion="%VisualStudioVersion%" /flp:Verbosity=Diagnostic /fl /verbosity:Minimal
 
