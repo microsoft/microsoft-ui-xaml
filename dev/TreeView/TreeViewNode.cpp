880dd229078ab8e68263c29a43f9f6580b10195f
Marcel Wagner <marcel.alex.wagner@outlook.com>
2019-10-01 03:05:11 +0200

    Fix bug with TreeView not updating nodes correctly when collection triggers reset event (#1387)
    * Add TreeView test page in cx app
    
    * Fix bug with IVector.ReplaceAll not working on TreeView correctly
    
    * Switch to better solution on fixing treeview issue
    
    * Add test for bug #1379
    
    * Add missing copyright header
    
    * Add documentation for ExtendedObservableCollectopn

--- dev/TreeView/TreeViewNode.cpp
+++ dev/TreeView/TreeViewNode.cpp
@@ -194,10 +194,7 @@ void TreeViewNode::OnItemsSourceChanged(const winrt::IInspectable& sender, const
 
     case winrt::NotifyCollectionChangedAction::Reset:
     {
-        if (Children().Size() > 0)
-        {
-            Children().Clear();
-        }
+        SyncChildrenNodesWithItemsSource();
         break;
     }
 
---
+++ test/TestAppCX/TreeViewTestPage.xaml.h
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "TreeViewTestPage.g.h"
+
+namespace TestAppCX
+{
+    public ref class TreeViewData sealed
+    {
+    private:
+        Platform::String^ m_content;
+        Windows::Foundation::Collections::IObservableVector<TreeViewData^>^ m_children;
+
+    public:
+        TreeViewData(Platform::String^ content) : m_content(content)
+        {
+            m_content = content;
+            m_children = ref new Platform::Collections::Vector<TreeViewData^>();
+        }
+
+        property Platform::String^ Content
+        {
+            Platform::String^ get() { return m_content; }
+        }
+
+        property Windows::Foundation::Collections::IObservableVector<TreeViewData^>^ Children
+        {
+            Windows::Foundation::Collections::IObservableVector<TreeViewData^>^ get() { return m_children; }
+        }
+
+    };
+
+    [Windows::Foundation::Metadata::WebHostHidden]
+    public ref class TreeViewTestPage sealed
+    {
+
+    private:
+        Windows::Foundation::Collections::IObservableVector<TreeViewData^>^ m_items = ref new Platform::Collections::Vector<TreeViewData^>();
+
+    public:
+        property Windows::Foundation::Collections::IObservableVector<TreeViewData^>^ Items
+        {
+            Windows::Foundation::Collections::IObservableVector<TreeViewData^>^ get() { return m_items; }
+        }
+        TreeViewTestPage();
+    private:
+        void ReplaceAll_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Clear_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+
+}
