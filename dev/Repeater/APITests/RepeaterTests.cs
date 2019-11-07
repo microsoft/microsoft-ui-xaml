32490367df2d23089686b2b38dc73d14a9036db0
Marcel Wagner <marcel.alex.wagner@outlook.com>
2019-11-07 09:30:39 +0100

    Remove UIA name

--- dev/Repeater/APITests/RepeaterTests.cs
+++ dev/Repeater/APITests/RepeaterTests.cs
@@ -248,7 +248,7 @@ namespace Windows.UI.Xaml.Tests.MUXControls.ApiTests.RepeaterTests
 
                 var template = (DataTemplate)XamlReader.Load(
                         @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'> 
-                            <Button AutomationProperties.Name='{Binding}' Content='{Binding}'/>
+                            <Button Content='{Binding}'/>
                         </DataTemplate>");
 
                 repeater = new ItemsRepeater() {
