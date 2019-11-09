af949b42318f8674298e234ce2dbdf70a3cf1155
Jevan Saks <jevansa@microsoft.com>
2019-11-09 00:20:49 -0800

    Revert "make pri instead of compact.xaml for framework package" (#1578)
    * Revert "Arm64 min version to rs3 (#1574)"
    
    This reverts commit 7168c5d02b9eecd57f19dfafb949fcc8f12e9c2b.
    
    * Revert "make pri instead of compact.xaml for framework package (#1559)"
    
    This reverts commit b7b603e9fc61befdc3b7fea0e724be40007c59e9.

--- build/CopyFilesToStagingDir.ps1
+++ build/CopyFilesToStagingDir.ps1
@@ -36,7 +36,6 @@ PublishFile -IfExists $FullBuildOutput\Microsoft.UI.Xaml\Microsoft.UI.Xaml.pri $
 PublishFile -IfExists $FullBuildOutput\Microsoft.UI.Xaml\sdk\Microsoft.UI.Xaml.winmd $FullPublishDir\Microsoft.UI.Xaml\sdk\
 PublishFile -IfExists $FullBuildOutput\Microsoft.UI.Xaml\Generic.xaml $FullPublishDir\Microsoft.UI.Xaml\
 PublishFile -IfExists $FullBuildOutput\Microsoft.UI.Xaml.Design\Microsoft.UI.Xaml.Design.dll $FullPublishDir\Microsoft.UI.Xaml.Design\
-PublishFile -IfExists $FullBuildOutput\Microsoft.UI.Xaml.FrameworkPackagePRI\Microsoft.UI.Xaml.pri $FullPublishDir\Microsoft.UI.Xaml.FrameworkPackagePRI\
 PublishFile -IfExists $BuildOutputDir\$Configuration\AnyCPU\MUXControls.Test.TAEF\MUXControls.Test.dll $FullPublishDir\Test\
 PublishFile -IfExists $BuildOutputDir\$Configuration\AnyCPU\MUXControls.ReleaseTest.TAEF\MUXControls.ReleaseTest.dll $FullPublishDir\Test\
 
