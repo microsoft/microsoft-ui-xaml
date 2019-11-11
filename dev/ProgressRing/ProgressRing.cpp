370a8d03b563dff366cee3a121efd60d53ae8ff8
Karen Lai <7976322+karenbtlai@users.noreply.github.com>
2019-11-11 11:43:03 -0800

    render ellipse based on width

--- dev/ProgressRing/ProgressRing.cpp
+++ dev/ProgressRing/ProgressRing.cpp
@@ -120,6 +120,16 @@ void ProgressRing::UpdateRing()
     UpdateSegment();
 }
 
+winrt::Windows::Foundation::Size ProgressRing::ComputeEllipseSize(const double thickness)
+{
+    const double safeThickness = std::max(thickness, static_cast<double>(0.0));
+    const double actualWidth = ProgressRing::Width();
+    const double width = std::max((ProgressRing::Width() - safeThickness) / 2.0, 0.0);
+    const double height = std::max((ProgressRing::Height() - safeThickness) / 2.0, 0.0);
+
+    return winrt::Windows::Foundation::Size(static_cast<float>(width), static_cast<float>(height));
+}
+
 void ProgressRing::RenderAll()
 {
     if (auto&& progressRing = m_layoutRoot.get())
@@ -127,8 +137,14 @@ void ProgressRing::RenderAll()
         auto&& outlineFigure = m_outlineFigure.get();
         auto&& outlineArc = m_outlineArc.get();
 
-        outlineFigure.StartPoint(winrt::Windows::Foundation::Point(0,100));
-        outlineArc.Size(winrt::Windows::Foundation::Size(300, 50));
-        outlineArc.Point(winrt::Windows::Foundation::Point(200, 100));
+        const double thickness = ProgressRing::BorderThickness().Top;
+        const auto size = ComputeEllipseSize(thickness);
+
+        const float segmentWidth = size.Width;
+        const float translationFactor = static_cast<float>(std::max(thickness / 2.0, 0.0));
+
+        outlineFigure.StartPoint(winrt::Windows::Foundation::Point(segmentWidth + translationFactor, translationFactor));
+        outlineArc.Size(winrt::Windows::Foundation::Size(segmentWidth, size.Height));
+        outlineArc.Point(winrt::Windows::Foundation::Point(segmentWidth + translationFactor - 0.05f, translationFactor));
     }
 }
