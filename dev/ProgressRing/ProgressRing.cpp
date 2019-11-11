e8dd755cd622d0de5883f1653e9a734545b8e7b0
Karen Lai <7976322+karenbtlai@users.noreply.github.com>
2019-11-11 10:26:40 -0800

    render static ring

--- dev/ProgressRing/ProgressRing.cpp
+++ dev/ProgressRing/ProgressRing.cpp
@@ -19,6 +19,12 @@ void ProgressRing::OnApplyTemplate()
     winrt::IControlProtected controlProtected{ *this };
 
     // TODO: Implement
+
+    m_layoutRoot.set(GetTemplateChildT<winrt::Grid>(s_LayoutRootName, controlProtected));
+    m_outlineFigure.set(GetTemplateChildT<winrt::Windows::UI::Xaml::Media::PathFigure>(s_OutlineFigureName, controlProtected));
+    m_outlineArc.set(GetTemplateChildT<winrt::Windows::UI::Xaml::Media::ArcSegment>(s_OutlineArcName, controlProtected));
+
+    RenderAll();
 }
 
 void  ProgressRing::OnPropertyChanged(const winrt::DependencyPropertyChangedEventArgs& args)
@@ -26,4 +32,17 @@ void  ProgressRing::OnPropertyChanged(const winrt::DependencyPropertyChangedEven
     winrt::IDependencyProperty property = args.Property();
     
     // TODO: Implement
+}
+
+void ProgressRing::RenderAll()
+{
+    if (auto&& progressRing = m_layoutRoot.get())
+    {
+        auto&& outlineFigure = m_outlineFigure.get();
+        auto&& outlineArc = m_outlineArc.get();
+
+        outlineFigure.StartPoint(winrt::Windows::Foundation::Point(0,100));
+        outlineArc.Size(winrt::Windows::Foundation::Size(300, 50));
+        outlineArc.Point(winrt::Windows::Foundation::Point(200, 100));
+    }
 }
--- dev/ProgressRing/ProgressRing.h
+++ dev/ProgressRing/ProgressRing.h
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #pragma once
@@ -22,4 +22,19 @@ public:
     void OnApplyTemplate();
 
     void OnPropertyChanged(const winrt::DependencyPropertyChangedEventArgs& args);
+
+private:
+    void RenderAll();
+
+    winrt::Grid::Loaded_revoker m_layoutRootLoadedRevoker{};
+    winrt::Path::Loaded_revoker m_outlineFigureRevoker{};
+    winrt::Path::Loaded_revoker m_outlineArcRevoker{};
+
+    tracker_ref<winrt::Grid> m_layoutRoot{ this };
+    tracker_ref<winrt::Windows::UI::Xaml::Media::PathFigure> m_outlineFigure{ this };
+    tracker_ref<winrt::Windows::UI::Xaml::Media::ArcSegment> m_outlineArc{ this };
+
+    static constexpr wstring_view s_LayoutRootName{ L"LayoutRoot" };
+    static constexpr wstring_view s_OutlineFigureName{ L"OutlineFigurePart" };
+    static constexpr wstring_view s_OutlineArcName{ L"OutlineArcPart" };
 };
