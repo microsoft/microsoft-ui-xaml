bc82718fea071bb65e99ffa391cc199af59987c6
Karen Lai <7976322+karenbtlai@users.noreply.github.com>
2019-11-11 10:26:40 -0800

    render static ring

--- dev/ProgressRing/ProgressRing.h
+++ dev/ProgressRing/ProgressRing.h
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-=======
 // Copyright (c) Microsoft Corporation. All rights reserved.
->>>>>>> initial commit
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #pragma once
@@ -26,7 +22,7 @@ public:
     void OnApplyTemplate();
 
     void OnPropertyChanged(const winrt::DependencyPropertyChangedEventArgs& args);
-<<<<<<< HEAD
+
     void OnStrokeThicknessPropertyChanged(const winrt::DependencyPropertyChangedEventArgs& args);
 
 private:
@@ -55,6 +51,5 @@ private:
     static constexpr wstring_view s_OutlineArcName{ L"OutlineArcPart" };
     static constexpr wstring_view s_BarFigureName{ L"BarFigurePart" };
     static constexpr wstring_view s_BarArcName{ L"BarArcPart" };
-=======
->>>>>>> initial commit
+
 };
