0f0a3f3580aa5bf7f14b9631ce9b04a1a5d5c6bf
Union Palenshus <unionp@microsoft.com>
2019-11-14 18:27:25 -0800

    PR feedback

--- dev/Telemetry/MicrosoftTelemetry.h
+++ dev/Telemetry/MicrosoftTelemetry.h
@@ -1,6 +1,7 @@
 ﻿/* ++
 
-Copyright (c) Microsoft Corporation.  All rights reserved.
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License. See LICENSE in the project root for license information.
 
 Module Name:
 
