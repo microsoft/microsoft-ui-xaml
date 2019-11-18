30c3ced675c1c778116abdab64fb595c8f512cd7
Dilip Ojha <dilip.ojha@microsoft.com>
2019-11-17 17:57:57 -0800

    update and bug fix
    - more automation peer functionality
    - edge case fix for when user sets SelectedItem in ItemInvoked callback
    - removed RS4 testing

--- dev/NavigationView/NavigationViewItemAutomationPeer.h
+++ dev/NavigationView/NavigationViewItemAutomationPeer.h
@@ -51,4 +51,5 @@ private:
     int32_t GetNavigationViewItemCountInTopNav();
     int32_t GetPositionOrSetCountInLeftNavHelper(AutomationOutput automationOutput);
     int32_t GetPositionOrSetCountInTopNavHelper(winrt::IVector<winrt::IInspectable> navigationViewElements, AutomationOutput automationOutput);
+    void ChangeSelection(bool isSelected);
 };
