9b58b5c26d567c79d7daf37e7b6682fce6313e1e
Stephen Peters <stpete@microsoft.com>
2019-11-27 16:16:57 -0800

    Add a visual verification test

---
+++ dev/NumberBox/APITests/NumberBoxTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Common;
+using MUXControlsTestApp.Utilities;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using System.Linq;
+using System.Collections.Generic;
+using MUXControlsTestApp;
+using Microsoft.UI.Xaml.Controls;
+
+#if USING_TAEF
+using WEX.TestExecution;
+using WEX.TestExecution.Markup;
+using WEX.Logging.Interop;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+#endif
+
+namespace Windows.UI.Xaml.Tests.MUXControls.ApiTests
+{
+    [TestClass]
+    public class NumberBoxTests
+    {
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestUtilities.ClearVisualTreeRoot();
+        }
+
+        [TestMethod]
+        public void VerifyVisualTree()
+        {
+            NumberBox numberBox = null;
+            RunOnUIThread.Execute(() =>
+            {
+                numberBox = new NumberBox();
+            });
+            TestUtilities.SetAsVisualTreeRoot(numberBox);
+            Verify.IsNotNull(numberBox);
+            VisualTreeTestHelper.VerifyVisualTree(root: numberBox, masterFilePrefix: "NumberBoxHiddenSpinButtons");
+
+            RunOnUIThread.Execute(() =>
+            {
+                numberBox.SpinButtonPlacementMode = NumberBoxSpinButtonPlacementMode.Compact;
+            });
+            VisualTreeTestHelper.VerifyVisualTree(root: numberBox, masterFilePrefix: "NumberBoxCompactSpinButtons");
+
+            RunOnUIThread.Execute(() =>
+            {
+                numberBox.SpinButtonPlacementMode = NumberBoxSpinButtonPlacementMode.Inline;
+            });
+            VisualTreeTestHelper.VerifyVisualTree(root: numberBox, masterFilePrefix: "NumberBoxInlineSpinButtons");
+        }
+    }
+}
