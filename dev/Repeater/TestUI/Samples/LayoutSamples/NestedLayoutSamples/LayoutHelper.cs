0c8bfd3201c2052378e533b34ac125c65c3ca06c
Ranjesh Jaganathan <28935693+ranjeshj@users.noreply.github.com>
2020-01-13 16:39:06 -0800

    Moving around ItemsRepeater sampes to organize them better in anticipation of adding more samples.

---
+++ dev/Repeater/TestUI/Samples/LayoutSamples/NestedLayoutSamples/LayoutHelper.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using Windows.UI.Xaml.Controls;
+
+using Layout = Microsoft.UI.Xaml.Controls.Layout;
+using FlowLayout = Microsoft.UI.Xaml.Controls.FlowLayout;
+using FlowLayoutLineAlignment = Microsoft.UI.Xaml.Controls.FlowLayoutLineAlignment;
+using UniformGridLayout = Microsoft.UI.Xaml.Controls.UniformGridLayout;
+using UniformGridLayoutItemsJustification = Microsoft.UI.Xaml.Controls.UniformGridLayoutItemsJustification;
+using UniformGridLayoutItemsStretch = Microsoft.UI.Xaml.Controls.UniformGridLayoutItemsStretch;
+using StackLayout = Microsoft.UI.Xaml.Controls.StackLayout;
+
+namespace MUXControlsTestApp.Samples
+{
+    public static class LayoutHelper
+    {
+        public static void SetMaxRowsOrColumns(Layout layout, int value)
+        {
+            if (layout is UniformGridLayout)
+            {
+                ((UniformGridLayout)layout).MaximumRowsOrColumns = value;
+            }
+        }
+
+        public static void SetMinRowSpacing(Layout layout, double value)
+        {
+            if (layout is UniformGridLayout)
+            {
+                ((UniformGridLayout)layout).MinRowSpacing = value;
+            }
+            else if (layout is FlowLayout)
+            {
+                ((FlowLayout)layout).MinRowSpacing = value;
+            }
+            else if (layout is StackLayout)
+            {
+                // no-op
+            }
+        }
+
+        public static void SetMinColumnSpacing(Layout layout, double value)
+        {
+            if (layout is UniformGridLayout)
+            {
+                ((UniformGridLayout)layout).MinColumnSpacing = value;
+            }
+            else if (layout is FlowLayout)
+            {
+                ((FlowLayout)layout).MinColumnSpacing = value;
+            }
+            else if (layout is StackLayout)
+            {
+                ((StackLayout)layout).Spacing = value;
+            }
+        }
+
+        public static void SetLineAlignment(Layout layout, string value)
+        {
+            if (layout is UniformGridLayout)
+            {
+                var alignment = (UniformGridLayoutItemsJustification)Enum.Parse(typeof(UniformGridLayoutItemsJustification), value);
+                ((UniformGridLayout)layout).ItemsJustification = alignment;
+            }
+            else if (layout is FlowLayout)
+            {
+                var alignment = (FlowLayoutLineAlignment)Enum.Parse(typeof(FlowLayoutLineAlignment), value);
+                ((FlowLayout)layout).LineAlignment = alignment;
+            }
+            else if (layout is StackLayout)
+            {
+                // no-op
+            }
+        }
+
+        public static void SetItemsStretch(Layout layout, string value)
+        {
+            if (layout is UniformGridLayout)
+            {
+                var stretch = (UniformGridLayoutItemsStretch)Enum.Parse(typeof(UniformGridLayoutItemsStretch), value);
+                ((UniformGridLayout)layout).ItemsStretch = stretch;
+            }
+            else
+            {
+                // no-op
+            }
+        }
+    }
+}
---
+++ dev/Repeater/TestUI/Samples/LayoutSamples/NestedLayoutSamples/StoreMockData.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.ObjectModel;
+
+namespace MUXControlsTestApp.Samples
+{
+    public class StoreMockData
+    {
+        public static ObservableCollection<Category> Create(int numCategories, int numItemsPerCategory)
+        {
+            var data = new ObservableCollection<Category>();
+            for (int i = 0; i < numCategories; i++)
+            {
+                var category = CreateCategory(numItemsPerCategory, "Category " + i.ToString());
+                data.Add(category);
+            }
+
+            return data;
+        }
+
+        public static Category CreateCategory(int numItems, string name)
+        {
+            var category = new Category() { Name = name };
+            for (int i = 0; i < numItems; i++)
+            {
+                category.Add(new Item() { Name = "Item " + i.ToString() });
+            }
+
+            return category;
+        }
+    }
+
+    public class Category : ObservableCollection<Item>
+    {
+        public string Name { get; set; }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+    }
+}
