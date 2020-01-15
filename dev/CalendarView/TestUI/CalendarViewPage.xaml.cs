e8b2bceb9a545b7c80f5854f24f07b06e0c0dd91
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-01-15 18:55:17 +0100

    Improve UI of MUXControlsTestApp (#1793)
    * Improve UI of MUXControlsTestApp
    
    * Improve "Toggle theme" functionality
    
    * Fix test
    
    * Fix failed tests from last CI run
    
    * Small UI improvements, revert AnimatedVisualPlayerPage
    
    * Improve UI of MUXControlsTestApp
    
    * Improve "Toggle theme" functionality
    
    * Fix failed tests from last CI run
    
    * Small UI improvements, revert AnimatedVisualPlayerPage
    
    * First bulk of page layout improvements
    
    * Second bulk of page refactors
    
    * Fix AutoSuggestBoxTest failure
    
    * Additional page layout improvements
    
    * Revert pages related to failing tests
    
    * Remove faulty character, change color
    
    * Revert more pages

--- dev/CalendarView/TestUI/CalendarViewPage.xaml.cs
+++ dev/CalendarView/TestUI/CalendarViewPage.xaml.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using Windows.Globalization;
+using Windows.UI.Xaml.Controls;
+
 namespace MUXControlsTestApp
 {
     [TopLevelTestPage(Name = "CalendarView", Icon = "CalendarView.png")]
@@ -9,6 +14,33 @@ namespace MUXControlsTestApp
         public CalendarViewPage()
         {
             this.InitializeComponent();
+
+
+            List<string> calendarIdentifiers = new List<string>()
+            {
+                CalendarIdentifiers.Gregorian,
+                CalendarIdentifiers.Hebrew,
+                CalendarIdentifiers.Hijri,
+                CalendarIdentifiers.Japanese,
+                CalendarIdentifiers.Julian,
+                CalendarIdentifiers.Korean,
+                CalendarIdentifiers.Persian,
+                CalendarIdentifiers.Taiwan,
+                CalendarIdentifiers.Thai,
+                CalendarIdentifiers.UmAlQura,
+            };
+
+            calendarIdentifier.ItemsSource = calendarIdentifiers;
+            calendarIdentifier.SelectedItem = CalendarIdentifiers.Gregorian;
+        }
+
+        private void SelectionMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CalendarViewSelectionMode selectionMode;
+            if (Enum.TryParse<CalendarViewSelectionMode>((sender as ComboBox).SelectedItem.ToString(), out selectionMode))
+            {
+                PageCalendar.SelectionMode = selectionMode;
+            }
         }
     }
 }
