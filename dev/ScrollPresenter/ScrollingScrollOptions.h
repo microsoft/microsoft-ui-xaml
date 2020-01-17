d846816f1f0122b7fa1b199d6cf628fadcf942ce
Regis Brid <regisb@microsoft.com>
2020-01-17 15:45:27 -0800

    Fixing renaming misses.

--- dev/ScrollPresenter/ScrollingScrollOptions.h
+++ dev/ScrollPresenter/ScrollingScrollOptions.h
@@ -10,24 +10,24 @@ class ScrollingScrollOptions :
     public winrt::implementation::ScrollingScrollOptionsT<ScrollingScrollOptions>
 {
 public:
-    ScrollingScrollOptions(winrt::AnimationMode const& animationMode);
-    ScrollingScrollOptions(winrt::AnimationMode const& animationMode, winrt::SnapPointsMode const& snapPointsMode);
+    ScrollingScrollOptions(winrt::ScrollingAnimationMode const& animationMode);
+    ScrollingScrollOptions(winrt::ScrollingAnimationMode const& animationMode, winrt::ScrollingSnapPointsMode const& snapPointsMode);
 
     ~ScrollingScrollOptions()
     {
         SCROLLPRESENTER_TRACE_VERBOSE(nullptr, TRACE_MSG_METH, METH_NAME, this);
     }
 
-    static constexpr winrt::AnimationMode s_defaultAnimationMode{ winrt::AnimationMode::Auto };
-    static constexpr winrt::SnapPointsMode s_defaultSnapPointsMode{ winrt::SnapPointsMode::Default };
+    static constexpr winrt::ScrollingAnimationMode s_defaultAnimationMode{ winrt::ScrollingAnimationMode::Auto };
+    static constexpr winrt::ScrollingSnapPointsMode s_defaultSnapPointsMode{ winrt::ScrollingSnapPointsMode::Default };
 
-    winrt::AnimationMode AnimationMode() const;
-    void AnimationMode(winrt::AnimationMode const& animationMode);
+    winrt::ScrollingAnimationMode AnimationMode() const;
+    void AnimationMode(winrt::ScrollingAnimationMode const& animationMode);
 
-    winrt::SnapPointsMode SnapPointsMode() const;
-    void SnapPointsMode(winrt::SnapPointsMode const& snapPointsMode);
+    winrt::ScrollingSnapPointsMode SnapPointsMode() const;
+    void SnapPointsMode(winrt::ScrollingSnapPointsMode const& snapPointsMode);
 
 private:
-    winrt::AnimationMode m_animationMode{ s_defaultAnimationMode };
-    winrt::SnapPointsMode m_snapPointsMode{ s_defaultSnapPointsMode };
+    winrt::ScrollingAnimationMode m_animationMode{ s_defaultAnimationMode };
+    winrt::ScrollingSnapPointsMode m_snapPointsMode{ s_defaultSnapPointsMode };
 };
