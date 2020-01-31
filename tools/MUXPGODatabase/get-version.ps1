80a4ab55e6423893c85fcbd8e0190d03c35d9bb3
Bartek Knapp <baknapp@microsoft.com>
2020-01-31 12:54:03 -0800

    diag

--- tools/MUXPGODatabase/get-version.ps1
+++ tools/MUXPGODatabase/get-version.ps1
@@ -1,20 +1,28 @@
 function get_version( $pgoBranch )
 {
+    Write-Output $pgoBranch
+
     $forkSHA = ( & git merge-base --fork-point $pgoBranch )
+
+    Write-Output $forkSHA
 
     if ( $LastExitCode -ne 0 )
     {
+        Write-Output $LastExitCode
         throw "FAILED: git merge-base"
     }
 
     $forkDate = ( Get-Date -Date ( & git log -1 $forkSHA --date=iso --pretty=format:"%ad" ) ).ToUniversalTime().ToString("yyMMddHHmm")
 
+    Write-Output $forkDate
+
     if ( $LastExitCode -ne 0 )
     {
+        Write-Output $LastExitCode
         throw "FAILED: Get forkDate"
     }
 
-    if ($pgoBranch -eq "origin/master")
+    if ($pgoBranch -eq "master")
     {
         $version = $forkDate
     }
