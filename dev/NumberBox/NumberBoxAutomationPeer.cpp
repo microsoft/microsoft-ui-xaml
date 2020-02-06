6a291e85ebfaf0434ccfc7aec0a65076e6d66da1
T Paine <30241445+teaP@users.noreply.github.com>
2020-02-05 18:38:59 -0800

    Fixes for a few NumberBox bugs (#1933)
    * Fixes for a few more numberbox bugs
    
    * If AutomationProperties.Name is empty, and Header is also empty, then we should just return an empty string.

--- dev/NumberBox/NumberBoxAutomationPeer.cpp
+++ dev/NumberBox/NumberBoxAutomationPeer.cpp
@@ -31,6 +31,21 @@ hstring NumberBoxAutomationPeer::GetClassNameCore()
     return winrt::hstring_name_of<winrt::NumberBox>();
 }
 
+hstring NumberBoxAutomationPeer::GetNameCore()
+{
+    winrt::hstring name = __super::GetNameCore();
+
+    if (name.empty())
+    {
+        if (auto numberBox = Owner().try_as<winrt::NumberBox>())
+        {
+            name = SharedHelpers::TryGetStringRepresentationFromObject(numberBox.Header());
+        }
+    }
+
+    return name;
+}
+
 winrt::AutomationControlType NumberBoxAutomationPeer::GetAutomationControlTypeCore()
 {
     return winrt::AutomationControlType::Spinner;
