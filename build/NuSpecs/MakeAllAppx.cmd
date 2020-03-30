e733130a0e9577614ba51cbc0879aff9f1f42348
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-03-30 18:49:07 +0200

    Update MakeAllAppx.cmd

--- build/NuSpecs/MakeAllAppx.cmd
+++ build/NuSpecs/MakeAllAppx.cmd
@@ -1,6 +1,10 @@
-call ..\..\tools\MakeAppxHelper.cmd x86 release %*
-call ..\..\tools\MakeAppxHelper.cmd x86 debug %*
-call ..\..\tools\MakeAppxHelper.cmd x64 release %*
-call ..\..\tools\MakeAppxHelper.cmd x64 debug %*
-call ..\..\tools\MakeAppxHelper.cmd arm release %*
-call ..\..\tools\MakeAppxHelper.cmd arm debug %*
+REM %~dp0 is position of script file
+call %~dp0..\..\tools\MakeAppxHelper.cmd x86 release %*
+call %~dp0..\..\tools\MakeAppxHelper.cmd x86 debug %*
+call %~dp0..\..\tools\MakeAppxHelper.cmd x64 release %*
+call %~dp0..\..\tools\MakeAppxHelper.cmd x64 debug %*
+call %~dp0..\..\tools\MakeAppxHelper.cmd arm release %*
+call %~dp0..\..\tools\MakeAppxHelper.cmd arm debug %*
+REM Exit with 0 since some script may fail but we don't want to block pipeline
+REM If one of the scripts failed unexpectedly, later steps in pipeline will fail 
+EXIT 0
