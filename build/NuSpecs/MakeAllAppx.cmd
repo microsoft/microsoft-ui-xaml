5f765b6309c7eb9af28ad0723a50abdc1a4377cb
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-03-30 23:18:49 +0200

    Update makeallappx to only call script on dlls that exist

--- build/NuSpecs/MakeAllAppx.cmd
+++ build/NuSpecs/MakeAllAppx.cmd
@@ -1,10 +1,28 @@
+@echo off
 REM %~dp0 is position of script file
-call %~dp0..\..\tools\MakeAppxHelper.cmd x86 release %*
-call %~dp0..\..\tools\MakeAppxHelper.cmd x86 debug %*
-call %~dp0..\..\tools\MakeAppxHelper.cmd x64 release %*
-call %~dp0..\..\tools\MakeAppxHelper.cmd x64 debug %*
-call %~dp0..\..\tools\MakeAppxHelper.cmd arm release %*
-call %~dp0..\..\tools\MakeAppxHelper.cmd arm debug %*
-REM Exit with 0 since some script may fail but we don't want to block pipeline
-REM If one of the scripts failed unexpectedly, later steps in pipeline will fail 
-EXIT 0
+
+
+if exist %~dp0"..\..\BuildOutput\Debug\x86\Microsoft.UI.Xaml" (
+	call %~dp0..\..\tools\MakeAppxHelper.cmd x86 debug %*
+)
+if exist %~dp0"..\..\BuildOutput\Release\x86\Microsoft.UI.Xaml" (
+	call %~dp0..\..\tools\MakeAppxHelper.cmd x86 release%*
+)
+if exist %~dp0"..\..\BuildOutput\Debug\x64\Microsoft.UI.Xaml" (
+  call %~dp0..\..\tools\MakeAppxHelper.cmd x64 debug %*
+)
+if exist %~dp0"..\..\BuildOutput\Release\x64\Microsoft.UI.Xaml" (
+  call %~dp0..\..\tools\MakeAppxHelper.cmd x64 release %*
+)
+if exist %~dp0"..\..\BuildOutput\Debug\arm\Microsoft.UI.Xaml" (
+  call %~dp0..\..\tools\MakeAppxHelper.cmd arm debug %*
+)
+if exist %~dp0"..\..\BuildOutput\Release\arm\Microsoft.UI.Xaml" (
+  call %~dp0..\..\tools\MakeAppxHelper.cmd arm release %*
+)
+if exist %~dp0"..\..\BuildOutput\Debug\arm64\Microsoft.UI.Xaml" (
+  call %~dp0..\..\tools\MakeAppxHelper.cmd arm64 debug %*
+)
+if exist %~dp0"..\..\BuildOutput\Release\arm64\Microsoft.UI.Xaml" (
+  call %~dp0..\..\tools\MakeAppxHelper.cmd arm64 release %*
+)
