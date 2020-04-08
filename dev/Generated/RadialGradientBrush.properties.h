990fc6861a6fa3beaf460c0f9b9605bea4a733dd
Ranjesh <28935693+ranjeshj@users.noreply.github.com>
2020-04-07 21:26:13 -0700

    move RadialGradientBrush from preview to public (#2192)
    * make RadialGradientBrush public
    
    * Revert "Refactor loading of AdditionalStyles.xaml file (#2136)"
    
    This reverts commit 290b432858a464965ee634c3314046e79d1311dc.
    
    * Make GradientOrigin always be relative to top left of the element.
    
    * Revert "Revert "Refactor loading of AdditionalStyles.xaml file (#2136)""
    
    This reverts commit 68295b939e74f085930050566bf7a9c408d0760d.
    
    * Change EllipseCenter to Center and EllipseRadius to RadiusX/radiusY to match with WPF.
    
    Co-authored-by: Keith Mahoney <keith.mahoney@microsoft.com>

--- dev/Generated/RadialGradientBrush.properties.h
+++ dev/Generated/RadialGradientBrush.properties.h
@@ -9,11 +9,8 @@ class RadialGradientBrushProperties
 public:
     RadialGradientBrushProperties();
 
-    void EllipseCenter(winrt::Point const& value);
-    winrt::Point EllipseCenter();
-
-    void EllipseRadius(winrt::Point const& value);
-    winrt::Point EllipseRadius();
+    void Center(winrt::Point const& value);
+    winrt::Point Center();
 
     void GradientOrigin(winrt::Point const& value);
     winrt::Point GradientOrigin();
@@ -24,31 +21,35 @@ public:
     void MappingMode(winrt::BrushMappingMode const& value);
     winrt::BrushMappingMode MappingMode();
 
+    void RadiusX(double value);
+    double RadiusX();
+
+    void RadiusY(double value);
+    double RadiusY();
+
     void SpreadMethod(winrt::GradientSpreadMethod const& value);
     winrt::GradientSpreadMethod SpreadMethod();
 
-    static winrt::DependencyProperty EllipseCenterProperty() { return s_EllipseCenterProperty; }
-    static winrt::DependencyProperty EllipseRadiusProperty() { return s_EllipseRadiusProperty; }
+    static winrt::DependencyProperty CenterProperty() { return s_CenterProperty; }
     static winrt::DependencyProperty GradientOriginProperty() { return s_GradientOriginProperty; }
     static winrt::DependencyProperty InterpolationSpaceProperty() { return s_InterpolationSpaceProperty; }
     static winrt::DependencyProperty MappingModeProperty() { return s_MappingModeProperty; }
+    static winrt::DependencyProperty RadiusXProperty() { return s_RadiusXProperty; }
+    static winrt::DependencyProperty RadiusYProperty() { return s_RadiusYProperty; }
     static winrt::DependencyProperty SpreadMethodProperty() { return s_SpreadMethodProperty; }
 
-    static GlobalDependencyProperty s_EllipseCenterProperty;
-    static GlobalDependencyProperty s_EllipseRadiusProperty;
+    static GlobalDependencyProperty s_CenterProperty;
     static GlobalDependencyProperty s_GradientOriginProperty;
     static GlobalDependencyProperty s_InterpolationSpaceProperty;
     static GlobalDependencyProperty s_MappingModeProperty;
+    static GlobalDependencyProperty s_RadiusXProperty;
+    static GlobalDependencyProperty s_RadiusYProperty;
     static GlobalDependencyProperty s_SpreadMethodProperty;
 
     static void EnsureProperties();
     static void ClearProperties();
 
-    static void OnEllipseCenterPropertyChanged(
-        winrt::DependencyObject const& sender,
-        winrt::DependencyPropertyChangedEventArgs const& args);
-
-    static void OnEllipseRadiusPropertyChanged(
+    static void OnCenterPropertyChanged(
         winrt::DependencyObject const& sender,
         winrt::DependencyPropertyChangedEventArgs const& args);
 
@@ -61,6 +62,14 @@ public:
         winrt::DependencyPropertyChangedEventArgs const& args);
 
     static void OnMappingModePropertyChanged(
+        winrt::DependencyObject const& sender,
+        winrt::DependencyPropertyChangedEventArgs const& args);
+
+    static void OnRadiusXPropertyChanged(
+        winrt::DependencyObject const& sender,
+        winrt::DependencyPropertyChangedEventArgs const& args);
+
+    static void OnRadiusYPropertyChanged(
         winrt::DependencyObject const& sender,
         winrt::DependencyPropertyChangedEventArgs const& args);
 
