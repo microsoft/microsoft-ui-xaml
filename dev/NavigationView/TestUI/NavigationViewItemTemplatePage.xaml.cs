e51acddfec4b6cac7b9d57a38761d8c664e85d09
Dilip Ojha <dilip.ojha@microsoft.com>
2020-06-15 17:35:14 -0700

    added test

--- dev/NavigationView/TestUI/NavigationViewItemTemplatePage.xaml.cs
+++ dev/NavigationView/TestUI/NavigationViewItemTemplatePage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Navigation;
 
 using NavigationViewPaneDisplayMode = Microsoft.UI.Xaml.Controls.NavigationViewPaneDisplayMode;
 using MaterialHelperTestApi = Microsoft.UI.Private.Media.MaterialHelperTestApi;
+using NavigationView = Microsoft.UI.Xaml.Controls.NavigationView;
+using NavigationViewItemInvokedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs;
+using NavigationViewSelectionChangedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs;
 
 namespace MUXControlsTestApp
 {
@@ -54,6 +57,40 @@ namespace MUXControlsTestApp
         private void FlipOrientation_Click(object sender, RoutedEventArgs e)
         {
             NavView.PaneDisplayMode = NavView.PaneDisplayMode == NavigationViewPaneDisplayMode.Top ? NavigationViewPaneDisplayMode.Auto : NavigationViewPaneDisplayMode.Top;
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            // Reset argument type indicatiors
+            ItemInvokedItemType.Text = "null";
+            ItemInvokedItemContainerType.Text = "null";
+
+            if (args.InvokedItem != null)
+            {
+                ItemInvokedItemType.Text = args.InvokedItem.GetType().ToString();
+            }
+
+            if (args.InvokedItemContainer != null)
+            {
+                ItemInvokedItemContainerType.Text = args.InvokedItemContainer.GetType().ToString();
+            }
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            // Reset argument type indicatiors
+            SelectionChangedItemType.Text = "null";
+            SelectionChangedItemContainerType.Text = "null";
+
+            if (args.SelectedItem != null)
+            {
+                SelectionChangedItemType.Text = args.SelectedItem.GetType().ToString();
+            }
+
+            if (args.SelectedItemContainer != null)
+            {
+                SelectionChangedItemContainerType.Text = args.SelectedItemContainer.GetType().ToString();
+            }
         }
     }
 }
