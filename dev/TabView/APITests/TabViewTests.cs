d0c43176a4e6ac6adc8afd11288a90911c74d87e
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-06-25 12:17:54 +0200

    Remove unused import

--- dev/TabView/APITests/TabViewTests.cs
+++ dev/TabView/APITests/TabViewTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using MUXControlsTestApp.Utilities;
@@ -10,7 +10,6 @@ using Windows.UI.Xaml.Media;
 using Common;
 using Microsoft.UI.Xaml.Controls;
 using System.Collections.Generic;
-using System.Threading.Tasks.Dataflow;
 
 #if USING_TAEF
 using WEX.TestExecution;
