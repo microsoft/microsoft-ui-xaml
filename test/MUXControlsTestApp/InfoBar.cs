d35fe483ac05e297c85237ea1cf3bd01feb65533
chenss3 <sophia.s.chen@vanderbilt.edu>
2020-06-29 15:59:17 -0500

    First commit to add in the infobar prototype

---
+++ test/MUXControlsTestApp/InfoBar.cs
@@ -0,0 +1,471 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.UI.Composition;
+
+public class InfoBar
+{
+    public enum InfoBarCloseReason
+    {
+        CloseButton,
+        Programattic
+    }
+    public enum InfoBarSeverity
+    {
+        Critical,
+        Warning,
+        Informational,
+        Success,
+        Default,
+        None
+    }
+
+    public class InfoBarClosedEventArgs : EventArgs
+    {
+        public InfoBarCloseReason Reason
+        {
+            get; set;
+        }
+    }
+
+    public class InfoBarClosingEventArgs : EventArgs
+    {
+        public InfoBarCloseReason Reason
+        {
+            get; set;
+        }
+        public bool Cancel
+        {
+            get; set;
+        }
+
+    }
+    public class InfoBarEventArgs : EventArgs
+    {
+        public bool IsHandled
+        {
+            get; set;
+        }
+    }
+    public sealed class InfoBar : ContentControl
+    {
+        Button _actionButton;
+        Button _alternateCloseButton;
+        Button _closeButton;
+        Border _myBorder;
+
+        public event EventHandler<RoutedEventArgs> ActionButtonClick;
+        public event EventHandler<InfoBarEventArgs> CloseButtonClick;
+        public event TypedEventHandler<InfoBar, InfoBarClosedEventArgs> Closed;
+        public event TypedEventHandler<InfoBar, InfoBarClosingEventArgs> Closing;
+
+        private InfoBarCloseReason lastCloseReason = InfoBarCloseReason.Programattic;
+
+
+        public InfoBar()
+        {
+            this.DefaultStyleKey = typeof(InfoBar);
+        }
+
+
+        T GetTemplateChild<T>(string name) where T : DependencyObject
+        {
+            var child = GetTemplateChild(name) as T;
+            if (child == null)
+            {
+                throw new NullReferenceException(name);
+            }
+            return child;
+        }
+
+
+        protected override void OnApplyTemplate()
+        {
+            _alternateCloseButton = GetTemplateChild<Button>("AlternateCloseButton");
+            _closeButton = GetTemplateChild<Button>("CloseButton");
+            _actionButton = GetTemplateChild<Button>("ActionButton");
+            _myBorder = GetTemplateChild<Border>("Container");
+
+            UpdateButtonsState();
+            UpdateSeverityState();
+            OnIsOpenChanged();
+
+            _alternateCloseButton.Click += new RoutedEventHandler(OnCloseButtonClick);
+            _closeButton.Click += new RoutedEventHandler(OnCloseButtonClick);
+            _actionButton.Click += (s, e) => ActionButtonClick?.Invoke(s, e);
+        }
+
+
+        private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DependencyProperty property = e.Property;
+            InfoBar infoBar = d as InfoBar;
+            if (property == SeverityProperty)
+            {
+                infoBar.UpdateSeverityState();
+            }
+            else if (property == ActionButtonContentProperty || property == CloseButtonContentProperty)
+            {
+                infoBar.UpdateButtonsState();
+            }
+            else if (property == IsOpenProperty)
+            {
+                infoBar.OnIsOpenChanged();
+            }
+
+        }
+
+
+
+        /* Open Properties
+         * 
+         */
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register(nameof(IsOpen), typeof(bool), typeof(InfoBar), new PropertyMetadata(false, OnPropertyChanged));
+
+
+        public bool ShowCloseButton
+        {
+            get { return (bool)GetValue(ShowCloseButtonProperty); }
+            set { SetValue(ShowCloseButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty ShowCloseButtonProperty =
+            DependencyProperty.Register(nameof(ShowCloseButton), typeof(bool), typeof(InfoBar), new PropertyMetadata(true));
+
+
+
+
+        /* Message Title Properties
+         * 
+         */
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(InfoBar), new PropertyMetadata(""));
+
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register(nameof(Message), typeof(string), typeof(InfoBar), new PropertyMetadata(""));
+
+
+
+
+        /* Action Button Properties
+         * 
+         */
+        public object ActionButtonContent
+        {
+            get { return (object)GetValue(ActionButtonContentProperty); }
+            set { SetValue(ActionButtonContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ActionButtonContentProperty =
+            DependencyProperty.Register(nameof(ActionButtonContent), typeof(object), typeof(InfoBar), new PropertyMetadata(null, OnPropertyChanged));
+
+
+        public Style ActionButtonStyle
+        {
+            get { return (Style)GetValue(ActionButtonStyleProperty); }
+            set { SetValue(ActionButtonStyleProperty, value); }
+        }
+
+        public static readonly DependencyProperty ActionButtonStyleProperty =
+            DependencyProperty.Register(nameof(ActionButtonStyle), typeof(Style), typeof(InfoBar), new PropertyMetadata(null));
+
+
+        public ICommand ActionButtonCommand
+        {
+            get { return (ICommand)GetValue(ActionButtonCommandProperty); }
+            set { SetValue(ActionButtonCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty ActionButtonCommandProperty =
+            DependencyProperty.Register(nameof(ActionButtonCommand), typeof(ICommand), typeof(InfoBar), new PropertyMetadata(null));
+
+
+        public object ActionButtonCommandParameter
+        {
+            get { return (object)GetValue(ActionButtonCommandParameterProperty); }
+            set { SetValue(ActionButtonCommandParameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty ActionButtonCommandParameterProperty =
+            DependencyProperty.Register(nameof(ActionButtonCommandParameter), typeof(object), typeof(InfoBar), new PropertyMetadata(null));
+
+
+
+
+
+        /* Close Button Properties
+         * 
+         */
+        public object CloseButtonContent
+        {
+            get { return (object)GetValue(CloseButtonContentProperty); }
+            set { SetValue(CloseButtonContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty CloseButtonContentProperty =
+            DependencyProperty.Register(nameof(CloseButtonContent), typeof(object), typeof(InfoBar), new PropertyMetadata(null, OnPropertyChanged));
+
+        public Style CloseButtonStyle
+        {
+            get { return (Style)GetValue(CloseButtonStyleProperty); }
+            set { SetValue(CloseButtonStyleProperty, value); }
+        }
+
+        public static readonly DependencyProperty CloseButtonStyleProperty =
+            DependencyProperty.Register(nameof(CloseButtonStyle), typeof(Style), typeof(InfoBar), new PropertyMetadata(null));
+
+
+        public ICommand CloseButtonCommand
+        {
+            get { return (ICommand)GetValue(CloseButtonCommandProperty); }
+            set { SetValue(CloseButtonCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CloseButtonCommandProperty =
+            DependencyProperty.Register(nameof(CloseButtonCommand), typeof(ICommand), typeof(InfoBar), new PropertyMetadata(null));
+
+
+        public object CloseButtonCommandParameter
+        {
+            get { return (object)GetValue(CloseButtonCommandParameterProperty); }
+            set { SetValue(CloseButtonCommandParameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty CloseButtonCommandParameterProperty =
+            DependencyProperty.Register(nameof(CloseButtonCommandParameter), typeof(object), typeof(InfoBar), new PropertyMetadata(null));
+
+
+
+
+        /* Severity-Related Properties
+        * 
+        */
+        public InfoBarSeverity Severity
+        {
+            get { return (InfoBarSeverity)GetValue(SeverityProperty); }
+            set { SetValue(SeverityProperty, value); }
+        }
+
+        public static readonly DependencyProperty SeverityProperty =
+            DependencyProperty.Register(nameof(Severity), typeof(InfoBarSeverity), typeof(InfoBar), new PropertyMetadata(InfoBarSeverity.Default, OnPropertyChanged));
+
+        public Color StatusColor
+        {
+            get { return (Color)GetValue(StatusColorProperty); }
+            set { SetValue(StatusColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty StatusColorProperty =
+            DependencyProperty.Register(nameof(StatusColor), typeof(Color), typeof(InfoBar), new PropertyMetadata(Color.FromArgb(0, 0, 0, 0)));
+
+
+        public IconSource IconSource
+        {
+            get { return (IconSource)GetValue(IconSourceProperty); }
+            set { SetValue(IconSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconSourceProperty =
+            DependencyProperty.Register(nameof(IconSource), typeof(IconSource), typeof(InfoBar), new PropertyMetadata(default));
+
+
+
+        // Event handlers for Close Button and Action Button
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            lastCloseReason = InfoBarCloseReason.CloseButton;
+            InfoBarEventArgs newE = new InfoBarEventArgs();
+            if (CloseButtonClick != null)
+            {
+                CloseButtonClick.Invoke(this, newE);
+            }
+            if (newE.IsHandled == false)
+            {
+                RaiseClosingEvent();
+                Open(IsOpen);
+                RaiseClosedEvent();
+            }
+
+        }
+
+
+        void RaiseClosingEvent()
+        {
+            InfoBarClosingEventArgs args = new InfoBarClosingEventArgs();
+            args.Reason = lastCloseReason;
+
+            if (Closing != null)
+            {
+                Closing.Invoke(this, args);
+            }
+
+            if (!args.Cancel)
+            {
+                _myBorder.Visibility = Visibility.Collapsed;
+                IsOpen = false;
+            }
+            else
+            {
+                // The developer has changed the Cancel property to true, indicating that they wish to Cancel the
+                // closing of this tip, so we need to revert the IsOpen property to true.
+                IsOpen = true;
+            }
+
+
+        }
+
+
+        void RaiseClosedEvent()
+        {
+            InfoBarClosedEventArgs args = new InfoBarClosedEventArgs();
+            args.Reason = lastCloseReason;
+            if (Closed != null)
+            {
+                Closed.Invoke(this, args);
+            }
+
+        }
+
+
+
+        // Updates Severity state of InfoBar
+        void UpdateSeverityState()
+        {
+            if (Severity == InfoBarSeverity.Critical)
+            {
+                VisualStateManager.GoToState(this, "Critical", false);
+            }
+            else if (Severity == InfoBarSeverity.Warning)
+            {
+                VisualStateManager.GoToState(this, "Warning", false);
+            }
+            else if (Severity == InfoBarSeverity.Informational)
+            {
+                VisualStateManager.GoToState(this, "Informational", false);
+            }
+            else if (Severity == InfoBarSeverity.Success)
+            {
+                VisualStateManager.GoToState(this, "Success", false);
+            }
+            else if (Severity == InfoBarSeverity.None)
+            {
+                VisualStateManager.GoToState(this, "None", false);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Default", false);
+            }
+        }
+
+
+        // Updates visibility of buttons
+        void UpdateButtonsState()
+        {
+            if (ShowCloseButton)
+            {
+                if (CloseButtonContent != null && ActionButtonContent != null)
+                {
+                    VisualStateManager.GoToState(this, "BothButtonsVisible", false);
+                    VisualStateManager.GoToState(this, "NoDefaultCloseButton", false);
+                }
+                else if (CloseButtonContent != null)
+                {
+                    VisualStateManager.GoToState(this, "CloseButtonVisible", false);
+                    VisualStateManager.GoToState(this, "NoDefaultCloseButton", false);
+                }
+                else if (ActionButtonContent != null)
+                {
+                    VisualStateManager.GoToState(this, "ActionButtonVisible", false);
+                    VisualStateManager.GoToState(this, "DefaultCloseButton", false);
+                }
+                else if (ActionButtonContent == null && CloseButtonContent == null)
+                {
+                    VisualStateManager.GoToState(this, "NoButtonsVisible", false);
+                    VisualStateManager.GoToState(this, "DefaultCloseButton", false);
+                }
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "NoDefaultCloseButton", false);
+                if (ActionButtonContent != null)
+                {
+                    VisualStateManager.GoToState(this, "ActionButtonVisible", false);
+
+                }
+                else
+                {
+                    VisualStateManager.GoToState(this, "NoButtonsVisible", false);
+                }
+            }
+        }
+
+
+        // Updates if InfoBar is opened
+        void OnIsOpenChanged()
+        {
+            if (IsOpen)
+            {
+                lastCloseReason = InfoBarCloseReason.Programattic;
+
+            }
+            else
+            {
+                RaiseClosingEvent();
+                RaiseClosedEvent();
+            }
+            Open(IsOpen);
+        }
+
+
+        // Opens or closes the InfoBar
+        private void Open(bool value)
+        {
+            if (_myBorder != null)
+            {
+
+                if (value)
+                {
+                    _myBorder.Visibility = Visibility.Visible;
+                    IsOpen = true;
+                }
+                else
+                {
+                    _myBorder.Visibility = Visibility.Collapsed;
+                    IsOpen = false;
+                }
+            }
+        }
+    }
+}
---
+++ test/MUXControlsTestApp/InfoBarTestPage.xaml.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MUXControlsTestApp.Shared;
+using InfoBar; 
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MUXControlsTestApp.Shared
+{
+    [TopLevelTestPage(Name = "InfoBar")]
+    public sealed partial class InfoBarTestPage : Page
+    {
+
+
+        InfoBarSeverity severity;
+        IconSource icon;
+        String title;
+        String message;
+        String actionButtonContent;
+        String closeButtonContent;
+        bool open;
+
+
+        public InfoBarTestPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Test_ActionButtonClick(object sender, RoutedEventArgs e)
+        {
+            await new MessageDialog("Thank you, mate").ShowAsync();
+        }
+
+        private async void Test_CloseButtonClick(object sender, InfoBarEventArgs e)
+        {
+            e.IsHandled = true;
+            await new MessageDialog("Thank you, mate").ShowAsync();
+        }
+
+        private async void Test_Closing(InfoBar sender, InfoBarClosingEventArgs args)
+        {
+
+            await new MessageDialog("Thank you, mate im closing").ShowAsync();
+        }
+
+        private async void Test_Closed(InfoBar sender, InfoBarClosedEventArgs args)
+        {
+            await new MessageDialog("Thank you, mate im closed").ShowAsync();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string severityName = e.AddedItems[0].ToString();
+
+            switch (severityName)
+            {
+                case "Critical":
+                    severity = InfoBarSeverity.Critical;
+                    break;
+                case "Warning":
+                    severity = InfoBarSeverity.Warning;
+                    break;
+                case "Informational":
+                    severity = InfoBarSeverity.Informational;
+                    break;
+                case "Success":
+                    severity = InfoBarSeverity.Success;
+                    break;
+                case "Default":
+                    severity = InfoBarSeverity.Default;
+                    break;
+                case "None":
+                    severity = InfoBarSeverity.None;
+                    break;
+            }
+        }
+
+        private void SeverityButton_Click(object sender, RoutedEventArgs e)
+        {
+            Test.Severity = severity;
+        }
+
+        private void IconComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string iconName = e.AddedItems[0].ToString();
+
+            switch (iconName)
+            {
+                case "Pin Icon":
+
+                    SymbolIconSource sym2 = new SymbolIconSource();
+                    sym2.Symbol = Symbol.Pin;
+
+                    icon = (IconSource)sym2;
+                    break;
+                case "No Icon":
+                    SymbolIconSource sym3 = new SymbolIconSource();
+                    sym3.Symbol = new Symbol();
+                    icon = sym3;
+                    break;
+
+            }
+        }
+
+        private void IconButton_Click(object sender, RoutedEventArgs e)
+        {
+            Test.IconSource = icon;
+        }
+
+        private void TitleComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string iconName = e.AddedItems[0].ToString();
+
+            switch (iconName)
+            {
+                case "Short Title":
+                    title = "Short Title.";
+                    break;
+                case "Long Title":
+                    title = "Long Title. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+                    break;
+                case "No Title":
+                    title = "";
+                    break;
+            }
+        }
+
+        private void TitleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Test.Title = title;
+        }
+
+        private void MessageComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string iconName = e.AddedItems[0].ToString();
+
+            switch (iconName)
+            {
+                case "Short Message":
+                    message = "Short Message.";
+                    break;
+                case "Long Message":
+                    message = "Long Message. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+                    break;
+                case "No Message":
+                    message = "";
+                    break;
+            }
+        }
+
+        private void MessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Test.Message = message;
+        }
+
+        private void CloseButtonContentComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string iconName = e.AddedItems[0].ToString();
+
+            switch (iconName)
+            {
+                case "Short Text":
+                    closeButtonContent = "C:Short";
+                    break;
+                case "Long Text":
+                    closeButtonContent = "C:LongTextLorem ipsum dolor sit amet.";
+                    break;
+                case "No Text":
+                    closeButtonContent = "";
+                    break;
+            }
+        }
+
+        private void CloseButtonContent_Click(object sender, RoutedEventArgs e)
+        {
+            Test.CloseButtonContent = closeButtonContent;
+        }
+
+        private void ActionButtonContentComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string iconName = e.AddedItems[0].ToString();
+
+            switch (iconName)
+            {
+                case "Short Text":
+                    actionButtonContent = "A:Short";
+                    break;
+                case "Long Text":
+                    actionButtonContent = "A:LongTextLorem ipsum dolor sit amet.";
+                    break;
+                case "No Text":
+                    actionButtonContent = "";
+                    break;
+            }
+        }
+
+        private void ActionButtonContent_Click(object sender, RoutedEventArgs e)
+        {
+            Test.ActionButtonContent = actionButtonContent;
+        }
+
+        private void IsOpen_Checked(object sender, RoutedEventArgs e)
+        {
+            open = true;
+        }
+
+        private void IsOpen_Unchecked(object sender, RoutedEventArgs e)
+        {
+            open = false;
+        }
+
+        private void IsOpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            Test.IsOpen = open;
+        }
+    }
+}
