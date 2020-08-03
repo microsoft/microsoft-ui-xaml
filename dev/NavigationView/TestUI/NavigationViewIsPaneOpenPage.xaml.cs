c058b6ad951e595111007836b6dc597e11cf06b0
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-08-03 13:53:50 +0200

    Fix issue with item that just got uncollapsed not showing selection

--- dev/NavigationView/TestUI/NavigationViewIsPaneOpenPage.xaml.cs
+++ dev/NavigationView/TestUI/NavigationViewIsPaneOpenPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Microsoft.UI.Xaml.Controls;
+
 namespace MUXControlsTestApp
 {
     public sealed partial class NavigationViewIsPaneOpenPage : TestPage
@@ -8,6 +10,17 @@ namespace MUXControlsTestApp
         public NavigationViewIsPaneOpenPage()
         {
             this.InitializeComponent();
+
+        }
+
+        private void NavigationView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+            if((args.InvokedItem as string) == "Apps")
+            {
+                CollapsedItem.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+                NavView.SelectedItem = CollapsedItem;
+            }
         }
     }
 }
