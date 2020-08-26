120928404893b3175ab56ad91699fff039f18630
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-08-26 17:42:04 +0200

    Fix issue with color changing when updating value while error state

---
+++ dev/Common/SolidColorBrushToColorConverter.h
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+#include "SolidColorBrushToColorConverter.g.h"
+
+class SolidColorBrushToColorConverter :
+    public winrt::implementation::SolidColorBrushToColorConverterT<SolidColorBrushToColorConverter>
+{
+public:
+    winrt::IInspectable Convert(
+        winrt::IInspectable const& value,
+        winrt::TypeName const& targetType,
+        winrt::IInspectable const& parameter,
+        winrt::hstring const& language);
+
+    winrt::IInspectable ConvertBack(
+        winrt::IInspectable const& value,
+        winrt::TypeName const& targetType,
+        winrt::IInspectable const& parameter,
+        winrt::hstring const& language);
+};
