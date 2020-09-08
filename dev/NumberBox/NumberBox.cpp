b6e356fe62ab072f33a39d50b914ba29fcc19506
robloo <robert.looyengoed@gmail.com>
2020-09-08 16:31:05 -0400

    Switch to using const auto for variables

--- dev/NumberBox/NumberBox.cpp
+++ dev/NumberBox/NumberBox.cpp
@@ -52,8 +52,8 @@ void NumberBox::SetDefaultInputScope()
 {
     // Sets the default value of the InputScope property.
     // Note that InputScope is a class that cannot be set to a default value within the IDL.
-    winrt::InputScopeName inputScopeName = winrt::InputScopeName(winrt::InputScopeNameValue::Number);
-    winrt::InputScope inputScope = winrt::InputScope();
+    const auto inputScopeName = winrt::InputScopeName(winrt::InputScopeNameValue::Number);
+    const auto inputScope = winrt::InputScope();
     inputScope.Names().Append(inputScopeName);
 
     static_cast<NumberBox*>(this)->SetValue(s_InputScopeProperty, ValueHelper<winrt::InputScope>::BoxValueIfNecessary(inputScope));
