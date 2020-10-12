794cf606c22a966a524ca16fbe7eae944a06d86e
Stephen L Peters <stpete@microsoft.com>
2020-10-12 13:59:39 -0700

    Update progress ring API (#3396)
    * Update progress ring to change base class back to control from range base.
    
    * Update ProgressRingPage.xaml
    
    remove the tab stop setter

--- dev/ProgressRing/ProgressRingAutomationPeer.h
+++ dev/ProgressRing/ProgressRingAutomationPeer.h
@@ -13,9 +13,22 @@ class ProgressRingAutomationPeer :
 public:
     ProgressRingAutomationPeer(winrt::ProgressRing const& owner);
 
+    winrt::IInspectable GetPatternCore(winrt::PatternInterface const& patternInterface);
     winrt::hstring GetClassNameCore();
     winrt::hstring GetNameCore();
     winrt::AutomationControlType GetAutomationControlTypeCore();
     winrt::hstring GetLocalizedControlTypeCore();
+
+    // IRangeValueProvider
+    bool IsReadOnly() { return true; }
+    double Minimum();
+    double Maximum();
+    double Value();
+    double SmallChange();
+    double LargeChange();
+    void SetValue(double value);
+
+private:
+    com_ptr<ProgressRing> GetImpl();
 };
 
