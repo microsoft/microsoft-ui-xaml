79d8a7a22e4ca16fb23b30fdc702403e9f29b288
Eduardo Serna <edserna@microsoft.com>
2020-10-26 12:52:08 -0700

    Remove left/right logic and add events source custom test for expanders.

--- dev/Expander/Expander.cpp
+++ dev/Expander/Expander.cpp
@@ -38,7 +38,8 @@ void Expander::OnApplyTemplate()
     // (or on the events source that's set, if it's set) and
     //
     // 2. Set the expander's automation properties name to the
-    // toggleButton's in case the expander doesn't have one.
+    // toggleButton's in case the expander doesn't have one. This just follows
+    // what WPF does.
     if (winrt::AutomationPeer toggleButtonPeer = winrt::FrameworkElementAutomationPeer::FromElement(toggleButton))
     {
         // 1. Set the events source of the toggle button peer to the expander's.
@@ -131,12 +132,6 @@ void Expander::UpdateExpandDirection(bool useTransitions)
         break;
     case winrt::ExpandDirection::Up:
         winrt::VisualStateManager::GoToState(*this, L"Up", useTransitions);
-        break;
-    case winrt::ExpandDirection::Left:
-        winrt::VisualStateManager::GoToState(*this, L"Left", useTransitions);
-        break;
-    case winrt::ExpandDirection::Right:
-        winrt::VisualStateManager::GoToState(*this, L"Right", useTransitions);
         break;
     }
 }
--- dev/Expander/TestUI/ExpanderPage.xaml.cs
+++ dev/Expander/TestUI/ExpanderPage.xaml.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
@@ -11,15 +11,39 @@ using Windows.UI;
 using System.Windows.Input;
 
 using Expander = Microsoft.UI.Xaml.Controls.Expander;
+using Windows.UI.Xaml.Tests.MUXControls.ApiTests.RepeaterTests.Common;
+using Windows.UI.Xaml.Automation.Peers;
 
 namespace MUXControlsTestApp
 {
+    // We are going to test setting the events source of an expander to the customcontrol's
+    public sealed class TestControl : ContentControl
+    {
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return new TestControlAutomationPeer(this);
+        }
+    }
+
+    public sealed class TestControlAutomationPeer : FrameworkElementAutomationPeer
+    {
+        public TestControlAutomationPeer(TestControl owner) : base(owner){}
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.Custom;
+        }
+    }
+
     [TopLevelTestPage(Name = "Expander")]
     public sealed partial class ExpanderPage : TestPage
     {
         public ExpanderPage()
         {
             this.InitializeComponent();
+            var customControlPeer = FrameworkElementAutomationPeer.FromElement(CustomControl);
+            var expanderPeer = FrameworkElementAutomationPeer.FromElement(ExpanderWithCustomEventsSource);
+
+            expanderPeer.EventsSource = customControlPeer;
         }
     }
 }
