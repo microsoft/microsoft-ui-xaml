ef26816db97c3fbb896c0603e9edd79662d99215
Tammy Paine <30241445+teaP@users.noreply.github.com>
2020-10-28 15:41:26 -0700

    Added elevation border brushes

--- dev/CommonStyles/TestUI/VisualStatesPage.xaml.cs
+++ dev/CommonStyles/TestUI/VisualStatesPage.xaml.cs
@@ -1,19 +1,11 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using MUXControlsTestApp.Utilities;
-using System.Runtime.CompilerServices;
 
 namespace MUXControlsTestApp
 {
