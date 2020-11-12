54e5c1e07a591a776467266ae20192a428b984b1
Felix <FelixDev91@gmail.com>
2020-11-13 00:48:55 +0100

    Ensure that listview selected index is not outside of the item range.

--- dev/TabView/TabView.cpp
+++ dev/TabView/TabView.cpp
@@ -998,7 +998,12 @@ void TabView::UpdateSelectedIndex()
 {
     if (auto&& listView = m_listView.get())
     {
-        listView.SelectedIndex(SelectedIndex());
+        const auto selectedIndex = SelectedIndex();
+        // Ensure that the selected index is within range of the items
+        if (selectedIndex < GetItemCount())
+        {
+            listView.SelectedIndex(selectedIndex);
+        }
     }
 }
 
