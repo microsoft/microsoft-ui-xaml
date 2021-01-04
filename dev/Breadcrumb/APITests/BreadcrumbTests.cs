7105cbf6f1e7aa1677dbc1d195c6f06a96caf4d7
Alberto Medina Gutierrez <almedina@microsoft.com>
2021-01-04 11:26:21 -0800

    Add first API test

--- dev/Breadcrumb/APITests/BreadcrumbTests.cs
+++ dev/Breadcrumb/APITests/BreadcrumbTests.cs
@@ -1,11 +1,15 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
 using Common;
-using System.Collections.Generic;
-using Microsoft.UI.Xaml.Controls;
 using MUXControlsTestApp.Utilities;
+
+using Windows.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls;
+using Common;
+using Windows.UI.Xaml.Markup;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Media;
 
 #if USING_TAEF
 using WEX.TestExecution;
@@ -23,9 +27,65 @@ namespace Windows.UI.Xaml.Tests.MUXControls.ApiTests
     public class BreadcrumbTests : ApiTestBase
     {
         [TestMethod]
-        public void BasicTest()
+        public void VerifyCustomItemTemplate()
         {
-            Log.Comment("Breadcrumb Basic Test");
+            Breadcrumb breadcrumb = null;
+            Breadcrumb breadcrumb2 = null;
+            RunOnUIThread.Execute(() =>
+            {
+                breadcrumb = new Breadcrumb();
+                breadcrumb.ItemsSource = new List<string>() { "Option 1", "Option 2" };
+
+                // Set a custom ItemTemplate to be wrapped in a BreadcrumbItem.
+                var itemTemplate = (DataTemplate)XamlReader.Load(
+                        @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                            <TextBlock Text = '{Binding}'/>
+                        </DataTemplate>");
+
+                breadcrumb.ItemTemplate = itemTemplate;
+
+                breadcrumb2 = new Breadcrumb();
+                breadcrumb2.ItemsSource = new List<string>() { "Option 1", "Option 2" };
+
+                // Set a custom ItemTemplate which is already a BreadcrumbItem. No wrapping should be performed.
+                var itemTemplate2 = (DataTemplate)XamlReader.Load(
+                        @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                            <BreadcrumbItem Foreground='Blue'>
+                              <TextBlock Text = '{Binding}'/>
+                            </BreadcrumbItem>
+                        </DataTemplate>");       
+
+                breadcrumb2.ItemTemplate = itemTemplate2;
+
+                var stackPanel = new StackPanel();
+                stackPanel.Children.Add(breadcrumb);
+                stackPanel.Children.Add(breadcrumb2);
+
+                Content = stackPanel;
+                Content.UpdateLayout();
+            });
+
+            IdleSynchronizer.Wait();
+
+            RunOnUIThread.Execute(() =>
+            {
+                ItemsRepeater breadcrumbItemRepeater = (ItemsRepeater)breadcrumb.FindVisualChildByName("BreadcrumbItemRepeater");
+                ItemsRepeater breadcrumbItemRepeater2 = (ItemsRepeater)breadcrumb2.FindVisualChildByName("BreadcrumbItemRepeater");
+                Verify.IsNotNull(breadcrumbItemRepeater, "The underlying items repeater could not be retrieved");
+                Verify.IsNotNull(breadcrumbItemRepeater2, "The underlying items repeater could not be retrieved");
+
+                var breadcrumbNode1 = breadcrumbItemRepeater.TryGetElement(1) as BreadcrumbItem;
+                var breadcrumbNode2 = breadcrumbItemRepeater.TryGetElement(1) as BreadcrumbItem;
+                Verify.IsNotNull(breadcrumbNode1, "Our custom ItemTemplate should have been wrapped in a BreadcrumbItem.");
+                Verify.IsNotNull(breadcrumbNode2, "Our custom ItemTemplate should have been wrapped in a BreadcrumbItem.");
+
+                // change this conditions
+                bool testCondition = !(breadcrumbNode1.Foreground is SolidColorBrush brush && brush.Color == Colors.Blue);
+                Verify.IsTrue(testCondition, "Default foreground color of the BreadcrumbItem should not have been [blue].");
+
+                testCondition = breadcrumbNode2.Foreground is SolidColorBrush brush2 && brush2.Color == Colors.Blue;
+                Verify.IsTrue(testCondition, "The foreground color of the BreadcrumbItem should have been [blue].");
+            });
         }
     }
 }
