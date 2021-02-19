1bc16ecccadcdc797fb90552f7909dd304c40048
Regis Brid <regisb@microsoft.com>
2021-02-18 19:10:46 -0800

    Mostly BreadcrumbDropDownItem updates

--- dev/Breadcrumb/BreadcrumbDropDownItem.cpp
+++ dev/Breadcrumb/BreadcrumbDropDownItem.cpp
@@ -5,9 +5,6 @@
 #include "common.h"
 #include "BreadcrumbDropDownItem.h"
 #include "RuntimeProfiler.h"
-#include "ResourceAccessor.h"
-
-#include "ItemTemplateWrapper.h"
 #include "BreadcrumbItem.h"
 #include "BreadcrumbDropDownItemAutomationPeer.h"
 
@@ -23,6 +20,7 @@ BreadcrumbDropDownItem::BreadcrumbDropDownItem()
     __RP_Marker_ClassById(RuntimeProfiler::ProfId_BreadcrumbDropDownItem);
 
     SetDefaultStyleKey(this);
+    HookListeners();
 }
 
 BreadcrumbDropDownItem::~BreadcrumbDropDownItem()
@@ -30,57 +28,37 @@ BreadcrumbDropDownItem::~BreadcrumbDropDownItem()
     RevokeListeners();
 }
 
-void BreadcrumbDropDownItem::RevokeListeners()
+void BreadcrumbDropDownItem::HookListeners()
 {
-    m_dropDownItemContentPresenterLoadedRevoker.revoke();
-}
-
-void BreadcrumbDropDownItem::OnApplyTemplate()
-{
-    __super::OnApplyTemplate();
-
-    RevokeListeners();
-
-    winrt::IControlProtected controlProtected{ *this };
-
-    m_dropDownItemContentPresenter.set(GetTemplateChildT<winrt::ContentPresenter>(L"PART_ContentPresenter", controlProtected));
-
     if (auto const& thisAsIUIElement7 = this->try_as<winrt::IUIElement7>())
     {
         thisAsIUIElement7.PreviewKeyDown({ this, &BreadcrumbDropDownItem::OnChildPreviewKeyDown });
     }
     else if (auto const& thisAsUIElement = this->try_as<winrt::UIElement>())
     {
-        m_dropDownItemKeyDownRevoker = AddRoutedEventHandler<RoutedEventType::KeyDown>(thisAsUIElement,
+        m_keyDownRevoker = AddRoutedEventHandler<RoutedEventType::KeyDown>(thisAsUIElement,
             { this, &BreadcrumbDropDownItem::OnChildPreviewKeyDown },
             true /*handledEventsToo*/);
     }
 
-    if (const auto& contentPresenter = m_dropDownItemContentPresenter.get())
-    {
-        m_dropDownItemContentPresenterLoadedRevoker = contentPresenter.Loaded(winrt::auto_revoke, { this, &BreadcrumbDropDownItem::OnLoadedEvent });
-
-        // Register for pointer events so we can keep track of the last used pointer type
-        m_breadcrumbItemPointerEnteredRevoker = PointerEntered(winrt::auto_revoke, { this, &BreadcrumbDropDownItem::OnPointerEnteredEvent });
-        m_breadcrumbItemPointerExitedRevoker = PointerExited(winrt::auto_revoke, { this, &BreadcrumbDropDownItem::OnPointerExitedEvent });
-        m_breadcrumbItemPointerPressedRevoker = PointerPressed(winrt::auto_revoke, { this, &BreadcrumbDropDownItem::OnPointerPressedEvent });
-        m_breadcrumbItemPointerReleasedRevoker = PointerReleased(winrt::auto_revoke, { this, &BreadcrumbDropDownItem::OnPointerReleasedEvent });
-        m_breadcrumbItemPointerCanceledRevoker = PointerCanceled(winrt::auto_revoke, { this, &BreadcrumbDropDownItem::OnPointerReleasedEvent });
-        m_breadcrumbItemPointerCaptureLostRevoker = PointerCaptureLost(winrt::auto_revoke, { this, &BreadcrumbDropDownItem::OnPointerReleasedEvent });
-    }
-       
-    AddHandler(winrt::UIElement::PointerPressedEvent(),
-        winrt::box_value<winrt::PointerEventHandler>({ this, &BreadcrumbDropDownItem::OnClickEvent }),
-        true);
+    m_isEnabledChangedRevoker = IsEnabledChanged(winrt::auto_revoke, { this,  &BreadcrumbDropDownItem::OnIsEnabledChanged });
 }
 
-void BreadcrumbDropDownItem::OnLoadedEvent(const winrt::IInspectable&, const winrt::RoutedEventArgs&)
+void BreadcrumbDropDownItem::RevokeListeners()
 {
+    m_keyDownRevoker.revoke();
+}
+
+void BreadcrumbDropDownItem::OnApplyTemplate()
+{
+    __super::OnApplyTemplate();
+
+    UpdateCommonVisualState(false /*useTransitions*/);
 }
 
 void BreadcrumbDropDownItem::OnChildPreviewKeyDown(const winrt::IInspectable& sender, const winrt::KeyRoutedEventArgs& args)
 {
-    if (args.Key() == winrt::VirtualKey::Enter)
+    if (args.Key() == winrt::VirtualKey::Enter || args.Key() == winrt::VirtualKey::Space)
     {
         this->OnClickEvent(sender, nullptr);
         args.Handled(true);
@@ -109,66 +87,164 @@ void BreadcrumbDropDownItem::SetEllipsisBreadcrumbItem(const winrt::BreadcrumbIt
     m_ellipsisBreadcrumbItem.set(ellipsisBreadcrumbItem);
 }
 
-void BreadcrumbDropDownItem::OnPointerEvent(const winrt::IInspectable& sender, const winrt::PointerRoutedEventArgs& args)
+void BreadcrumbDropDownItem::OnIsEnabledChanged(
+    const winrt::IInspectable&,
+    const winrt::DependencyPropertyChangedEventArgs&)
 {
-    UpdateCommonVisualState();
+    UpdateCommonVisualState(true /*useTransitions*/);
 }
 
-void BreadcrumbDropDownItem::OnPointerEnteredEvent(const winrt::IInspectable&, const winrt::PointerRoutedEventArgs&)
+void BreadcrumbDropDownItem::OnPointerEntered(const winrt::PointerRoutedEventArgs& args)
 {
-    m_isPointerOver = true;
-    UpdateCommonVisualState();
+    __super::OnPointerEntered(args);
+
+    ProcessPointerOver(args);
 }
 
-void BreadcrumbDropDownItem::OnPointerPressedEvent(const winrt::IInspectable&, const winrt::PointerRoutedEventArgs&)
+void BreadcrumbDropDownItem::OnPointerMoved(const winrt::PointerRoutedEventArgs& args)
 {
-    m_isPressed = true;
-    UpdateCommonVisualState();
+    __super::OnPointerMoved(args);
+
+    ProcessPointerOver(args);
 }
 
-void BreadcrumbDropDownItem::OnPointerReleasedEvent(const winrt::IInspectable&, const winrt::PointerRoutedEventArgs& args)
+void BreadcrumbDropDownItem::OnPointerExited(const winrt::PointerRoutedEventArgs& args)
 {
-    OnClickEvent(nullptr, nullptr);
-    m_isPressed = false;
-    UpdateCommonVisualState();
+    __super::OnPointerExited(args);
+
+    ProcessPointerCanceled(args);
 }
 
-void BreadcrumbDropDownItem::OnPointerExitedEvent(const winrt::IInspectable&, const winrt::PointerRoutedEventArgs&)
+void BreadcrumbDropDownItem::OnPointerPressed(const winrt::PointerRoutedEventArgs& args)
 {
-    m_isPointerOver = false;
-    UpdateCommonVisualState();
-}
+    __super::OnPointerPressed(args);
 
-void BreadcrumbDropDownItem::OnVisualPropertyChanged(const winrt::DependencyObject&, const winrt::DependencyProperty&)
-{
-    UpdateCommonVisualState();
-}
-
-void BreadcrumbDropDownItem::UpdateCommonVisualState()
-{
-    if (const auto& contentPresenter = m_dropDownItemContentPresenter.get())
+    if (IgnorePointerId(args))
     {
-        hstring commonVisualStateName = L"";
-
-        if (!IsEnabled())
-        {
-            commonVisualStateName = commonVisualStateName + L"Disabled";
-        }
-        else if (m_isPressed)
-        {
-            commonVisualStateName = commonVisualStateName + L"Pressed";
-        }
-        else if (m_isPointerOver)
-        {
-            commonVisualStateName = commonVisualStateName + L"PointerOver";
-        }
-        else
-        {
-            commonVisualStateName = commonVisualStateName + L"Normal";
-        }
-
-        winrt::VisualStateManager::GoToState(*this, commonVisualStateName, false);
+        return;
     }
+
+    MUX_ASSERT(!m_isPressed);
+
+    if (args.Pointer().PointerDeviceType() == winrt::PointerDeviceType::Mouse)
+    {
+        auto pointerProperties = args.GetCurrentPoint(*this).Properties();
+        m_isPressed = pointerProperties.IsLeftButtonPressed();
+    }
+    else
+    {
+        m_isPressed = true;
+    }
+
+    if (m_isPressed)
+    {
+        UpdateCommonVisualState(true /*useTransitions*/);
+    }
+}
+
+void BreadcrumbDropDownItem::OnPointerReleased(const winrt::PointerRoutedEventArgs& args)
+{
+    __super::OnPointerReleased(args);
+
+    if (IgnorePointerId(args))
+    {
+        return;
+    }
+
+    if (m_isPressed)
+    {
+        m_isPressed = false;
+        UpdateCommonVisualState(true /*useTransitions*/);
+        OnClickEvent(nullptr, nullptr);
+    }
+}
+
+void BreadcrumbDropDownItem::OnPointerCanceled(const winrt::PointerRoutedEventArgs& args)
+{
+    __super::OnPointerCanceled(args);
+
+    ProcessPointerCanceled(args);
+}
+
+void BreadcrumbDropDownItem::OnPointerCaptureLost(const winrt::PointerRoutedEventArgs& args)
+{
+    __super::OnPointerCaptureLost(args);
+
+    ProcessPointerCanceled(args);
+}
+
+void BreadcrumbDropDownItem::ProcessPointerOver(const winrt::PointerRoutedEventArgs& args)
+{
+    if (IgnorePointerId(args))
+    {
+        return;
+    }
+
+    if (!m_isPointerOver)
+    {
+        m_isPointerOver = true;
+        UpdateCommonVisualState(true /*useTransitions*/);
+    }
+}
+
+void BreadcrumbDropDownItem::ProcessPointerCanceled(const winrt::PointerRoutedEventArgs& args)
+{
+    if (IgnorePointerId(args))
+    {
+        return;
+    }
+
+    m_isPressed = false;
+    m_isPointerOver = false;
+    ResetTrackedPointerId();
+    UpdateCommonVisualState(true /*useTransitions*/);
+}
+
+void BreadcrumbDropDownItem::ResetTrackedPointerId()
+{
+    m_trackedPointerId = 0;
+}
+
+// Returns False when the provided pointer Id matches the currently tracked Id.
+// When there is no currently tracked Id, sets the tracked Id to the provided Id and returns False.
+// Returns True when the provided pointer Id does not match the currently tracked Id.
+bool BreadcrumbDropDownItem::IgnorePointerId(const winrt::PointerRoutedEventArgs& args)
+{
+    uint32_t pointerId = args.Pointer().PointerId();
+
+    if (m_trackedPointerId == 0)
+    {
+        m_trackedPointerId = pointerId;
+    }
+    else if (m_trackedPointerId != pointerId)
+    {
+        return true;
+    }
+    return false;
+}
+
+void BreadcrumbDropDownItem::UpdateCommonVisualState(bool useTransitions)
+{
+    hstring commonVisualStateName;
+
+    if (!IsEnabled())
+    {
+        commonVisualStateName = s_disabledStateName;
+    }
+    else if (m_isPressed)
+    {
+        commonVisualStateName = s_pressedStateName;
+    }
+    else if (m_isPointerOver)
+    {
+        commonVisualStateName = s_pointerOverStateName;
+    }
+    else
+    {
+        commonVisualStateName = s_normalStateName;
+    }
+
+    winrt::VisualStateManager::GoToState(*this, commonVisualStateName, useTransitions);
 }
 
 winrt::AutomationPeer BreadcrumbDropDownItem::OnCreateAutomationPeer()
