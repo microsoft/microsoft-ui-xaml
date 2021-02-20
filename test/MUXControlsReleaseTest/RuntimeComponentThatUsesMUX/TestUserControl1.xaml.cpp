7cfbebd5586aef3c8ccad6888a3818bd3a416941
Luke Longley <18177025+llongley@users.noreply.github.com>
2021-02-19 19:18:57 -0800

    Adding to a test app to exercise these changes in the release pipeline.

---
+++ test/MUXControlsReleaseTest/RuntimeComponentThatUsesMUX/TestUserControl1.xaml.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "TestUserControl1.xaml.h"
+#if __has_include("TestUserControl1.g.cpp")
+#include "TestUserControl1.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::RuntimeComponentThatUsesMUX::implementation
+{
+    TestUserControl1::TestUserControl1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ test/MUXControlsReleaseTest/RuntimeComponentThatUsesMUX/TestUserControl1.xaml.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "winrt/Windows.UI.Xaml.h"
+#include "winrt/Windows.UI.Xaml.Markup.h"
+#include "winrt/Windows.UI.Xaml.Controls.Primitives.h"
+#include "TestUserControl1.g.h"
+
+namespace winrt::RuntimeComponentThatUsesMUX::implementation
+{
+    struct TestUserControl1 : TestUserControl1T<TestUserControl1>
+    {
+        TestUserControl1();
+    };
+}
+
+namespace winrt::RuntimeComponentThatUsesMUX::factory_implementation
+{
+    struct TestUserControl1 : TestUserControl1T<TestUserControl1, implementation::TestUserControl1>
+    {
+    };
+}
