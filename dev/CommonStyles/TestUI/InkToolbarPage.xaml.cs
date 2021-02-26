c6a1b38616858b4d41ad15aa33d8c85c53ae8ea5
Vivek Sharma <vivsha@microsoft.com>
2021-02-25 18:59:58 -0800

    Added InkToolbar Changes

--- dev/CommonStyles/TestUI/InkToolbarPage.xaml.cs
+++ dev/CommonStyles/TestUI/InkToolbarPage.xaml.cs
@@ -1,25 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace CommonStyles_TestUI
+namespace MUXControlsTestApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    [TopLevelTestPage(Name = "InkToolbar")]
     public sealed partial class InkToolbarPage : Page
     {
         public InkToolbarPage()
