c4aab78c73cd3be7048ea3223774dfb7a66c2932
Vivek Sharma <vivsha@microsoft.com>
2021-03-10 20:19:35 -0800

    Added code to write on Ink Canvas

--- dev/CommonStyles/TestUI/InkToolbarPage.xaml.cs
+++ dev/CommonStyles/TestUI/InkToolbarPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Windows.UI.Core;
+using Windows.UI.Input.Inking;
 using Windows.UI.Xaml.Controls;
 
 namespace MUXControlsTestApp
@@ -8,9 +10,14 @@ namespace MUXControlsTestApp
     [TopLevelTestPage(Name = "InkToolbar")]
     public sealed partial class InkToolbarPage : Page
     {
+        private InkPresenter _inkPresenter;
+
         public InkToolbarPage()
         {
             this.InitializeComponent();
+            _inkPresenter = inkCanvas.InkPresenter;
+            _inkPresenter.InputDeviceTypes =
+            CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Touch;
         }
     }
 }
