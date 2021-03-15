a4c37149e4763ef65b46b6c37732189701e595e8
Luke Longley <18177025+llongley@users.noreply.github.com>
2021-03-15 03:34:30 -0700

    Fix WACK certification error for transitive WinUI references (#4218)
    WACK expects to be able to find WinMDs for type resolution purposes in the root of the AppX, but when WinUI 2 is referenced via framework package, the WinMD is instead placed in a WinMetadata subdirectory. As a result, applications that reference nuget packages with references to WinUI 2, WACK certification fails. I'm not sure whether placing the WinMD file in that directory is still needed, so to work around this issue in the safest manner, this adds an additional build target that places the WinMD where WACK certification needs it to be.
    
    An example of an issue that will be resolved by this fix: windows-toolkit/Lottie-Windows#214

---
+++ test/MUXControlsReleaseTest/RuntimeComponentThatUsesMUX/MUXCInterfaceImplementation.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "MUXCInterfaceImplementation.h"
+#if __has_include("MUXCInterfaceImplementation.g.cpp")
+#include "MUXCInterfaceImplementation.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::RuntimeComponentThatUsesMUX::implementation
+{
+    MUXCInterfaceImplementation::MUXCInterfaceImplementation()
+    {
+    }
+
+    winrt::Microsoft::UI::Xaml::Controls::IAnimatedVisual MUXCInterfaceImplementation::TryCreateAnimatedVisual(
+        winrt::Windows::UI::Composition::Compositor const&,
+        winrt::Windows::Foundation::IInspectable&)
+    {
+        return nullptr;
+    }
+}
