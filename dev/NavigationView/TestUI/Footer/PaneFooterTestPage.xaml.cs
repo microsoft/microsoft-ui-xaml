f3dd3e9546d0403fd6a9cb9cad796ef0d81132d7
Karen Lai <biilai@microsoft.com>
2021-03-24 11:33:06 -0700

    add paneFooter testpage

---
+++ dev/NavigationView/TestUI/Footer/PaneFooterTestPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using NavigationViewPaneDisplayMode = Microsoft.UI.Xaml.Controls.NavigationViewPaneDisplayMode;
+
+namespace MUXControlsTestApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PaneFooterTestPage : TestPage
+    {
+        public PaneFooterTestPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void FlipOrientation_Click(object sender, RoutedEventArgs e)
+        {
+            NavView.PaneDisplayMode = NavView.PaneDisplayMode == NavigationViewPaneDisplayMode.Top ? NavigationViewPaneDisplayMode.Auto : NavigationViewPaneDisplayMode.Top;
+        }
+    }
+}
