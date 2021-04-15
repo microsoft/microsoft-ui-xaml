50bd44b1452dc9b719e57d4662586d23ea6815ea
Jevan Saks <jevansa@microsoft.com>
2021-04-15 12:37:06 -0700

    huh

--- tools/InternalWindowsSDKNuget/SetUpInternalWindowsSDK.cmd
+++ tools/InternalWindowsSDKNuget/SetUpInternalWindowsSDK.cmd
@@ -5,5 +5,7 @@ set ERRORLEVEL=
 echo executing %~dp0..\..\packages\InternalWindowsSDK\winsdksetup.exe /quiet
 call %~dp0..\..\packages\InternalWindowsSDK\winsdksetup.exe /quiet
 
+dir "c:\program files (x86)\windows kits\10\References"
+
 :END
 exit /B %ERRORLEVEL%
