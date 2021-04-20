ef2017bf459838ca2ff1fc10ee406ad706b5d5e0
Luke Longley <18177025+llongley@users.noreply.github.com>
2021-04-19 21:18:54 -0700

    Changing so we always install AppX certificates before we install the AppX itself.

--- build/Helix/InstallTestAppDependencies.ps1
+++ build/Helix/InstallTestAppDependencies.ps1
@@ -2,30 +2,48 @@
 $ProgressPreference = "SilentlyContinue"
 
 $dependencyFiles = Get-ChildItem -Filter "*dependencies.txt"
+$dependencies = [System.Collections.Generic.List[string]]@()
 
 foreach ($file in $dependencyFiles)
 {
-    Write-Host "Adding dependencies in $file..."
+    Write-Host "Collecting dependencies for $file..."
 
     foreach ($line in Get-Content $file)
     {
-    	Write-Host "    Adding dependency $line..."
-        Add-AppxPackage $line -ErrorVariable appxerror -ErrorAction SilentlyContinue
-
-        if($appxerror)
+        if (-not $dependencies.Contains($line))
         {
-            foreach($error in $appxerror)
+            $dependencies.Add($line)
+        }
+    }
+}
+
+foreach ($dependency in $dependencies)
+{
+    Write-Host "Adding AppX dependency $dependency..."
+    
+    Write-Host "    Retrieving certificate..."
+    $dependencyCertFile = "$env:TEMP\$($dependency.Replace(".appx", ".cer"))"
+    [System.IO.File]::WriteAllBytes($dependencyCertFile, (Get-AuthenticodeSignature $dependency).SignerCertificate.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert))
+    
+    Write-Host "    Installing certificate..."
+    Start-Process "certutil.exe" -ArgumentList "-addStore TrustedPeople $dependencyCertFile" -NoNewWindow -Wait
+    
+    Write-Host "    Installing AppX..."
+    Add-AppxPackage $dependency -ErrorVariable appxerror -ErrorAction SilentlyContinue
+
+    if ($appxerror)
+    {
+        foreach ($error in $appxerror)
+        {
+            # In the case where the package does not install becasuse a higher version is already installed
+            # we don't want to print an error message, since that is just noise. Filter out such errors.
+            if($error.Exception.Message -match "0x80073D06")
             {
-                # In the case where the package does not install becasuse a higher version is already installed
-                # we don't want to print an error message, since that is just noise. Filter out such errors.
-                if($error.Exception.Message -match "0x80073D06")
-                {
-                    Write-Host "A higher version of this package is already installed."
-                }
-                else
-                {
-                    Write-Error $error
-                }
+                Write-Host "A higher version of this package is already installed."
+            }
+            else
+            {
+                Write-Error $error
             }
         }
     }
