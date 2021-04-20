5b757e8e9f2dc7c5648ad45364c327337f279dcb
Keith Mahoney <41657372+kmahone@users.noreply.github.com>
2021-04-20 11:51:47 -0700

    Refactor WinUI Helix functionality into reusable WinUI.Helix nuget package (#4678)

--- build/Helix/PrepareHelixPayload.ps1
+++ build/Helix/PrepareHelixPayload.ps1
@@ -1,11 +1,16 @@
 [CmdLetBinding()]
 Param(
-    [string]$Platform,
-    [string]$Configuration,
+    [ValidateSet("x86", "x64")]
+    [string]$Platform = "x86",
+
+    [ValidateSet("Debug", "Release")]
+    [string]$Configuration= "Debug",
+
     [string]$ArtifactName='drop'
 )
 
 $payloadDir = "HelixPayload\$Configuration\$Platform"
+
 
 $repoDirectory = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "..\..\"
 $nugetPackagesDir = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "packages"
@@ -17,14 +22,29 @@ If(test-path $payloadDir)
 }
 New-Item -ItemType Directory -Force -Path $payloadDir
 
+
+[xml]$pkgVerData = (Get-Content "$PSScriptRoot\packages.config")
+$winuiHelixVer = $pkgVerData.SelectSingleNode("//packages/package[@id=`"Microsoft.Internal.WinUI.Helix`"]").version
+$mitaVer = $pkgVerData.SelectSingleNode("//packages/package[@id=`"microsoft.windows.apps.test`"]").version
+$taefVer = $pkgVerData.SelectSingleNode("//packages/package[@id=`"TAEF.Redist.Wlk`"]").version
+$muxcustomBuildTasksVer = $pkgVerData.SelectSingleNode("//packages/package[@id=`"MUXCustomBuildTasks`"]").version
+$netCoreAppVer = $pkgVerData.SelectSingleNode("//packages/package[@id=`"runtime.win-$Platform.microsoft.netcore.app`"]").version
+
+Write-Host "winuiHelixVer = $winuiHelixVer"
+Write-Host "mitaVer = $mitaVer"
+Write-Host "taefVer = $taefVer"
+Write-Host "muxcustomBuildTasksVer = $muxcustomBuildTasksVer"
+Write-Host "netCoreAppVer = $netCoreAppVer"
+
 # Copy files from nuget packages
-Copy-Item "$nugetPackagesDir\microsoft.windows.apps.test.1.0.181203002\lib\netcoreapp2.1\*.dll" $payloadDir
-Copy-Item "$nugetPackagesDir\taef.redist.wlk.10.31.180822002\build\Binaries\$Platform\*" $payloadDir
-Copy-Item "$nugetPackagesDir\taef.redist.wlk.10.31.180822002\build\Binaries\$Platform\CoreClr\*" $payloadDir
+Copy-Item "$nugetPackagesDir\Microsoft.Internal.WinUI.Helix.$winuiHelixVer\scripts\test\*" $payloadDir
+Copy-Item "$nugetPackagesDir\microsoft.windows.apps.test.$mitaVer\lib\netcoreapp2.1\*.dll" $payloadDir
+Copy-Item "$nugetPackagesDir\taef.redist.wlk.$taefVer\build\Binaries\$Platform\*" $payloadDir
+Copy-Item "$nugetPackagesDir\taef.redist.wlk.$taefVer\build\Binaries\$Platform\CoreClr\*" $payloadDir
 New-Item -ItemType Directory -Force -Path "$payloadDir\.NETCoreApp2.1\"
-Copy-Item "$nugetPackagesDir\runtime.win-$Platform.microsoft.netcore.app.2.1.0\runtimes\win-$Platform\lib\netcoreapp2.1\*" "$payloadDir\.NETCoreApp2.1\"
-Copy-Item "$nugetPackagesDir\runtime.win-$Platform.microsoft.netcore.app.2.1.0\runtimes\win-$Platform\native\*" "$payloadDir\.NETCoreApp2.1\"
-Copy-Item "$nugetPackagesDir\MUXCustomBuildTasks.1.0.71\tools\$platform\WttLog.dll" $payloadDir
+Copy-Item "$nugetPackagesDir\runtime.win-$Platform.microsoft.netcore.app.$netCoreAppVer\runtimes\win-$Platform\lib\netcoreapp2.1\*" "$payloadDir\.NETCoreApp2.1\"
+Copy-Item "$nugetPackagesDir\runtime.win-$Platform.microsoft.netcore.app.$netCoreAppVer\runtimes\win-$Platform\native\*" "$payloadDir\.NETCoreApp2.1\"
+Copy-Item "$nugetPackagesDir\MUXCustomBuildTasks.$muxcustomBuildTasksVer\tools\$platform\WttLog.dll" $payloadDir
 
 function Copy-If-Exists
 {
@@ -84,12 +104,5 @@ Copy-Recursively-If-Exists "$repoDirectory\Artifacts\$ArtifactName\$Configuratio
 
 # Copy files from the repo
 New-Item -ItemType Directory -Force -Path "$payloadDir"
-Copy-Item "build\helix\ConvertWttLogToXUnit.ps1" "$payloadDir"
-Copy-Item "build\helix\OutputFailedTestQuery.ps1" "$payloadDir"
-Copy-Item "build\helix\OutputSubResultsJsonFiles.ps1" "$payloadDir"
-Copy-Item "build\helix\HelixTestHelpers.cs" "$payloadDir"
-Copy-Item "build\helix\runtests.cmd" $payloadDir
-Copy-Item "build\helix\InstallTestAppDependencies.ps1" "$payloadDir"
-Copy-Item "build\Helix\EnsureMachineState.ps1" "$payloadDir"
+Copy-Item "build\helix\scripts\*" "$payloadDir"
 Copy-Item "version.props" "$payloadDir"
-Copy-Item "build\Helix\CopyVisualTreeVerificationFiles.ps1" "$payloadDir"
