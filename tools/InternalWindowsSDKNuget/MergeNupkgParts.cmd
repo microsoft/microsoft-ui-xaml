9a74d1633bcc8ae4e486ba59af1337c777003693
Régis Brid <36547063+RBrid@users.noreply.github.com>
2021-04-27 08:44:10 -0700

    Upgrading version of internal Windows SDK to  (#4903)
    * Fixing NavigationView verification files
    
    * Upgrading internal Windows SDK to version 21361.

--- tools/InternalWindowsSDKNuget/MergeNupkgParts.cmd
+++ tools/InternalWindowsSDKNuget/MergeNupkgParts.cmd
@@ -2,11 +2,11 @@
 SETLOCAL
 set ERRORLEVEL=
 
-echo robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part1.1.0.3\windowssdk-21330.1000.210303-1505-part1 %~dp0..\..\packages\InternalWindowsSDK.1.0.3
-call robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part1.1.0.3\windowssdk-21330.1000.210303-1505-part1 %~dp0..\..\packages\InternalWindowsSDK.1.0.3
+echo robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part1.1.0.5\windowssdk-21361.1000.210413-1700-part1 %~dp0..\..\packages\InternalWindowsSDK.1.0.5
+call robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part1.1.0.5\windowssdk-21361.1000.210413-1700-part1 %~dp0..\..\packages\InternalWindowsSDK.1.0.5
 
-echo robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part2.1.0.3\windowssdk-21330.1000.210303-1505-part2 %~dp0..\..\packages\InternalWindowsSDK.1.0.3
-call robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part2.1.0.3\windowssdk-21330.1000.210303-1505-part2 %~dp0..\..\packages\InternalWindowsSDK.1.0.3
+echo robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part2.1.0.5\windowssdk-21361.1000.210413-1700-part2 %~dp0..\..\packages\InternalWindowsSDK.1.0.5
+call robocopy /E %~dp0..\..\packages\InternalWindowsSDK-part2.1.0.5\windowssdk-21361.1000.210413-1700-part2 %~dp0..\..\packages\InternalWindowsSDK.1.0.5
 
 REM robocopy returns:
 REM - 0x0 for no files copied,
