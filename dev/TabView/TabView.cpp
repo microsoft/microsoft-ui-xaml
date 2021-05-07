7f665bc602ecf8ad632c5138c03c37b2348995a9
beervoley <sevkorobot@gmail.com>
2021-05-06 19:36:35 -0700

    add copyright notes

--- dev/TabView/TabView.cpp
+++ dev/TabView/TabView.cpp
@@ -1,4 +1,5 @@
-﻿
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #include "pch.h"
 #include "common.h"
