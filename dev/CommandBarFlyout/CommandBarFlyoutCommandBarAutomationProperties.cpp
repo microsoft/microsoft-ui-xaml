8241b5ca297c0490a5740369ecc3c5748f3a3877
Luke Longley <18177025+llongley@users.noreply.github.com>
2021-05-21 12:20:02 -0700

    Consuming AutomationProperties.ControlType.

---
+++ dev/CommandBarFlyout/CommandBarFlyoutCommandBarAutomationProperties.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
+#include "common.h"
+#include "CommandBarFlyoutCommandBarAutomationProperties.h"
+
+// Forward changes to CommandBarFlyoutCommandBarAutomationProperties.ControlType on to
+// AutomationProperties.ControlType.  We need to do this indirection since we can't
+// access types in XAML that we're interacting with via a known interface GUID.
+/* static */ void CommandBarFlyoutCommandBarAutomationProperties::OnControlTypePropertyChanged(
+    const winrt::DependencyObject& sender,
+    const winrt::DependencyPropertyChangedEventArgs& args)
+{
+    winrt::IActivationFactory automationPropertiesStatics = winrt::get_activation_factory(L"Windows.UI.Xaml.Automation.AutomationProperties");
+
+    if (auto automationPropertiesStatics9 = automationPropertiesStatics.try_as<winrt::IAutomationPropertiesStatics9>())
+    {
+        automationPropertiesStatics9.SetControlType(sender, unbox_value<winrt::AutomationControlType>(args.NewValue()));
+    }
+}
