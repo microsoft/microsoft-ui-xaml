d41b46a8ef90e8c23f049f1e082d96994263ce5b
Stephen Peters <stpete@microsoft.com>
2021-06-28 14:58:15 -0700

    Save State

---
+++ dev/NavigationView/TestUI/NavigationViewBackButton.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Windows.UI.Xaml;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MUXControlsTestApp
+{
+    [TopLevelTestPage(Name = "BackButton")]
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NavigationViewBackButton : TestPage
+    {
+        public NavigationViewBackButton()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
