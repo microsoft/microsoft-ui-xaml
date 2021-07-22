ce762662d8cf2c6963ada49f076df0c3ff64a843
Ana Wishnoff <anawish@microsoft.com>
2021-07-22 13:49:57 -0400

    Website: updating code to get latest header/footer (#5527)
    * trying some things out
    
    * Got skip to main content and header working
    
    * Cleaning up js file

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-
 var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
+xhttp.onreadystatechange =  function() {
     if (this.readyState == 4 && this.status == 200) {
         var xmlResp = this.responseXML;
-        var header_markup = xmlResp.getElementsByTagName("headerHtml")[0];
-        document.getElementById("UHF-header").innerHTML = header_markup.textContent;
 
+        var header_markup = xmlResp.getElementsByTagName("headerHtml")[0];
         var footer_markup = xmlResp.getElementsByTagName("footerHtml")[0];
-        document.getElementById("UHF-footer").innerHTML = footer_markup.textContent;
+
+
+        if (document.getElementById("UHF-header") != null){
+            document.getElementById("UHF-header").innerHTML += header_markup.textContent;
+        }
+        else {
+            document.getElementById("UHF-header-about").innerHTML += header_markup.textContent;
+        }
+
+        if (document.getElementById("UHF-footer") != null){
+            document.getElementById("UHF-footer").innerHTML += footer_markup.textContent;
+        }
+        else{
+            document.getElementById("UHF-footer-about").innerHTML += footer_markup.textContent;
+        }
+
+        var css_markup = xmlResp.getElementsByTagName("cssIncludes")[0];
+
+        var head = document.getElementsByTagName("head")[0];
+        head.innerHTML += css_markup.textContent;
+        console.log(header_markup.textContent);
     }
 };
+   
 xhttp.open("GET", "https://uhf.microsoft.com/en-US/shell/xml/MSWinUI?headerId=MSWinUIHeader&footerid=MSWinUIFooter&CookieComplianceEnabled=true", true);
 xhttp.send();
-
-function Include(type, src) {
-    var head = document.getElementsByTagName('head')[0];
-    var link;
-    if (type.indexOf('css') > -1) {
-        link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = src;
-    } else {
-        link = document.createElement('script')
-        link.src = src;
-    }
-    link.type = type;
-    head.appendChild(link);
-}
-
-fetch('https://uhf.microsoft.com/en-us/shell/api/mscc?sitename=WinUI&domain=https://microsoft.github.io/microsoft-ui-xaml/&country=detect')
-.then(response => response.json())
-.then(function (data) {
-    document.body.insertAdjacentHTML('beforebegin', data.Markup)
-    Include('text/css', data.Css);
-    Include('text/javascript', data.Js);
-});
 
