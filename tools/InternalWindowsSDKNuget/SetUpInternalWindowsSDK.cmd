3ec89e949c3b7bde42b26a392b66f2f1ce987a40
Keith Mahoney <41657372+kmahone@users.noreply.github.com>
2021-08-12 13:16:35 -0700

    Fix source indexing to enable source debugging for official builds. (#5669)

--- tools/InternalWindowsSDKNuget/SetUpInternalWindowsSDK.cmd
+++ tools/InternalWindowsSDKNuget/SetUpInternalWindowsSDK.cmd
@@ -2,8 +2,8 @@
 SETLOCAL
 set ERRORLEVEL=
 
-echo executing %~dp0..\..\packages\InternalWindowsSDK.1.0.7\winsdksetup.exe /quiet
-call %~dp0..\..\packages\InternalWindowsSDK.1.0.7\winsdksetup.exe /quiet
+echo executing %~dp0..\..\packages\InternalWindowsSDK.1.0.7\winsdksetup.exe /features OptionId.UWPCpp OptionId.DesktopCPPx64 OptionId.DesktopCPPx86 OptionId.DesktopCPPARM64 OptionId.DesktopCPPARM /quiet
+call %~dp0..\..\packages\InternalWindowsSDK.1.0.7\winsdksetup.exe /features OptionId.UWPCpp OptionId.DesktopCPPx64 OptionId.DesktopCPPx86 OptionId.DesktopCPPARM64 OptionId.DesktopCPPARM /quiet
 
 :END
 exit /B %ERRORLEVEL%
