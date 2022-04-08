84e30d9cf6fb0a07239cb753325421d7fc7ea659
Karen Lai <7976322+karenbtlai@users.noreply.github.com>
2022-04-08 11:11:26 -0700

    Update Sample to include MenuFlyout Invocation at Cursor Position (#6835)
    * update test page with cursor positioned menu flyout
    
    * changes from comments

--- dev/CommonStyles/TestUI/MenuFlyoutPage.xaml.cs
+++ dev/CommonStyles/TestUI/MenuFlyoutPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Windows.Foundation;
 using Windows.Foundation.Metadata;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -12,15 +13,32 @@ namespace MUXControlsTestApp
     [TopLevelTestPage(Name = "MenuFlyout", Icon = "MenuFlyout.png")]
     public sealed partial class MenuFlyoutPage : TestPage
     {
+        private MenuFlyout sharedFlyout;
         public MenuFlyoutPage()
         {
             this.InitializeComponent();
+
+            sharedFlyout = (MenuFlyout)Resources["SampleContextMenu"];
         }
 
         private void TestMenuFlyoutItemClick(object sender, object e)
         {
             TestMenuFlyoutItemHeightTextBlock.Text = $"{TestMenuFlyoutItem.ActualHeight}";
             TestMenuFlyoutItemWidthTextBlock.Text = $"{TestMenuFlyoutItem.ActualWidth}";
+        }
+
+        private void Grid_ContextRequested(UIElement sender, ContextRequestedEventArgs args)
+        {
+            var requestedElement = sender as FrameworkElement;
+
+            if (args.TryGetPosition(requestedElement, out Point point))
+            {
+                sharedFlyout.ShowAt(requestedElement, point);
+            }
+            else
+            {
+                sharedFlyout.ShowAt(requestedElement);
+            }
         }
     }
 }
