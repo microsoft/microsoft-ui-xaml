94ce1f5771f23101947e245715050efe0a1a0c71
Matthew Machuca <t-mamachuca@microsoft.com>
2022-06-28 15:43:15 -0700

    began adding c++ flexbox implementation, need to address namespace errors

---
+++ dev/Repeater/FlexboxLayout.h
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "NonVirtualizingLayout.h"
+#include "FlexboxLayout.g.h"
+#include "FlexboxLayout.properties.h"
+
+class FlexboxLayout :
+    public ReferenceTracker<FlexboxLayout, winrt::implementation::FlexboxLayoutT, NonVirtualizingLayout>,
+    public FlexboxLayoutProperties
+{
+public:
+    FlexboxLayout();
+
+    winrt::FlexboxWrap Wrap();
+    void Wrap(winrt::FlexboxWrap const& value);
+
+    winrt::FlexboxDirection Direction();
+    void Direction(winrt::FlexboxDirection const& value);
+
+    winrt::FlexboxJustifyContent JustifyContent();
+    void JustifyContent(winrt::FlexboxJustifyContent const& value);
+
+    winrt::FlexboxAlignItems AlignItems();
+    void AlignItems(winrt::FlexboxAlignItems const& value);
+
+    winrt::FlexboxAlignContent AlignContent();
+    void AlignContent(winrt::FlexboxAlignContent const& value);
+
+#pragma region INonVirtualizingLayoutOverrides
+    void InitializeForContextCore(winrt::LayoutContext const& context);
+    void UninitializeForContextCore(winrt::LayoutContext const& context);
+
+    winrt::Size MeasureOverride(winrt::NonVirtualizingLayoutContext const& context, winrt::Size const& availableSize);
+    winrt::Size ArrangeOverride(winrt::NonVirtualizingLayoutContext const& context, winrt::Size const& finalSize);
+#pragma endregion
+
+    static void OnChildPropertyChanged(
+        const winrt::DependencyObject& sender,
+        const winrt::DependencyPropertyChangedEventArgs& args);
+
+    void OnPropertyChanged(const winrt::DependencyPropertyChangedEventArgs& args);
+
+private:
+    bool IsHorizontal();
+    bool IsReversed();
+    bool IsWrapping();
+    float MainAxis(winrt::Size const& value);
+    float CrossAxis(winrt::Size const& value);
+    winrt::Size CreateSize(float mainAxis, float crossAxis);
+    winrt::Point CreatePoint(float mainAxis, float crossAxis);
+    std::vector<winrt::UIElement> ChildrenSortedByOrder(winrt::NonVirtualizingLayoutContext const& context);
+
+    winrt::FlexboxWrap m_wrap;
+    winrt::FlexboxDirection m_direction;
+    winrt::FlexboxJustifyContent m_justifyContent;
+    winrt::FlexboxAlignItems m_alignItems;
+    winrt::FlexboxAlignContent m_alignContent;
+};
---
+++ dev/Repeater/FlexboxLayoutState.h
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+class FlexboxLayoutState :
+    public winrt::implements<FlexboxLayoutState, winrt::IInspectable>
+{
+public:
+    struct RowMeasureInfo
+    {
+    public:
+        float MainAxis;
+        float CrossAxis;
+        unsigned int Count;
+        float Grow;
+    };
+
+    std::vector<RowMeasureInfo> Rows;
+};
