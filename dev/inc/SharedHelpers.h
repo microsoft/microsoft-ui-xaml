4b1116875f55bb4563844cf4f9c4d534aa31132b
Luke Longley <18177025+llongley@users.noreply.github.com>
2022-07-21 16:44:03 -0700

    Removing a stray whitespace.

--- dev/inc/SharedHelpers.h
+++ dev/inc/SharedHelpers.h
@@ -1,5 +1,4 @@
-﻿
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 #pragma once
