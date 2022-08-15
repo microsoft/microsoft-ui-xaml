b6922635e4d30564d6683ea2dc45d68d197772ef
Luke Longley <18177025+llongley@users.noreply.github.com>
2022-08-15 13:25:01 -0700

    Cherry-picking a NavigationView crash fix to 2.7
    This cherry-picks [6240](https://github.com/microsoft/microsoft-ui-xaml/pull/6240) to 2.7, which is needed to fix a crash related to NavigationView.

--- dev/NavigationView/NavigationView.h
+++ dev/NavigationView/NavigationView.h
@@ -189,7 +189,8 @@ private:
     void ClearNavigationViewItemRevokers(const winrt::NavigationViewItem& nvi);
     void ClearAllNavigationViewItemRevokers() noexcept;
     void RevokeNavigationViewItemRevokers(const winrt::NavigationViewItem& nvi);
-    std::set<winrt::NavigationViewItem> m_itemsWithRevokerObjects;
+    std::set<winrt::NavigationViewItem> m_itemsWithRevokerObjects{};
+    std::once_flag s_NavigationViewItemRevokersPropertySet;
 
     void InvalidateTopNavPrimaryLayout();
     // Measure functions for top navigation   
