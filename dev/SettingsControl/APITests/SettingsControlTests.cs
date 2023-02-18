b1e6e0ce26fca4d7576aa4bb5d827a28fbe2f4c4
U+5BFA <onein528@outlook.com>
2023-02-18 17:18:34 +0900

    Initialized SettingsControls projects

---
+++ dev/SettingsControl/APITests/SettingsControlTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Common;
+using Microsoft.UI.Xaml.Controls;
+using MUXControlsTestApp.Utilities;
+using Microsoft.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Automation.Provider;
+using Windows.UI.Xaml.Automation.Peers;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Automation;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Controls;
+#if USING_TAEF
+using WEX.TestExecution;
+using WEX.TestExecution.Markup;
+using WEX.Logging.Interop;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+#endif
+
+namespace Windows.UI.Xaml.Tests.MUXControls.ApiTests
+{
+    [TestClass]
+    public class SettingsControlTests : ApiTestBase
+    {
+    }
+}
