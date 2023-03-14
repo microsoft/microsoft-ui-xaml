2fc53c0fb48b93ac67e1c67c4960a0790494aefa
Keith Mahoney <Keith.Mahoney@microsoft.com>
2023-03-14 16:39:55 -0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- tools/MUXPGODatabase/version.ps1
+++ tools/MUXPGODatabase/version.ps1
@@ -92,10 +92,7 @@ function CompareRevisions ( $version1, $version2 )
 function GetForkPoint ( $pgoBranch )
 {
     Write-Host "!!!! git log --oneline -n 100"
-    (git log --oneline -n 100 --decorate=full) | ForEach-Object 
-    {
-        Write-Host $_
-    }
+    Write-Host (git log --oneline -n 100 --decorate=full)
     
     # Write-Host "!!!! git fetch origin"
     # git fetch origin
