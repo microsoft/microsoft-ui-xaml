75291c497655864ad0b765bfd5879d6dec6d0307
Keith Mahoney <kmahone@microsoft.com>
2023-08-03 21:31:42 +0000

    Merged PR 9387077: Temporarily hard-code PGO package to 2.8 until we can fix PGO pipeline for 2.9
    Temporarily hard-code PGO package to 2.8 until we can fix PGO pipeline for 2.9

--- tools/MUXPGODatabase/config.ps1
+++ tools/MUXPGODatabase/config.ps1
@@ -3,7 +3,10 @@ $packageId           = "MUXPGODatabase"
 
 # Get release version
 [xml] $customProps   = ( Get-Content "..\..\version.props" )
-$releaseVersionMajor      = ( [int64]::Parse( $customProps.GetElementsByTagName("MUXVersionMajor").'#text' ) )
-$releaseVersionMinor      = ( [int64]::Parse( $customProps.GetElementsByTagName("MUXVersionMinor").'#text' ) )
+# $releaseVersionMajor      = ( [int64]::Parse( $customProps.GetElementsByTagName("MUXVersionMajor").'#text' ) )
+# $releaseVersionMinor      = ( [int64]::Parse( $customProps.GetElementsByTagName("MUXVersionMinor").'#text' ) )
+# Temporarily hard-code to 2.8 until we can fix the PGO Pipeline for 2.9
+$releaseVersionMajor      = [int64]2
+$releaseVersionMinor      = [int64]8
 $releaseVersionPatch      = [int64] 0
 $releaseVersionPrerelease = $null
