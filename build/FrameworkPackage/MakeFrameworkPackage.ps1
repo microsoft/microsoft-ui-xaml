ee39a9d6004bca459b7536899e8084c3b10cfa98
Keith Mahoney <you@example.com>
2024-08-29 16:12:29 -0700

    fixes

--- build/FrameworkPackage/MakeFrameworkPackage.ps1
+++ build/FrameworkPackage/MakeFrameworkPackage.ps1
@@ -317,7 +317,19 @@ $priOutputPath = [IO.Path]::GetFullPath("$fullOutputPath\resources.pri")
 $priCBSOutputPath = [IO.Path]::GetFullPath("$fullOutputPath\CBSresources.pri")
 $noiseAssetPath = [IO.Path]::GetFullPath("$fullOutputPath\Assets\NoiseAsset_256x256_PNG.png")
 $resourceContents = [IO.Path]::GetFullPath("$fullOutputPath\Resources")
-$pfxPath = [IO.Path]::GetFullPath("..\WinUITest.pfx")
+
+$pfxPath = [IO.Path]::GetFullPath("MSTest.pfx")
+$CertificateFriendlyName = "MSTest"
+
+$cert = New-SelfSignedCertificate -Type Custom `
+    -Subject $Publisher `
+    -KeyUsage DigitalSignature `
+    -FriendlyName $CertificateFriendlyName `
+    -CertStoreLocation "Cert:\CurrentUser\My" `
+    -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+
+$certificateBytes = $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12)
+[System.IO.File]::WriteAllBytes($pfxPath, $certificateBytes)
 
 pushd $fullOutputPath\PackageContents
 
