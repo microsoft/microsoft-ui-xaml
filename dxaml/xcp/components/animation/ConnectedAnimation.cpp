8e63feb60cb76355f7f0f94bdfbba137ec36e318
Nook <1934285489lyz@gmail.com>
2026-08-15 02:06:35 +0800

    Detach ConnectedAnimation snapshot surfaces during cleanup

--- dxaml/xcp/components/animation/ConnectedAnimation.cpp
+++ dxaml/xcp/components/animation/ConnectedAnimation.cpp
@@ -1320,6 +1320,14 @@ _Check_return_ HRESULT CConnectedAnimation::ClearElementInfo(_In_ ConnectedAnima
 
     info.element = nullptr;
     info.visual = nullptr;
+    if (info.surfaceBrush != nullptr)
+    {
+        // Detach the visual surface so its cached snapshot can be released.
+        Microsoft::WRL::ComPtr<WUComp::ICompositionSurfaceBrush> surfaceBrush;
+        IFC_RETURN(info.surfaceBrush.As(&surfaceBrush));
+        IFC_RETURN(surfaceBrush->put_Surface(nullptr));
+    }
+    info.surfaceBrush.Reset();
     return S_OK;
 }
 
