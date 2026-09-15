79d9edc88224c7633875bad8c1e3247f176e5dac
Evelyn Wu <50890554+evelynwu-msft@users.noreply.github.com>
2026-09-14 17:18:43 -0700

    Regenerate XamlCompiler test masters to account for tabular rename

--- src/XamlCompiler/TestMasters/RegressionProjects/chk/Features/Conditionals/ConditionalControls/obj/x86/Debug/XamlTypeInfo.g.cs
+++ src/XamlCompiler/TestMasters/RegressionProjects/chk/Features/Conditionals/ConditionalControls/obj/x86/Debug/XamlTypeInfo.g.cs
@@ -407,7 +407,7 @@ namespace ConditionalControlsV2.ConditionalControls_XamlTypeInfo
                     global::Microsoft.UI.Xaml.Markup.IXamlMetadataProvider provider;
                     provider = new global::Microsoft.UI.Xaml.XamlTypeInfo.XamlControlsXamlMetaDataProvider() as global::Microsoft.UI.Xaml.Markup.IXamlMetadataProvider;
                     otherProviders.Add(provider); 
-                    provider = new global::Microsoft.UI.Xaml.XamlTypeInfo.XamlControlsChartsXamlMetaDataProvider() as global::Microsoft.UI.Xaml.Markup.IXamlMetadataProvider;
+                    provider = new global::Microsoft.UI.Xaml.XamlTypeInfo.XamlControlsTabularXamlMetaDataProvider() as global::Microsoft.UI.Xaml.Markup.IXamlMetadataProvider;
                     otherProviders.Add(provider); 
                     _otherProviders = otherProviders;
                 }
--- src/XamlCompiler/TestMasters/RegressionProjects/fre/Features/LinkedMD/LinkedMdControlsCS/obj/x86/Debug/XamlTypeInfo.g.cs
+++ src/XamlCompiler/TestMasters/RegressionProjects/fre/Features/LinkedMD/LinkedMdControlsCS/obj/x86/Debug/XamlTypeInfo.g.cs
@@ -337,7 +337,7 @@ namespace ControlsCS.LinkedMdControlsCS_XamlTypeInfo
                     global::Microsoft.UI.Xaml.Markup.IXamlMetadataProvider provider;
                     provider = new global::Microsoft.UI.Xaml.XamlTypeInfo.XamlControlsXamlMetaDataProvider() as global::Microsoft.UI.Xaml.Markup.IXamlMetadataProvider;
                     otherProviders.Add(provider); 
-                    provider = new global::Microsoft.UI.Xaml.XamlTypeInfo.XamlControlsChartsXamlMetaDataProvider() as global::Microsoft.UI.Xaml.Markup.IXamlMetadataProvider;
+                    provider = new global::Microsoft.UI.Xaml.XamlTypeInfo.XamlControlsTabularXamlMetaDataProvider() as global::Microsoft.UI.Xaml.Markup.IXamlMetadataProvider;
                     otherProviders.Add(provider); 
                     provider = new global::SubControlsCS.LinkedMdSubControlsCS_XamlTypeInfo.XamlMetaDataProvider() as global::Microsoft.UI.Xaml.Markup.IXamlMetadataProvider;
                     otherProviders.Add(provider); 
