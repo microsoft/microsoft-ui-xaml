6bacd22c64bd391a09f1c83d4f4c0e8c14a04347
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 02:12:26 +0800

    Re runcodegen; format

--- dxaml/phone/lib/PhoneTypes.g.cpp
+++ dxaml/phone/lib/PhoneTypes.g.cpp
@@ -4666,14 +4666,15 @@ PickerConfirmedEventArgsGenerated::InitializeImpl()
 _Check_return_ HRESULT 
 PickerFlyoutGenerated::RuntimeClassInitialize()
 {
-    RRETURN(InitializeImpl());
+    RRETURN(InitializeImpl(nullptr));
 }
 
 _Check_return_ HRESULT
-PickerFlyoutGenerated::InitializeImpl()
+PickerFlyoutGenerated::InitializeImpl(_In_opt_ IInspectable* pOuter)
 {
     HRESULT hr = S_OK;
 
+    UNREFERENCED_PARAMETER(pOuter);
 
     // When types are created internally there is no guarantee that
     // the factory has been previously instantiated. 
@@ -4925,16 +4926,17 @@ void PickerFlyoutFactory::ClearProperties()
 IFACEMETHODIMP PickerFlyoutFactory::CreateInstance(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Microsoft::UI::Xaml::Controls::IPickerFlyout** ppInstance)
 {
     HRESULT hr = S_OK;
+
     IFC((pctl::AggregableComObject<
             ABI::Microsoft::UI::Xaml::Controls::PickerFlyout,
             ABI::Microsoft::UI::Xaml::Controls::IPickerFlyout>::CreateInstance(
             pOuter,
             ppInner,
             ppInstance)));
+
 Cleanup:
     RRETURN(hr);
 }
-
 // Static properties.
 
 // Dependency properties initializing functions
