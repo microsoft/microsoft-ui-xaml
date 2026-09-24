498473608029529444c1270db6af90e44a9629c6
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 01:59:09 +0800

    Reruncodegen

--- dxaml/xcp/dxaml/lib/winrtgeneratedclasses/Viewbox.g.cpp
+++ dxaml/xcp/dxaml/lib/winrtgeneratedclasses/Viewbox.g.cpp
@@ -80,7 +80,7 @@ HRESULT DirectUI::ViewboxFactory::QueryInterfaceImpl(_In_ REFIID iid, _Outptr_ v
     {
         *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IViewboxFactory*>(this);
     }
-    else     if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IViewboxStatics)))
+    else if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IViewboxStatics)))
     {
         *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IViewboxStatics*>(this);
     }
@@ -95,15 +95,25 @@ HRESULT DirectUI::ViewboxFactory::QueryInterfaceImpl(_In_ REFIID iid, _Outptr_ v
 
 
 // Factory methods.
-
-// Factory methods.
 IFACEMETHODIMP DirectUI::ViewboxFactory::CreateInstance(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Microsoft::UI::Xaml::Controls::IViewbox** ppInstance)
 {
+
 #if DBG
+    // We play some games with reinterpret_cast and assuming that the GUID type table is accurate - which is somewhat sketchy, but
+    // really good for binary size.  This code is a sanity check that the games we play are ok.
     const GUID uuidofGUID = __uuidof(ABI::Microsoft::UI::Xaml::Controls::IViewbox);
     const GUID metadataAPIGUID = MetadataAPI::GetClassInfoByIndex(GetTypeIndex())->GetGuid();
-    if (uuidofGUID != metadataAPIGUID) { XAML_FAIL_FAST(); }
+    const KnownTypeIndex typeIndex = GetTypeIndex();
+
+    if(uuidofGUID != metadataAPIGUID)
+    {
+        XAML_FAIL_FAST();
+    }
 #endif
+
+    // Can't just IFC(_RETURN) this because for some validate calls (those with multiple template parameters), the
+    // preprocessor gets confused at the "," in the template type-list before the function's opening parenthesis.
+    // So we'll use IFC_RETURN syntax with a local hr variable, kind of weirdly.
     const HRESULT hr = ctl::ValidateFactoryCreateInstanceWithBetterAggregableCoreObjectActivationFactory(pOuter, ppInner, reinterpret_cast<IUnknown**>(ppInstance), GetTypeIndex(), false /*isFreeThreaded*/);
     IFC_RETURN(hr);
     return S_OK;
