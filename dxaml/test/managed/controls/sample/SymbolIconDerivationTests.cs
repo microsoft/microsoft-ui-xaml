d56ff071f89757071fa0d6ecbcc090d218df33d8
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 02:05:07 +0800

    Re runcodegen

--- dxaml/test/managed/controls/sample/SymbolIconDerivationTests.cs
+++ dxaml/test/managed/controls/sample/SymbolIconDerivationTests.cs
@@ -1,3 +1,5 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 using WEX.TestExecution;
 using Private.Infrastructure;
 using XamlControls = Microsoft.UI.Xaml.Controls;
--- dxaml/xcp/dxaml/lib/winrtgeneratedclasses/SymbolIcon.g.cpp
+++ dxaml/xcp/dxaml/lib/winrtgeneratedclasses/SymbolIcon.g.cpp
@@ -78,14 +78,23 @@ HRESULT DirectUI::SymbolIconFactory::QueryInterfaceImpl(_In_ REFIID iid, _Outptr
 // Factory methods.
 IFACEMETHODIMP DirectUI::SymbolIconFactory::CreateInstance(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Microsoft::UI::Xaml::Controls::ISymbolIcon** ppInstance)
 {
+
 #if DBG
+    // We play some games with reinterpret_cast and assuming that the GUID type table is accurate - which is somewhat sketchy, but
+    // really good for binary size.  This code is a sanity check that the games we play are ok.
     const GUID uuidofGUID = __uuidof(ABI::Microsoft::UI::Xaml::Controls::ISymbolIcon);
     const GUID metadataAPIGUID = MetadataAPI::GetClassInfoByIndex(GetTypeIndex())->GetGuid();
-    if (uuidofGUID != metadataAPIGUID)
+    const KnownTypeIndex typeIndex = GetTypeIndex();
+
+    if(uuidofGUID != metadataAPIGUID)
     {
         XAML_FAIL_FAST();
     }
 #endif
+
+    // Can't just IFC(_RETURN) this because for some validate calls (those with multiple template parameters), the
+    // preprocessor gets confused at the "," in the template type-list before the function's opening parenthesis.
+    // So we'll use IFC_RETURN syntax with a local hr variable, kind of weirdly.
     const HRESULT hr = ctl::ValidateFactoryCreateInstanceWithBetterAggregableCoreObjectActivationFactory(pOuter, ppInner, reinterpret_cast<IUnknown**>(ppInstance), GetTypeIndex(), false /*isFreeThreaded*/);
     IFC_RETURN(hr);
     return S_OK;
