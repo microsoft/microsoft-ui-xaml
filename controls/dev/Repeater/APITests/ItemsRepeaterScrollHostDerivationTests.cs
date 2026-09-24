cc75b6fb85276a12adee5103dfbedce96bab1d22
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 03:00:45 +0800

    Make ItemsRepeaterScrollHost derivable
    Unseal the modern C++/WinRT runtimeclass and add an API test that derives and instantiates it.

---
+++ controls/dev/Repeater/APITests/ItemsRepeaterScrollHostDerivationTests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.UI.Xaml.Controls;
+using MUXControlsTestApp.Utilities;
+
+using WEX.TestExecution;
+
+namespace Microsoft.UI.Xaml.Tests.MUXControls.ApiTests.RepeaterTests
+{
+    [TestClass]
+    public class ItemsRepeaterScrollHostDerivationTests : ApiTestBase
+    {
+        private sealed class DerivedItemsRepeaterScrollHost : ItemsRepeaterScrollHost
+        {
+        }
+
+        [TestMethod]
+        public void CanDeriveFromItemsRepeaterScrollHost()
+        {
+            RunOnUIThread.Execute(() =>
+            {
+                var scrollHost = new DerivedItemsRepeaterScrollHost
+                {
+                    HorizontalAnchorRatio = 0.5,
+                    VerticalAnchorRatio = 0.5
+                };
+
+                Verify.AreEqual(0.5, scrollHost.HorizontalAnchorRatio);
+                Verify.AreEqual(0.5, scrollHost.VerticalAnchorRatio);
+            });
+        }
+    }
+}
