ee1796df0595d494c159de4e0f9a56c895c1a271
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 02:00:24 +0800

    Reruncodegen

--- dxaml/xcp/dxaml/lib/winrtgeneratedclasses/VariableSizedWrapGrid.g.cpp
+++ dxaml/xcp/dxaml/lib/winrtgeneratedclasses/VariableSizedWrapGrid.g.cpp
@@ -114,7 +114,7 @@ HRESULT DirectUI::VariableSizedWrapGridFactory::QueryInterfaceImpl(_In_ REFIID i
     {
         *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IVariableSizedWrapGridFactory*>(this);
     }
-    else     if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IVariableSizedWrapGridStatics)))
+    else if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IVariableSizedWrapGridStatics)))
     {
         *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IVariableSizedWrapGridStatics*>(this);
     }
@@ -129,14 +129,28 @@ HRESULT DirectUI::VariableSizedWrapGridFactory::QueryInterfaceImpl(_In_ REFIID i
 
 
 // Factory methods.
-
-// Factory methods.
 IFACEMETHODIMP DirectUI::VariableSizedWrapGridFactory::CreateInstance(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Microsoft::UI::Xaml::Controls::IVariableSizedWrapGrid** ppInstance)
 {
+
 #if DBG
-    const GUID uuidofGUID=__uuidof(ABI::Microsoft::UI::Xaml::Controls::IVariableSizedWrapGrid); const GUID metadataAPIGUID=MetadataAPI::GetClassInfoByIndex(GetTypeIndex())->GetGuid(); if(uuidofGUID!=metadataAPIGUID){XAML_FAIL_FAST();}
+    // We play some games with reinterpret_cast and assuming that the GUID type table is accurate - which is somewhat sketchy, but
+    // really good for binary size.  This code is a sanity check that the games we play are ok.
+    const GUID uuidofGUID = __uuidof(ABI::Microsoft::UI::Xaml::Controls::IVariableSizedWrapGrid);
+    const GUID metadataAPIGUID = MetadataAPI::GetClassInfoByIndex(GetTypeIndex())->GetGuid();
+    const KnownTypeIndex typeIndex = GetTypeIndex();
+
+    if(uuidofGUID != metadataAPIGUID)
+    {
+        XAML_FAIL_FAST();
+    }
 #endif
-    const HRESULT hr=ctl::ValidateFactoryCreateInstanceWithBetterAggregableCoreObjectActivationFactory(pOuter,ppInner,reinterpret_cast<IUnknown**>(ppInstance),GetTypeIndex(),false); IFC_RETURN(hr); return S_OK;
+
+    // Can't just IFC(_RETURN) this because for some validate calls (those with multiple template parameters), the
+    // preprocessor gets confused at the "," in the template type-list before the function's opening parenthesis.
+    // So we'll use IFC_RETURN syntax with a local hr variable, kind of weirdly.
+    const HRESULT hr = ctl::ValidateFactoryCreateInstanceWithBetterAggregableCoreObjectActivationFactory(pOuter, ppInner, reinterpret_cast<IUnknown**>(ppInstance), GetTypeIndex(), false /*isFreeThreaded*/);
+    IFC_RETURN(hr);
+    return S_OK;
 }
 
 // Dependency properties.
--- dxaml/xcp/dxaml/lib/winrtgeneratedclasses/VariableSizedWrapGrid.g.h
+++ dxaml/xcp/dxaml/lib/winrtgeneratedclasses/VariableSizedWrapGrid.g.h
@@ -107,7 +107,6 @@ namespace DirectUI
         // Factory methods.
         IFACEMETHOD(CreateInstance)(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Microsoft::UI::Xaml::Controls::IVariableSizedWrapGrid** ppInstance);
 
-
         // Static properties.
 
         // Dependency properties.
