9e4e34528fb86e23f0063070a6e1f136109f662a
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 02:06:22 +0800

    Re runcodegen

--- dxaml/test/managed/controls/sample/SemanticZoomDerivationTests.cs
+++ dxaml/test/managed/controls/sample/SemanticZoomDerivationTests.cs
@@ -1,1 +1,29 @@
-using WEX.TestExecution; using Private.Infrastructure; using XamlControls=Microsoft.UI.Xaml.Controls; namespace Microsoft.UI.Xaml.Tests.Controls { [TestClass] public class SemanticZoomDerivationTests:XamlTestsBase { private sealed class DerivedSemanticZoom:XamlControls.SemanticZoom{} [ClassInitialize][TestProperty("BinaryUnderTest","Microsoft.UI.Xaml.dll")][TestProperty("RunAs","UAP")][TestProperty("UAP:Praid","XamlManagedTAEFTests")][TestProperty("Classification","Integration")] public static void Setup(TestContext c)=>XamlTestsBase.SetupBase(c); [ClassCleanup] public void Cleanup()=>base.CommonClassCleanup(); [TestMethod] public void CanDerive(){UIExecutor.Execute(()=>{_=new DerivedSemanticZoom();});}}}
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+using WEX.TestExecution;
+using Private.Infrastructure;
+using XamlControls = Microsoft.UI.Xaml.Controls;
+
+namespace Microsoft.UI.Xaml.Tests.Controls
+{
+    [TestClass]
+    public class SemanticZoomDerivationTests : XamlTestsBase
+    {
+        private sealed class DerivedSemanticZoom : XamlControls.SemanticZoom { }
+        [ClassInitialize]
+        [TestProperty("BinaryUnderTest", "Microsoft.UI.Xaml.dll")]
+        [TestProperty("RunAs", "UAP")]
+        [TestProperty("UAP:Praid", "XamlManagedTAEFTests")]
+        [TestProperty("Classification", "Integration")]
+        public static void Setup(TestContext c) => XamlTestsBase.SetupBase(c);
+
+        [ClassCleanup]
+        public void Cleanup() => base.CommonClassCleanup();
+
+        [TestMethod]
+        public void CanDerive()
+        {
+            UIExecutor.Execute(() => { _ = new DerivedSemanticZoom(); });
+        }
+    }
+}
