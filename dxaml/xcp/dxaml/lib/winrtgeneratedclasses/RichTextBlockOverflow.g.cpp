9f7594775eb496edf0685f54ac55a313557a3199
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 02:11:07 +0800

    Re runcodegen

--- dxaml/xcp/dxaml/lib/winrtgeneratedclasses/RichTextBlockOverflow.g.cpp
+++ dxaml/xcp/dxaml/lib/winrtgeneratedclasses/RichTextBlockOverflow.g.cpp
@@ -203,7 +203,7 @@ HRESULT DirectUI::RichTextBlockOverflowFactory::QueryInterfaceImpl(_In_ REFIID i
     {
         *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockOverflowFactory*>(this);
     }
-    else     if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockOverflowStatics)))
+    else if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockOverflowStatics)))
     {
         *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockOverflowStatics*>(this);
     }
@@ -218,15 +218,25 @@ HRESULT DirectUI::RichTextBlockOverflowFactory::QueryInterfaceImpl(_In_ REFIID i
 
 
 // Factory methods.
-
-// Factory methods.
 IFACEMETHODIMP DirectUI::RichTextBlockOverflowFactory::CreateInstance(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockOverflow** ppInstance)
 {
+
 #if DBG
+    // We play some games with reinterpret_cast and assuming that the GUID type table is accurate - which is somewhat sketchy, but
+    // really good for binary size.  This code is a sanity check that the games we play are ok.
     const GUID uuidofGUID = __uuidof(ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockOverflow);
     const GUID metadataAPIGUID = MetadataAPI::GetClassInfoByIndex(GetTypeIndex())->GetGuid();
-    if (uuidofGUID != metadataAPIGUID) { XAML_FAIL_FAST(); }
+    const KnownTypeIndex typeIndex = GetTypeIndex();
+
+    if(uuidofGUID != metadataAPIGUID)
+    {
+        XAML_FAIL_FAST();
+    }
 #endif
+
+    // Can't just IFC(_RETURN) this because for some validate calls (those with multiple template parameters), the
+    // preprocessor gets confused at the "," in the template type-list before the function's opening parenthesis.
+    // So we'll use IFC_RETURN syntax with a local hr variable, kind of weirdly.
     const HRESULT hr = ctl::ValidateFactoryCreateInstanceWithBetterAggregableCoreObjectActivationFactory(pOuter, ppInner, reinterpret_cast<IUnknown**>(ppInstance), GetTypeIndex(), false /*isFreeThreaded*/);
     IFC_RETURN(hr);
     return S_OK;
