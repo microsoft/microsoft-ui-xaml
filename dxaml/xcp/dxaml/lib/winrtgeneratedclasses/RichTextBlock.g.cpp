5d395926b54ab373307f1ae1ffe6fc07a8a3c75b
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 02:09:51 +0800

    Re runcodegen

--- dxaml/xcp/dxaml/lib/winrtgeneratedclasses/RichTextBlock.g.cpp
+++ dxaml/xcp/dxaml/lib/winrtgeneratedclasses/RichTextBlock.g.cpp
@@ -608,7 +608,7 @@ HRESULT DirectUI::RichTextBlockFactory::QueryInterfaceImpl(_In_ REFIID iid, _Out
     {
         *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockFactory*>(this);
     }
-    else     if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockStatics)))
+    else if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockStatics)))
     {
         *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IRichTextBlockStatics*>(this);
     }
@@ -623,15 +623,25 @@ HRESULT DirectUI::RichTextBlockFactory::QueryInterfaceImpl(_In_ REFIID iid, _Out
 
 
 // Factory methods.
-
-// Factory methods.
 IFACEMETHODIMP DirectUI::RichTextBlockFactory::CreateInstance(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Microsoft::UI::Xaml::Controls::IRichTextBlock** ppInstance)
 {
+
 #if DBG
+    // We play some games with reinterpret_cast and assuming that the GUID type table is accurate - which is somewhat sketchy, but
+    // really good for binary size.  This code is a sanity check that the games we play are ok.
     const GUID uuidofGUID = __uuidof(ABI::Microsoft::UI::Xaml::Controls::IRichTextBlock);
     const GUID metadataAPIGUID = MetadataAPI::GetClassInfoByIndex(GetTypeIndex())->GetGuid();
-    if (uuidofGUID != metadataAPIGUID) { XAML_FAIL_FAST(); }
+    const KnownTypeIndex typeIndex = GetTypeIndex();
+
+    if(uuidofGUID != metadataAPIGUID)
+    {
+        XAML_FAIL_FAST();
+    }
 #endif
+
+    // Can't just IFC(_RETURN) this because for some validate calls (those with multiple template parameters), the
+    // preprocessor gets confused at the "," in the template type-list before the function's opening parenthesis.
+    // So we'll use IFC_RETURN syntax with a local hr variable, kind of weirdly.
     const HRESULT hr = ctl::ValidateFactoryCreateInstanceWithBetterAggregableCoreObjectActivationFactory(pOuter, ppInner, reinterpret_cast<IUnknown**>(ppInstance), GetTypeIndex(), false /*isFreeThreaded*/);
     IFC_RETURN(hr);
     return S_OK;
