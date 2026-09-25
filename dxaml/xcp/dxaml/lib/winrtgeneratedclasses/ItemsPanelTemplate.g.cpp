59014667315130d08edd067f3645ba64b0226de8
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 13:56:15 +0800

    Run codegen.cmd

--- dxaml/xcp/dxaml/lib/winrtgeneratedclasses/ItemsPanelTemplate.g.cpp
+++ dxaml/xcp/dxaml/lib/winrtgeneratedclasses/ItemsPanelTemplate.g.cpp
@@ -46,11 +46,59 @@ HRESULT DirectUI::ItemsPanelTemplate::QueryInterfaceImpl(_In_ REFIID iid, _Outpt
 
 // Methods.
 
+HRESULT DirectUI::ItemsPanelTemplateFactory::QueryInterfaceImpl(_In_ REFIID iid, _Outptr_ void** ppObject)
+{
+    if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IItemsPanelTemplateFactory)))
+    {
+        *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IItemsPanelTemplateFactory*>(this);
+    }
+    else
+    {
+        RRETURN(ctl::BetterAggregableCoreObjectActivationFactory::QueryInterfaceImpl(iid, ppObject));
+    }
+
+    AddRefOuter();
+    RRETURN(S_OK);
+}
+
+
+// Factory methods.
+IFACEMETHODIMP DirectUI::ItemsPanelTemplateFactory::CreateInstance(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Microsoft::UI::Xaml::Controls::IItemsPanelTemplate** ppInstance)
+{
+
+#if DBG
+    // We play some games with reinterpret_cast and assuming that the GUID type table is accurate - which is somewhat sketchy, but
+    // really good for binary size.  This code is a sanity check that the games we play are ok.
+    const GUID uuidofGUID = __uuidof(ABI::Microsoft::UI::Xaml::Controls::IItemsPanelTemplate);
+    const GUID metadataAPIGUID = MetadataAPI::GetClassInfoByIndex(GetTypeIndex())->GetGuid();
+    const KnownTypeIndex typeIndex = GetTypeIndex();
+
+    if(uuidofGUID != metadataAPIGUID)
+    {
+        XAML_FAIL_FAST();
+    }
+#endif
+
+    // Can't just IFC(_RETURN) this because for some validate calls (those with multiple template parameters), the
+    // preprocessor gets confused at the "," in the template type-list before the function's opening parenthesis.
+    // So we'll use IFC_RETURN syntax with a local hr variable, kind of weirdly.
+    const HRESULT hr = ctl::ValidateFactoryCreateInstanceWithBetterAggregableCoreObjectActivationFactory(pOuter, ppInner, reinterpret_cast<IUnknown**>(ppInstance), GetTypeIndex(), false /*isFreeThreaded*/);
+    IFC_RETURN(hr);
+    return S_OK;
+}
+
+// Dependency properties.
+
+// Attached properties.
+
+// Static properties.
+
+// Static methods.
 
 namespace DirectUI
 {
     _Check_return_ IActivationFactory* CreateActivationFactory_ItemsPanelTemplate()
     {
-        RRETURN(ctl::BetterActivationFactoryCreator::GetForDO(KnownTypeIndex::ItemsPanelTemplate));
+        RRETURN(ctl::ActivationFactoryCreator<ItemsPanelTemplateFactory>::CreateActivationFactory());
     }
 }
