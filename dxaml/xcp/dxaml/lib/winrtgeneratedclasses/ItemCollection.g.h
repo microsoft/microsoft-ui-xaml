84acdc1325fc229241e80f4eb18da3b4a84a860c
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 13:52:43 +0800

    Run codegen

--- dxaml/xcp/dxaml/lib/winrtgeneratedclasses/ItemCollection.g.h
+++ dxaml/xcp/dxaml/lib/winrtgeneratedclasses/ItemCollection.g.h
@@ -60,3 +60,49 @@ namespace DirectUI
 
 #include "ItemCollection_Partial.h"
 
+namespace DirectUI
+{
+    // Note that the ordering of the base types here is important - the base factory comes first, followed by all the
+    // interfaces specific to this type.  By doing this, we allow every Factory's CreateInstance method to be more
+    // COMDAT-folding-friendly.  Because this ensures that the first vfptr contains GetTypeIndex, it means that all
+    // CreateInstance functions with the same base factory generate the same assembly instructions and thus will
+    // fold together.  This is significant for binary size in Microsoft.UI.Xaml.dll so change this only with great
+    // care.
+    class __declspec(novtable) ItemCollectionFactory:
+       public ctl::BetterAggregableCoreObjectActivationFactory
+        , public ABI::Microsoft::UI::Xaml::Controls::IItemCollectionFactory
+    {
+        BEGIN_INTERFACE_MAP(ItemCollectionFactory, ctl::BetterAggregableCoreObjectActivationFactory)
+            INTERFACE_ENTRY(ItemCollectionFactory, ABI::Microsoft::UI::Xaml::Controls::IItemCollectionFactory)
+        END_INTERFACE_MAP(ItemCollectionFactory, ctl::BetterAggregableCoreObjectActivationFactory)
+
+    public:
+        // Factory methods.
+        IFACEMETHOD(CreateInstance)(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Windows::Foundation::Collections::IObservableVector<IInspectable*>** ppInstance);
+
+        // Static properties.
+
+        // Dependency properties.
+
+        // Attached properties.
+
+        // Static methods.
+
+        // Static events.
+
+    protected:
+        HRESULT QueryInterfaceImpl(_In_ REFIID iid, _Outptr_ void** ppObject) override;
+
+        KnownTypeIndex GetTypeIndex() const override
+        {
+            return KnownTypeIndex::ItemCollection;
+        }
+
+
+    private:
+
+        // Customized static properties.
+
+        // Customized static  methods.
+    };
+}
