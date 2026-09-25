82fcc0f592e14c09416563f10848152d36fb8c9f
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-25 12:47:36 +0800

    Regenerate ScrollViewer composable surface
    Restore the checked-in XamlGen outputs expected after removing ForceSealed from ScrollViewer while keeping ScrollViewerView sealed.

--- dxaml/xcp/dxaml/lib/winrtgeneratedclasses/ScrollViewer.g.cpp
+++ dxaml/xcp/dxaml/lib/winrtgeneratedclasses/ScrollViewer.g.cpp
@@ -1322,13 +1322,17 @@ _Check_return_ HRESULT DirectUI::ScrollViewerGenerated::EventRemoveHandlerByInde
 
 HRESULT DirectUI::ScrollViewerFactory::QueryInterfaceImpl(_In_ REFIID iid, _Outptr_ void** ppObject)
 {
-    if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IScrollViewerStatics)))
+    if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IScrollViewerFactory)))
+    {
+        *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IScrollViewerFactory*>(this);
+    }
+    else if (InlineIsEqualGUID(iid, __uuidof(ABI::Microsoft::UI::Xaml::Controls::IScrollViewerStatics)))
     {
         *ppObject = static_cast<ABI::Microsoft::UI::Xaml::Controls::IScrollViewerStatics*>(this);
     }
     else
     {
-        RRETURN(ctl::BetterCoreObjectActivationFactory::QueryInterfaceImpl(iid, ppObject));
+        RRETURN(ctl::BetterAggregableCoreObjectActivationFactory::QueryInterfaceImpl(iid, ppObject));
     }
 
     AddRefOuter();
@@ -1337,6 +1341,29 @@ HRESULT DirectUI::ScrollViewerFactory::QueryInterfaceImpl(_In_ REFIID iid, _Outp
 
 
 // Factory methods.
+IFACEMETHODIMP DirectUI::ScrollViewerFactory::CreateInstance(_In_opt_ IInspectable* pOuter, _Outptr_ IInspectable** ppInner, _Outptr_ ABI::Microsoft::UI::Xaml::Controls::IScrollViewer** ppInstance)
+{
+
+#if DBG
+    // We play some games with reinterpret_cast and assuming that the GUID type table is accurate - which is somewhat sketchy, but
+    // really good for binary size.  This code is a sanity check that the games we play are ok.
+    const GUID uuidofGUID = __uuidof(ABI::Microsoft::UI::Xaml::Controls::IScrollViewer);
+    const GUID metadataAPIGUID = MetadataAPI::GetClassInfoByIndex(GetTypeIndex())->GetGuid();
+    const KnownTypeIndex typeIndex = GetTypeIndex();
+
+    if(uuidofGUID != metadataAPIGUID)
+    {
+        XAML_FAIL_FAST();
+    }
+#endif
+
+    // Can't just IFC(_RETURN) this because for some validate calls (those with multiple template parameters), the
+    // preprocessor gets confused at the "," in the template type-list before the function's opening parenthesis.
+    // So we'll use IFC_RETURN syntax with a local hr variable, kind of weirdly.
+    const HRESULT hr = ctl::ValidateFactoryCreateInstanceWithBetterAggregableCoreObjectActivationFactory(pOuter, ppInner, reinterpret_cast<IUnknown**>(ppInstance), GetTypeIndex(), false /*isFreeThreaded*/);
+    IFC_RETURN(hr);
+    return S_OK;
+}
 
 // Dependency properties.
 IFACEMETHODIMP DirectUI::ScrollViewerFactory::get_HorizontalSnapPointsAlignmentProperty(_Out_ ABI::Microsoft::UI::Xaml::IDependencyProperty** ppValue)
